3ca49a1b3b59a02d64b2fd26f3db4717e0d2fe2b
lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2021-05-31 21:50:27 -0300

    Classes Round and RoundTest

--- src/test/java/com/bueno/truco/domain/entities/deck/TrucoDeckTest.java
+++ src/test/java/com/bueno/truco/domain/entities/deck/TrucoDeckTest.java
@@ -1,21 +1,33 @@
 package com.bueno.truco.domain.entities.deck;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class TrucoDeckTest {
 
+    private TrucoDeck deck;
+
+    @BeforeEach
+    void setUp() {
+        deck = new TrucoDeck();
+    }
+
+    @AfterEach
+    void tearDown() {
+        deck = null;
+    }
+
     @Test
     void shouldCreateDeckWith40Cards() {
-        TrucoDeck deck = new TrucoDeck();
         Assertions.assertEquals(40, deck.size());
     }
 
     @Test
     void shouldCreateSortedDeck() {
-        TrucoDeck deck = new TrucoDeck();
         List<Card> firstEight = List.of(
                 new Card(1, Suit.DIAMONDS), new Card(1, Suit.SPADES), new Card(1, Suit.HEARTS), new Card(1, Suit.CLUBS),
                 new Card(2, Suit.DIAMONDS), new Card(2, Suit.SPADES), new Card(2, Suit.HEARTS), new Card(2, Suit.CLUBS));
@@ -24,7 +36,6 @@ class TrucoDeckTest {
 
     @Test
     void shouldGetShuffledCards() {
-        TrucoDeck deck = new TrucoDeck();
         deck.shuffle();
         List<Card> firstEight = List.of(
                 new Card(1, Suit.DIAMONDS), new Card(1, Suit.SPADES), new Card(1, Suit.HEARTS), new Card(1, Suit.CLUBS),
@@ -34,7 +45,6 @@ class TrucoDeckTest {
 
     @Test
     void shouldNotContainEightsNinesTens() {
-        TrucoDeck deck = new TrucoDeck();
         List<Card> fullDeck = deck.take(40);
         Assertions.assertFalse(fullDeck.contains(new Card(8, Suit.SPADES)));
         Assertions.assertFalse(fullDeck.contains(new Card(9, Suit.CLUBS)));
