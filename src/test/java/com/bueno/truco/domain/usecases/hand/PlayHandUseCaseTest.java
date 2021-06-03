6b5de7e505970d44dc4531414f855c364d0a94a6
lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2021-06-03 14:42:33 -0300

    Warning fixes in PlayHandUseCaseTest.

--- src/test/java/com/bueno/truco/domain/usecases/hand/PlayHandUseCaseTest.java
+++ src/test/java/com/bueno/truco/domain/usecases/hand/PlayHandUseCaseTest.java
@@ -27,7 +27,7 @@ class PlayHandUseCaseTest {
         Player p2 = new PlayerMock("A", List.of(new Card(4, Suit.DIAMONDS), new Card(4, Suit.SPADES), new Card(4, Suit.HEARTS)));
         PlayHandUseCase hand = new PlayHandUseCase(p1, p2, new Card(6, Suit.DIAMONDS));
         HandResult result = hand.play();
-        Assertions.assertEquals(p1, result.getWinner().get());
+        Assertions.assertEquals(p1, result.getWinner().orElse(null));
     }
 
     @Test
@@ -36,7 +36,7 @@ class PlayHandUseCaseTest {
         Player p2 = new PlayerMock("B", List.of(new Card(4, Suit.CLUBS), new Card(5, Suit.SPADES), new Card(5, Suit.HEARTS)));
         PlayHandUseCase hand = new PlayHandUseCase(p1, p2, new Card(6, Suit.DIAMONDS));
         HandResult result = hand.play();
-        Assertions.assertEquals(p2, result.getWinner().get());
+        Assertions.assertEquals(p2, result.getWinner().orElse(null));
     }
 
     @Test
@@ -45,7 +45,7 @@ class PlayHandUseCaseTest {
         Player p2 = new PlayerMock("B", List.of(new Card(6, Suit.CLUBS), new Card(4, Suit.DIAMONDS), new Card(5, Suit.HEARTS)));
         PlayHandUseCase hand = new PlayHandUseCase(p1, p2, new Card(4, Suit.CLUBS));
         HandResult result = hand.play();
-        Assertions.assertEquals(p1, result.getWinner().get());
+        Assertions.assertEquals(p1, result.getWinner().orElse(null));
     }
 
     @Test
@@ -63,7 +63,7 @@ class PlayHandUseCaseTest {
         Player p2 = new PlayerMock("B", List.of(new Card(5, Suit.CLUBS), new Card(6, Suit.DIAMONDS), new Card('Q', Suit.CLUBS)));
         PlayHandUseCase hand = new PlayHandUseCase(p1, p2, new Card(7, Suit.DIAMONDS));
         HandResult result = hand.play();
-        Assertions.assertEquals(p2, result.getWinner().get());
+        Assertions.assertEquals(p2, result.getWinner().orElse(null));
     }
 
     @Test
@@ -72,6 +72,6 @@ class PlayHandUseCaseTest {
         Player p2 = new PlayerMock("B", List.of(new Card(5, Suit.CLUBS), new Card(6, Suit.DIAMONDS), new Card('Q', Suit.CLUBS)));
         PlayHandUseCase hand = new PlayHandUseCase(p1, p2, new Card('Q', Suit.DIAMONDS));
         HandResult result = hand.play();
-        Assertions.assertEquals(p2, result.getWinner().get());
+        Assertions.assertEquals(p2, result.getWinner().orElse(null));
     }
 }
