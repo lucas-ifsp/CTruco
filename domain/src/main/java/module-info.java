cd578b07b601e6f095f6630a7384d62e2f3d76f3
lucas-ifsp <lucas.ruas@gmail.com>
2022-01-20 22:40:14 -0300

    Removal or Truco and TrucoResult classes.

--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -6,7 +6,6 @@ module domain {
     exports com.bueno.domain.usecases.hand.validators to application.console, application.desktop;
 
     exports com.bueno.domain.entities.deck to application.desktop, application.console;
-    exports com.bueno.domain.entities.truco to application.desktop, application.console;
     exports com.bueno.domain.entities.player.util to application.desktop, application.console;
     exports com.bueno.domain.entities.player.dummybot to application.desktop, application.console;
     exports com.bueno.domain.entities.player.mineirobot to application.desktop, application.console;
