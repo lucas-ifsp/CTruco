fe6555b80dd31b74cf3dd60dde84e52b379c8118
lucas-ifsp <lucas.ruas@gmail.com>
2022-01-25 00:02:35 -0300

    Created a different module for persistence. Refactored the project to deal with the new module.

--- domain/src/main/java/com/bueno/domain/usecases/player/EntityAlreadyExistsException.java
+++ domain/src/main/java/com/bueno/domain/usecases/player/EntityAlreadyExistsException.java
@@ -18,7 +18,10 @@
  *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
  */
 
-package com.bueno.persistence.inmemory;
+package com.bueno.domain.usecases.player;
 
-public class InMemoryUserRepository {
+public class EntityAlreadyExistsException extends RuntimeException{
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
 }
