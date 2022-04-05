4d8fe95a7f4c83a17b32dccceb5bf6909be0ec15
lucas-ifsp <lucas.ruas@gmail.com>
2022-04-05 16:42:15 -0300

    Created documentation for public methods of CardToPlay class.

--- bot-spi/src/main/java/com/bueno/spi/model/CardToPlay.java
+++ bot-spi/src/main/java/com/bueno/spi/model/CardToPlay.java
@@ -29,22 +29,52 @@ public final class CardToPlay {
         this.discard = discard;
     }
 
+    /**
+     * <p>Represents the card that will be played in the round. The resulting {@link CardToPlay} object is final
+     * and will always return {@code false} when {@link #isDiscard()} is invoked.</p>
+     *
+     * @param card a {@link TrucoCard} that will be played in the round, must be non-null
+     * @return a {@link CardToPlay} order describing a {@link TrucoCard} to be played in the round
+     * @throws NullPointerException if {@code card} is null
+     * */
     public static CardToPlay of(TrucoCard card){
         return new CardToPlay(card, false);
     }
 
+    /**
+     * <p>Represents the card that will be discarded in the round. The resulting {@link CardToPlay} object is final
+     * and will always return {@code true} when {@link #isDiscard()} is invoked.</p>
+     *
+     * @param card a {@link TrucoCard} that will be discarded in the round, must be non-null
+     * @return a {@link CardToPlay} order describing a {@link TrucoCard} to be discarded in the round
+     * @throws NullPointerException if {@code card} is null
+     * */
     public static CardToPlay discard(TrucoCard card){
         return new CardToPlay(card, true);
     }
 
+    /**
+     * <p>Returns the wrapped {@link TrucoCard} object if the {@link CardToPlay} is planed to be played or
+     * {@link TrucoCard#closed()} if the {@link TrucoCard} is planed to be discarded.</p>
+     *
+     * @return an ordinary {@link TrucoCard} to be played or {@link TrucoCard#closed()}
+     * */
     public TrucoCard value() {
         return discard ? TrucoCard.closed() : content;
     }
 
+    /**
+     * <p>Returns the wrapped {@link TrucoCard} object without any changes.</p>
+     * @return the same {@link TrucoCard} object used to create the {@link CardToPlay}
+     * */
     public TrucoCard content() {
         return content;
     }
 
+    /**
+     * <p>Indicates if the {@link CardToPlay} is intended to be discarded or not.</p>
+     * @return {@code true} if the card is intended to be discarded or {@code false} otherwise
+     * */
     public boolean isDiscard() {
         return discard;
     }
