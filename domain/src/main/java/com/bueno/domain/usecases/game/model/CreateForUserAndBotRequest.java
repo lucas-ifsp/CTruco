c2087323fabd8e2db203ee0452bc70cb1f662391
lucas-ifsp <lucas.ruas@gmail.com>
2022-05-05 21:39:59 -0300

    refactor: change of CreateForUserAndBotRequest implementation from Lombok to Record

--- domain/src/main/java/com/bueno/domain/usecases/game/model/CreateForUserAndBotRequest.java
+++ domain/src/main/java/com/bueno/domain/usecases/game/model/CreateForUserAndBotRequest.java
@@ -20,22 +20,13 @@
 
 package com.bueno.domain.usecases.game.model;
 
-import lombok.Getter;
-import lombok.ToString;
-
 import java.util.Objects;
 import java.util.UUID;
 
-@Getter
-@ToString
-public final class CreateForUserAndBotRequest {
-
-    private final UUID userUuid;
-    private final String botName;
-
+public record CreateForUserAndBotRequest(UUID userUuid, String botName) {
     public CreateForUserAndBotRequest(UUID userUuid, String botName) {
         this.userUuid = Objects.requireNonNull(userUuid, "User UUID must not be null!");
         this.botName = Objects.requireNonNull(botName, "Bot name must not be null!");
-        if(botName.isEmpty()) throw new IllegalArgumentException("Bot name must not be empty!");
+        if (botName.isEmpty()) throw new IllegalArgumentException("Bot name must not be empty!");
     }
 }
