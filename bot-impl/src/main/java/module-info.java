283c68a30caa9260e4736fd18878c1d827ef613a
lucas-ifsp <lucas.ruas@gmail.com>
2022-05-17 15:25:11 -0300

    fix: exported dummybot from bot.impl module

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -4,5 +4,6 @@ import com.bueno.impl.mineirobot.MineiroBot;
 module bot.impl {
     requires bot.spi;
     exports com.bueno.impl.mineirobot;
+    exports com.bueno.impl.dummybot;
     provides com.bueno.spi.service.BotServiceProvider with MineiroBot, DummyBot;
 }
