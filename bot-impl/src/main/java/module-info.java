27acc505df26329b16ad0b48b939743cb277b203
lucas-ifsp <lucas.ruas@gmail.com>
2022-07-26 17:38:36 -0300

    refactor: remove MineiroBot from the project to use it as a jar dependency

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 import com.bueno.impl.dummybot.DummyBot;
-import com.bueno.impl.mineirobot.MineiroBot;
 
 module bot.impl {
     requires bot.spi;
-    exports com.bueno.impl.mineirobot;
     exports com.bueno.impl.dummybot;
-    provides com.bueno.spi.service.BotServiceProvider with MineiroBot, DummyBot;
+    provides com.bueno.spi.service.BotServiceProvider with DummyBot;
 }
