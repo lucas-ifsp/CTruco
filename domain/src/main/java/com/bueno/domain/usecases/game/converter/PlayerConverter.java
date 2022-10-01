d30c3b03780f39e05fa97eacdf69c5d5f35a006d
lucas-ifsp <lucas.ruas@gmail.com>
2022-10-01 16:49:54 -0300

    feat (domain): add PlayerConverter class

---
+++ domain/src/main/java/com/bueno/domain/usecases/game/converter/PlayerConverter.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2022 Lucas B. R. de Oliveira - IFSP/SCL
+ *  Contact: lucas <dot> oliveira <at> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.bueno.domain.usecases.game.converter;
+
+import com.bueno.domain.entities.player.Player;
+import com.bueno.domain.usecases.game.dtos.PlayerDto;
+import com.bueno.domain.usecases.intel.converters.CardConverter;
+import com.bueno.domain.usecases.intel.dtos.CardDto;
+
+import java.util.List;
+
+public class PlayerConverter {
+    public static PlayerDto toDto(Player player){
+        final List<CardDto> cardDtos = player.getCards().stream()
+                .map(CardConverter::toDto)
+                .toList();
+        return  new PlayerDto(
+                player.getUsername(),
+                player.getUuid(),
+                player.getScore(),
+                player.isBot(),
+                cardDtos);
+    }
+}
