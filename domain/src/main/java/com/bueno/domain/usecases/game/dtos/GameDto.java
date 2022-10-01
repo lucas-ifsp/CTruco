e731d188431028670102e9c9a1a546254ef812cc
lucas-ifsp <lucas.ruas@gmail.com>
2022-10-01 16:36:43 -0300

    feat (domain): add GameDto

---
+++ domain/src/main/java/com/bueno/domain/usecases/game/dtos/GameDto.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) 2022 Lucas B. R. de Oliveira - IFSP/SCL
+ *  Contact: lucas <dot> oliveira <at> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.bueno.domain.usecases.game.dtos;
+
+import com.bueno.domain.usecases.hand.dtos.HandDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record GameDto(LocalDateTime timestamp, UUID player1, UUID player2,
+                      UUID firstToPlay, UUID lastToPlay, List<HandDto> hands) {
+}
