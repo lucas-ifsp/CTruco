95fe8a4c2e289f2a184fdd8cc8520301fa2b5787
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-06 18:24:56 -0300

    feat(bot-impl): add PaulistaBot class without implemetantion

---
+++ bot-impl/src/main/java/com/bonelli/noli/paulistabot/PaulistaBot.java
@@ -0,0 +1,28 @@
+package com.bonelli.noli.paulistabot;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class PaulistaBot implements BotServiceProvider {
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 1;
+    }
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return CardToPlay.of(intel.getCards().get(0));
+    }
+}
--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -1,7 +1,8 @@
+import com.bonelli.noli.paulistabot.PaulistaBot;
 import com.bueno.impl.dummybot.DummyBot;
 
 module bot.impl {
     requires bot.spi;
     exports com.bueno.impl.dummybot;
-    provides com.bueno.spi.service.BotServiceProvider with DummyBot;
+    provides com.bueno.spi.service.BotServiceProvider with DummyBot, PaulistaBot;
 }
