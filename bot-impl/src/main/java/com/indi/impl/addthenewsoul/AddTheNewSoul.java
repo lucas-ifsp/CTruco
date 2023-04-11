9357c80a51e4098727d65c10af4e6c405630904f
IngridNery <neryingrid16@gmail.com>
2023-04-11 13:27:09 -0300

    feat(bot): Add bot AddTheNewSoul to play

---
+++ bot-impl/src/main/java/com/indi/impl/addthenewsoul/AddTheNewSoul.java
@@ -0,0 +1,27 @@
+package com.indi.impl.addthenewsoul;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class AddTheNewSoul implements BotServiceProvider {
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+}
