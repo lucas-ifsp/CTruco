3cd0a6140029f30bfdb4dcff29d04fc643899aad
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-13 21:57:40 -0300

    feat(bot-impl): created public method that returns if there is an open card besides the turn

--- bot-impl/src/main/java/com/bonelli/noli/paulistabot/FirstRound.java
+++ bot-impl/src/main/java/com/bonelli/noli/paulistabot/FirstRound.java
@@ -2,8 +2,28 @@ package com.bonelli.noli.paulistabot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 
-public class FirstRound implements Strategy{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FirstRound implements Strategy {
+
+    private final TrucoCard vira;
+
+    private final List<TrucoCard> cardList;
+
+    private final List<TrucoCard> openCards;
+
+    private final GameIntel intel;
+
+    public FirstRound(GameIntel intel) {
+        this.vira = intel.getVira();
+        this.cardList = new ArrayList<>(intel.getCards());
+        this.openCards = new ArrayList<>(intel.getOpenCards());
+        this.intel = intel;
+    }
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
@@ -23,5 +43,11 @@ public class FirstRound implements Strategy{
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
         return null;
+    }
+
+    public Optional<TrucoCard> getWhichBotShouldPlayFirst () {
+        if (this.intel.getOpponentCard().isEmpty())
+            return Optional.empty();
+        return Optional.of(this.intel.getOpponentCard().get());
     }
 }
