c14756f9dfc9e0cf819fea3c1b1f53c5514b39ef
Danilo Araujo <x45danilo45x@gmail.com>
2023-04-16 11:04:35 -0300

    feat(bot): add WrkncacterBot

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 import com.bueno.impl.dummybot.DummyBot;
+import com.hideki.araujo.wrkncacnterbot.WrkncacnterBot;
 
 module bot.impl {
     requires bot.spi;
     exports com.bueno.impl.dummybot;
-    provides com.bueno.spi.service.BotServiceProvider with DummyBot;
+    provides com.bueno.spi.service.BotServiceProvider with DummyBot, WrkncacnterBot;
 }
