05522560a6f8fc422bb6e30db8e2aa2dcd284b43
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-16 17:50:36 -0300

    feat(bot-impl): card decision to play if opponent starts

--- bot-impl/src/main/java/com/bonelli/noli/paulistabot/FirstRound.java
+++ bot-impl/src/main/java/com/bonelli/noli/paulistabot/FirstRound.java
@@ -5,6 +5,7 @@ import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,7 +43,51 @@ public class FirstRound implements Strategy {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
+        Optional<TrucoCard> opponentCard = getWhichBotShouldPlayFirst();
+        if (opponentCard.isPresent()) {
+            TrucoCard opponentCardBot = opponentCard.get();
+            int countCardsHigherOfOpponent = getCountCardsAreHigherOfOpponent(this.cardList, opponentCardBot, this.vira);
+            switch (countCardsHigherOfOpponent) {
+                case 1 -> {
+                    CardToPlay.of(checkWhichCardHasHigherValue(this.cardList, this.vira));
+                }
+                case 2, 3 -> {
+                    CardToPlay.of(chooseCardThatBeatsTheOpponent(this.cardList, this.vira, opponentCardBot));
+                }
+                default -> {
+                    CardToPlay.of(checkWhichCardHasLowerValue(this.cardList, this.vira));
+                }
+            }
+        }
         return null;
+    }
+
+    private TrucoCard chooseCardThatBeatsTheOpponent(List<TrucoCard> cardList, TrucoCard vira, TrucoCard opponentCard) {
+        return cardList.stream()
+                .filter(carta -> carta.relativeValue(vira) > opponentCard.relativeValue(vira))
+                .min(Comparator.comparingInt(carta -> carta.relativeValue(vira)))
+                .orElse(null);
+    }
+
+    private int getCountCardsAreHigherOfOpponent(List<TrucoCard> cards, TrucoCard card, TrucoCard vira) {
+        int count = 0;
+        for (TrucoCard trucoCard : cards) {
+            if (trucoCard.compareValueTo(card, vira) > 0)
+                count++;
+        }
+        return count;
+    }
+
+    private TrucoCard checkWhichCardHasLowerValue (List<TrucoCard> cards, TrucoCard vira) {
+        return cards.stream()
+                .min(Comparator.comparingInt(carta -> carta.relativeValue(vira)))
+                .orElse(null);
+    }
+
+    private TrucoCard checkWhichCardHasHigherValue (List<TrucoCard> cards, TrucoCard vira) {
+        return cards.stream()
+                .max(Comparator.comparingInt(carta -> carta.relativeValue(vira)))
+                .orElse(null);
     }
 
     public Optional<TrucoCard> getWhichBotShouldPlayFirst () {
