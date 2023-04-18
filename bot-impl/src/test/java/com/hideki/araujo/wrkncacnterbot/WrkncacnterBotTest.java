1048bc1e889c635d29f1b14cbd92b931c7f6438e
Lucas Hideki <lucashideki87@gmail.com>
2023-04-18 11:18:42 -0300

    test(bot): arquivos de teste do bot

---
+++ bot-impl/src/test/java/com/hideki/araujo/wrkncacnterbot/WrkncacnterBotTest.java
@@ -0,0 +1,22 @@
+package com.hideki.araujo.wrkncacnterbot;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WrkncacnterBotTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void decideIfRaises() {
+    }
+}
