6f4da1e39505e8fa8126f110d0dc226bd191bab5
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-04-19 10:01:08 -0300

    test(Carlsen): Add test class for carlsen bot

---
+++ bot-impl/src/test/java/com/cremonezzi/impl/carlsenbot/CarlsenTest.java
@@ -0,0 +1,12 @@
+package com.cremonezzi.impl.carlsenbot;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CarlsenTest {
+    private Carlsen carlsenBot;
+
+    @BeforeEach
+    public void config() {
+        carlsenBot = new Carlsen();
+    }
+}
