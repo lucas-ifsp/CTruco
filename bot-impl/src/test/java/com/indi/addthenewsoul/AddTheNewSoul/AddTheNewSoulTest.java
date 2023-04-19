d543f1083ef76ad0cf4bb7a03a860c8a0f2b9452
IngridNery <neryingrid16@gmail.com>
2023-04-18 22:25:46 -0300

    test(bot): adds tests for specific hand conditions in method decideIfRaises

--- bot-impl/src/test/java/com/indi/addthenewsoul/AddTheNewSoul/AddTheNewSoulTest.java
+++ bot-impl/src/test/java/com/indi/addthenewsoul/AddTheNewSoul/AddTheNewSoulTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,7 @@ public class AddTheNewSoulTest {
             TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
             TrucoCard.of(CardRank.SIX, CardSuit.SPADES),
             TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES));
+    TrucoCard opponentCard = TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS);
     private GameIntel.StepBuilder stepBuilder;
     @BeforeEach
     public void setUp(){
@@ -181,4 +183,210 @@ public class AddTheNewSoulTest {
                 .opponentCard(TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS));
         assertEquals(CardSuit.DIAMONDS, addTheNewSoul.chooseCard(stepBuilder.build()).content().getSuit());
     }
+    @Test
+    @DisplayName("Should not rise if the opponent has more than 8 points")
+    public void shouldNotRiseIfTheOpponentHasMoreThan8pointsTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+        List<TrucoCard> openCards = Arrays.asList(
+                vira,
+                opponentCard);
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(9)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should rise if has good cards and opponent less than 8 points")
+    public void shouldRiseIfHasGoodCardsAndOpponentLessThab8pointsTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+        List<TrucoCard> openCards = Arrays.asList(
+                vira,
+                opponentCard);
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(8)
+                .opponentCard(opponentCard);
+        assertTrue(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+
+    @Test
+    @DisplayName("Should not rise if dont have good cards")
+    public void shouldNotRiseIfDontHaveGoodCardsTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
+                opponentCard);
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 3)
+                .opponentScore(3)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+
+    }
+
+    @Test
+    @DisplayName("Should raise if it has manilha and card with relative value greater than 4")
+    public void shouldRaiseIfItHasManilhaAndCardWithRelativeValueGraterThan4Test(){
+        TrucoCard vira = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+        List<TrucoCard> openCards = Arrays.asList(
+                vira,
+                opponentCard);
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(4)
+                .opponentCard(opponentCard);
+        assertTrue(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+
+    @Test
+    @DisplayName("Should not raise if only have manilha")
+    public void shouldNotRaiseIfOnlyHaveManilhaTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS));
+        List<TrucoCard> cards = Arrays.asList(
+                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.TWO, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.SIX, CardSuit.CLUBS));
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(cards, 7)
+                .opponentScore(8)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should not raise if have only card good")
+    public void shouldNotRaiseIfHaveOnlyCardGoodTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS));
+        List<TrucoCard> cards = Arrays.asList(
+                TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.SIX, CardSuit.CLUBS));
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(cards, 4)
+                .opponentScore(6)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should increase if sum of card values is above average")
+    public void shouldIncreaseIfSumOfCardValuesIsAboveAverageTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS));
+        List<TrucoCard> cards = Arrays.asList(
+                TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.SEVEN, CardSuit.CLUBS));
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(cards, 3)
+                .opponentScore(6)
+                .opponentCard(opponentCard);
+        assertTrue(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+
+    @Test
+    @DisplayName("Should increase have just manilhas")
+    public void shouldIncreaseHaveJustManilhasTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                opponentCard);
+        List<TrucoCard> botCards = Arrays.asList(
+                TrucoCard.of(CardRank.TWO, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.TWO, CardSuit.SPADES));
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 3)
+                .opponentScore(2)
+                .opponentCard(opponentCard);
+        assertTrue(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should increase if winning by more than 3 points independent of cards")
+    public void shouldIncreaseIfWinningByMoreThan3PointsIndependentOfCardsTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Collections.singletonList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS));
+
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 6)
+                .opponentScore(2);
+        assertTrue(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should not raise if have 11 points and opponent has 10")
+    public void shouldNotRaiseIfHave11PointsAndOpponentHas10Test() {
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                opponentCard);
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 11)
+                .opponentScore(10)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should not raise if all cards are below seven")
+    public void shouldNotRaiseIfAllCardsAreBelowSevenTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                opponentCard);
+
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 3)
+                .opponentScore(2)
+                .opponentCard(opponentCard);
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+    @Test
+    @DisplayName("Should not raise if opponent has a better card than the best card")
+    public void shouldNotRaiseIfOpponentHasABetterCardThanTheBestCardTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.TWO, CardSuit.DIAMONDS));
+
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 3)
+                .opponentScore(2)
+                .opponentCard(TrucoCard.of(CardRank.TWO, CardSuit.DIAMONDS));
+        assertFalse(addTheNewSoul.decideIfRaises(stepBuilder.build()));
+    }
+
+
 }
