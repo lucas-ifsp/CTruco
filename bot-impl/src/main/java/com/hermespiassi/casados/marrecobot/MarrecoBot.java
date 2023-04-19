029b7fee4e5e6acdaf330cb6ea1d09d8baa0e2a7
NathanHermes <hermesnathan8@gmail.com>
2023-04-19 16:51:16 -0300

    :sparkles: feat: Create logic to return pica-fumo in first round

--- bot-impl/src/main/java/com/hermespiassi/casados/marrecobot/MarrecoBot.java
+++ bot-impl/src/main/java/com/hermespiassi/casados/marrecobot/MarrecoBot.java
@@ -1,12 +1,20 @@
 package com.hermespiassi.casados.marrecobot;
 
+import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.bueno.spi.model.CardSuit.SPADES;
 
 public class MarrecoBot implements BotServiceProvider {
     @Override
     public int getRaiseResponse(GameIntel intel) {
+        System.out.println(intel.toString());
         return 0;
     }
 
@@ -22,6 +30,17 @@ public class MarrecoBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return CardToPlay.of(intel.getCards().get(0));
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        Optional<TrucoCard> opponentCard = intel.getOpponentCard();
+
+        List<TrucoCard> manilhas = cards.stream().filter(_card -> _card.isManilha(vira)).toList();
+        if (!manilhas.isEmpty()) {
+            Optional<TrucoCard> picaFumo = manilhas.stream().filter(_card -> _card.isOuros(vira)).findFirst();
+
+            if (picaFumo.isPresent()) return CardToPlay.of(picaFumo.get());
+        }
+
+        return CardToPlay.of(cards.get(0));
     }
 }
