2242e3137989255bd6eab0eb8c3ae031652c7253
Caue <cauegastaldi@outlook.com>
2023-04-19 13:37:43 -0300

    style (bot test): added copyright header in bot test class

--- bot-impl/src/test/java/com/caueisa/destroyerbot/DestroyerBotTest.java
+++ bot-impl/src/test/java/com/caueisa/destroyerbot/DestroyerBotTest.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2021 Lucas B. R. de Oliveira - IFSP/SCL
+ *  Contact: lucas <dot> oliveira <at> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package com.caueisa.destroyerbot;
 
 import com.bueno.spi.model.GameIntel;
@@ -6,13 +26,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @ExtendWith(MockitoExtension.class)
 class DestroyerBotTest {
-  @Mock
-  private GameIntel intel;
-  private final BotServiceProvider sut = new DestroyerBot();
+
+    @Mock
+    private GameIntel intel;
+    private final BotServiceProvider sut = new DestroyerBot();
 
 }
