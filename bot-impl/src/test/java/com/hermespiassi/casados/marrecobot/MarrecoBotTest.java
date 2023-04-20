6c27aa4eb253de995e12bc5b6cdce68aecf33bfd
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-04-20 13:21:06 -0300

    test: test: test for bot amarrar if not has card for win and have two three.

--- bot-impl/src/test/java/com/hermespiassi/casados/marrecobot/MarrecoBotTest.java
+++ bot-impl/src/test/java/com/hermespiassi/casados/marrecobot/MarrecoBotTest.java
@@ -395,8 +395,8 @@ class MarrecoBotTest {
     void ShouldReturnThreeIfHasTwoThree() {
         results = List.of();
         botCards = List.of(
-                TrucoCard.of(THREE, CLUBS),
                 TrucoCard.of(THREE, DIAMONDS),
+                TrucoCard.of(THREE, CLUBS),
                 TrucoCard.of(FOUR, HEARTS)
         );
         vira = TrucoCard.of(ACE, HEARTS);
@@ -409,5 +409,26 @@ class MarrecoBotTest {
 
         CardToPlay cardToPlay = new MarrecoBot().chooseCard(stepBuilder.build());
         assertThat(cardToPlay.value()).isEqualTo(TrucoCard.of(THREE, DIAMONDS));
+    }
+
+    @Test
+    @DisplayName("Should amarrar if not has card for win and has two three")
+    void ShouldAmarrarIfNotHasCardForWin () {
+        results = List.of();
+        botCards = List.of(
+                TrucoCard.of(THREE, CLUBS),
+                TrucoCard.of(THREE, DIAMONDS),
+                TrucoCard.of(FOUR, HEARTS)
+        );
+        vira = TrucoCard.of(ACE, HEARTS);
+        openCards = List.of(vira, TrucoCard.of(THREE, SPADES));
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(results, openCards, vira, 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0)
+                .opponentCard(TrucoCard.of(FOUR, SPADES));
+
+        CardToPlay cardToPlay = new MarrecoBot().chooseCard(stepBuilder.build());
+        assertThat(cardToPlay.value()).isEqualTo(TrucoCard.of(THREE, CLUBS));
     }
 }
