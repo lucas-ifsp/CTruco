94351cbece80d36ae484d5b68d58113326601026
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-04-20 12:03:03 -0300

    test: test for bot return the three if has two three.

--- bot-impl/src/test/java/com/hermespiassi/casados/marrecobot/MarrecoBotTest.java
+++ bot-impl/src/test/java/com/hermespiassi/casados/marrecobot/MarrecoBotTest.java
@@ -389,4 +389,25 @@ class MarrecoBotTest {
         CardToPlay cardToPlay = new MarrecoBot().chooseCard(stepBuilder.build());
         assertThat(cardToPlay.value()).isEqualTo(TrucoCard.of(FIVE, DIAMONDS));
     }
+
+    @Test
+    @DisplayName("Should return three if has two three")
+    void ShouldReturnThreeIfHasTwoThree() {
+        results = List.of();
+        botCards = List.of(
+                TrucoCard.of(THREE, CLUBS),
+                TrucoCard.of(THREE, DIAMONDS),
+                TrucoCard.of(FOUR, HEARTS)
+        );
+        vira = TrucoCard.of(ACE, HEARTS);
+        openCards = List.of(vira, TrucoCard.of(ACE, SPADES));
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(results, openCards, vira, 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0)
+                .opponentCard(TrucoCard.of(FOUR, SPADES));
+
+        CardToPlay cardToPlay = new MarrecoBot().chooseCard(stepBuilder.build());
+        assertThat(cardToPlay.value()).isEqualTo(TrucoCard.of(THREE, DIAMONDS));
+    }
 }
