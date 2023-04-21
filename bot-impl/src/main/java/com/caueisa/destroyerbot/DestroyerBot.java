a9bb916da634667eb273349d96412c039719f4a1
isarnf <isarnf@gmail.com>
2023-04-21 20:29:11 -0300

    feat (bot): bot should accept mao de onze if opponent score is greater than 7 and bot has all cards above king rank

--- bot-impl/src/main/java/com/caueisa/destroyerbot/DestroyerBot.java
+++ bot-impl/src/main/java/com/caueisa/destroyerbot/DestroyerBot.java
@@ -47,9 +47,10 @@ public class DestroyerBot implements BotServiceProvider {
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         if(intel.getOpponentScore() == 11)
             return true;
-        if(getCardsAboveRank(intel, CardRank.ACE).size() == 3 && getManilhas(intel).size() > 0 && intel.getOpponentScore() <= 8){
+        if(getCardsAboveRank(intel, CardRank.ACE).size() == 3 && getManilhas(intel).size() > 0 && intel.getOpponentScore() <= 8)
             return true;
-        }
+        if(intel.getOpponentScore() >= 8 && getCardsAboveRank(intel, CardRank.KING).size() == 3)
+            return true;
         return false;
     }
 
