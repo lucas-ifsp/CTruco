20cf3cc7e8b4d9e3a10b16071d062c8127f5a2c0
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-21 17:07:33 -0300

    feat(bot-impl): chooseCard method finished for second round

--- bot-impl/src/main/java/com/bonelli/noli/paulistabot/SecondRound.java
+++ bot-impl/src/main/java/com/bonelli/noli/paulistabot/SecondRound.java
@@ -1,5 +1,6 @@
 package com.bonelli.noli.paulistabot;
 
+import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
@@ -59,27 +60,66 @@ public class SecondRound implements Strategy {
             case WON -> {
                 if (hasZap(intel)) return CardToPlay.discard(cards.get(0));
                 else if (calculateCurrentHandValue(intel) >= 17) return CardToPlay.of(cards.get(0));
+                else {
+                    if (calculateCurrentHandValue(intel) >= 5 && calculateCurrentHandValue(intel) < 13) return CardToPlay.of(cards.get(1));
+                    return CardToPlay.of(cards.get(0));
+                }
+            }
+            case LOST, DREW -> {
+                if (intel.getOpponentCard().isPresent()) {
+                    TrucoCard opponentCard = intel.getOpponentCard().get();
+                    if (opponentCard.getRank() != CardRank.HIDDEN) {
+                        int countCardsHigherOfOpponent = getCountCardsAreHigherOfOpponent(cards, opponentCard, vira);
+                        if (countCardsHigherOfOpponent == 0) {
+                            if (cardWithSameValueOfOpponent(cards, opponentCard)) return CardToPlay.of(getCardWithSameValueOfOpponent(cards, opponentCard)); //Corre risco de amarrar dessa forma
+                            return CardToPlay.discard(cards.get(0));
+                        }
+                        else if (countCardsHigherOfOpponent == 1) return CardToPlay.of(cards.get(1));
+                        else return CardToPlay.of(chooseCardThatBeatsTheOpponent(cards, opponentCard, vira));
+                    } else return CardToPlay.of(cards.get(0));
+                } else return CardToPlay.of(cards.get(1));
+            }
+            default -> {
+                return CardToPlay.of(cards.get(0));
             }
         }
+    }
 
-        return null;
+    public TrucoCard getCardWithSameValueOfOpponent(List<TrucoCard> cards, TrucoCard opponentCard) {
+        return cards.stream().filter(card -> card.getRank().value() == opponentCard.getRank().value()).findFirst().orElseThrow();
+    }
+
+    @Override
+    public boolean hasManilha(GameIntel intel) {
+        return intel.getCards().stream().anyMatch(card -> card.isManilha(intel.getVira()));
+    }
+
+    public boolean cardWithSameValueOfOpponent(List<TrucoCard> cards, TrucoCard opponentCard) {
+        return cards.stream().anyMatch(card -> card.getRank().value() == opponentCard.getRank().value());
     }
 
     private boolean hasZap(GameIntel intel) {
         return intel.getCards().stream().anyMatch(card -> card.isZap(intel.getVira()));
     }
 
-    @Override
-    public boolean hasManilha(GameIntel intel) {
-        return false;
-    }
-
-    private boolean hasTwoOrThree(GameIntel intel) {
-        return intel.getCards().stream().anyMatch(card -> card.isManilha(intel.getVira()));
-    }
-
-    private int calculateCurrentHandValue(GameIntel intel) {
+    public int calculateCurrentHandValue(GameIntel intel) {
         if (intel.getCards().isEmpty()) return 0;
         return intel.getCards().stream().map(card -> card.relativeValue(intel.getVira())).reduce(Integer::sum).orElseThrow();
+    }
+
+    public int getCountCardsAreHigherOfOpponent(List<TrucoCard> cards, TrucoCard card, TrucoCard vira) {
+        int count = 0;
+        for (TrucoCard trucoCard : cards) {
+            if (trucoCard.compareValueTo(card, vira) > 0)
+                count++;
+        }
+        return count;
+    }
+
+    public TrucoCard chooseCardThatBeatsTheOpponent(List<TrucoCard> cardList, TrucoCard vira, TrucoCard opponentCard) {
+        return cardList.stream()
+                .filter(carta -> carta.relativeValue(vira) > opponentCard.relativeValue(vira))
+                .min(Comparator.comparingInt(carta -> carta.relativeValue(vira)))
+                .orElseThrow();
     }
 }
