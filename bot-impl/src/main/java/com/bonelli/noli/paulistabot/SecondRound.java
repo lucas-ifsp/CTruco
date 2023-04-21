29f2c973305c2ba5f79236d811c77ede2d7bf2ec
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-20 22:07:01 -0300

    feat(bot-impl): getMaoDeOnzeResponse method completed for second round

--- bot-impl/src/main/java/com/bonelli/noli/paulistabot/SecondRound.java
+++ bot-impl/src/main/java/com/bonelli/noli/paulistabot/SecondRound.java
@@ -5,6 +5,7 @@ import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class SecondRound implements Strategy {
@@ -19,7 +20,18 @@ public class SecondRound implements Strategy {
 
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
-        return false;
+        List<TrucoCard> cards = new ArrayList<>(intel.getCards());
+        cards.sort((c1, c2) -> {
+            return c1.compareValueTo(c2, intel.getVira());
+        });
+
+        TrucoCard cardMedium = cards.get(0);
+
+        if (calculateCurrentHandValue(intel) >= 25) {
+            if (hasManilha(intel)) return true;
+            else if (intel.getOpponentScore() < 9 && hasTwoOrThree(intel)) return true;
+            else return intel.getOpponentScore() >= 9 && cardMedium.getRank().value() >= 9;
+        } else return false;
     }
 
     @Override
@@ -38,5 +50,14 @@ public class SecondRound implements Strategy {
     @Override
     public boolean hasManilha(GameIntel intel) {
         return false;
+    }
+
+    private boolean hasTwoOrThree(GameIntel intel) {
+        return intel.getCards().stream().anyMatch(card -> card.isManilha(intel.getVira()));
+    }
+
+    private int calculateCurrentHandValue(GameIntel intel) {
+        if (intel.getCards().isEmpty()) return 0;
+        return intel.getCards().stream().map(card -> card.relativeValue(intel.getVira())).reduce(Integer::sum).orElseThrow();
     }
 }
