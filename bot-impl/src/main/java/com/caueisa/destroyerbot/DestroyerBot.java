f917b1e99d809130900fca39ddabe3dab52d2297
isarnf <isarnf@gmail.com>
2023-04-23 15:16:57 -0300

    feat (bot): bot should ask for point raise if hand points equals to six and opponent has six or more points.

--- bot-impl/src/main/java/com/caueisa/destroyerbot/DestroyerBot.java
+++ bot-impl/src/main/java/com/caueisa/destroyerbot/DestroyerBot.java
@@ -33,6 +33,8 @@ public class DestroyerBot implements BotServiceProvider {
         int scoreDifference = getScoreDifference(intel);
         if (intel.getHandPoints() == 3 && intel.getOpponentScore() >= 9)
             return 1;
+        if (intel.getHandPoints() == 6 && intel.getOpponentScore() >= 6)
+            return 1;
         if (scoreDifference > 0) {
             if (scoreDifference <= 3 && getCardsAboveRank(intel, CardRank.SEVEN).size() == intel.getCards().size())
                 return 0;
