1a4ea73e88626a0537c6667dc802090a3489aa49
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-04-23 18:18:32 -0300

    test(Carlsen): Add test to check if it will raise again if opponent called it before playing a card if it have a good hand without manilha

--- bot-impl/src/test/java/com/cremonezzi/impl/carlsenbot/CarlsenTest.java
+++ bot-impl/src/test/java/com/cremonezzi/impl/carlsenbot/CarlsenTest.java
@@ -908,4 +908,29 @@ public class CarlsenTest {
 
         assertThat(carlsenBot.getRaiseResponse(intel)).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("Should raise again if opponent called it before playing a card and have good hand without manilha")
+    public void ShouldRaiseBeforeOpponentPlayWithMediumHand(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.SPADES);
+
+        //Game info
+        List<TrucoCard> openCards = List.of(vira);
+        List<GameIntel.RoundResult> roundResults = List.of();
+
+        //Bot info
+        List<TrucoCard> botCards = List.of(
+                TrucoCard.of(CardRank.KING, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                TrucoCard.of(CardRank.ACE, CardSuit.HEARTS)
+        );
+
+        GameIntel intel = GameIntel.StepBuilder.with()
+                .gameInfo(roundResults, openCards, vira, 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0)
+                .build();
+
+        assertThat(carlsenBot.getRaiseResponse(intel)).isEqualTo(1);
+    }
 }
