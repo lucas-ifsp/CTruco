7dab751673696244a0e36d0570b5f455bd767acd
alinirv <alini.vicente@yahoo.com.br>
2023-09-30 13:19:35 -0300

    feat(TecoNoMarreco):implements BotServiceProvider interface in TecoNoMarreco class.

--- bot-impl/src/main/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBot.java
+++ bot-impl/src/main/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBot.java
@@ -1,4 +1,28 @@
 package com.murilos.aline.teconomarrecobot;
 
-public class TecoNoMarrecoBot {
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class TecoNoMarrecoBot implements BotServiceProvider {
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
 }
