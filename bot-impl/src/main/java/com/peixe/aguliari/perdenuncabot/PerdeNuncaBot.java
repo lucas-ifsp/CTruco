44ad6dbd6a9307ea8b743b5643675f59876e1b7a
RamonPeixe <r-amoncp@hotmail.com>
2023-10-02 22:22:27 -0300

    feat(bot-impl): create PerdeNuncaBot class without implemetantions

---
+++ bot-impl/src/main/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBot.java
@@ -0,0 +1,27 @@
+package com.peixe.aguliari.perdenuncabot;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class PerdeNuncaBot implements BotServiceProvider{
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+}
