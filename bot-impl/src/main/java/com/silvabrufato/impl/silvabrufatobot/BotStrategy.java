6e9863bc980bacf76ce8a3c22245b41fb944929c
ReBrufato <brufato17.renan@gmail.com>
2023-10-06 00:07:48 -0300

    feat: add logic to the FIRST_HAND_STRATEGY enum to pass the shouldTryToWinTheFirstHandWithoutUsingManilha test

--- bot-impl/src/main/java/com/silvabrufato/impl/silvabrufatobot/BotStrategy.java
+++ bot-impl/src/main/java/com/silvabrufato/impl/silvabrufatobot/BotStrategy.java
@@ -2,12 +2,36 @@ package com.silvabrufato.impl.silvabrufatobot;
 
 import com.bueno.spi.model.*;
 
+import java.util.List;
+
 public enum BotStrategy {
 
     FIRST_HAND_STRATEGY {
         @Override
         public CardToPlay throwCard(GameIntel gameIntel) {
-            return CardToPlay.of(TrucoCard.of(CardRank.KING, CardSuit.CLUBS));
+            List<TrucoCard> cards = gameIntel.getCards();
+            TrucoCard vira = gameIntel.getVira();
+            TrucoCard manilha = null;
+            TrucoCard cardOne = null;
+            TrucoCard cardTwo = null;
+
+            for (TrucoCard card : cards) {
+                if (card.isManilha(vira)) manilha = card;
+            }
+
+            for (TrucoCard card : cards) {
+                if (!card.isManilha(vira)){
+                    cardOne = card;
+                }
+            }
+
+            for (TrucoCard card : cards) {
+                if (!card.isManilha(vira) && card != cardOne){
+                    cardTwo = card;
+                }
+            }
+
+            return (cardOne.compareValueTo(cardTwo, vira) >= 0) ? CardToPlay.of(cardOne) : CardToPlay.of(cardTwo);
         }
     },
     
