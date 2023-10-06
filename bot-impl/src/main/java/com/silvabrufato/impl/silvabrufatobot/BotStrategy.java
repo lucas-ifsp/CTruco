5284ad4e2d604884909212ede527f2f1530d3eab
ReBrufato <brufato17.renan@gmail.com>
2023-10-06 01:00:43 -0300

    feat: add logic to the FIRST_HAND_STRATEGY enum to pass the shouldWinTheSecondHandIfPossible test

--- bot-impl/src/main/java/com/silvabrufato/impl/silvabrufatobot/BotStrategy.java
+++ bot-impl/src/main/java/com/silvabrufato/impl/silvabrufatobot/BotStrategy.java
@@ -12,25 +12,46 @@ public enum BotStrategy {
             List<TrucoCard> cards = gameIntel.getCards();
             TrucoCard vira = gameIntel.getVira();
             TrucoCard manilha = null;
-            TrucoCard cardOne = null;
             TrucoCard cardTwo = null;
+            TrucoCard cardThree = null;
 
+            //verified if exists manilha
             for (TrucoCard card : cards) {
                 if (card.isManilha(vira)) manilha = card;
             }
 
-            for (TrucoCard card : cards) {
-                if (!card.isManilha(vira)){
-                    cardOne = card;
+            //if not exists manilha verified higest card
+            if (manilha == null) {
+                //compare 1° with 2°
+                if (cards.get(0).compareValueTo(cards.get(1), vira) >=0 ){
+                    //compare 1° with 3°
+                    if (cards.get(0).compareValueTo(cards.get(2), vira) >= 0){
+                        return CardToPlay.of(cards.get(0));
+                    }else{
+                        return CardToPlay.of(cards.get(2));
+                    }
+                }else {
+                    //compare 2° with 3°
+                    if (cards.get(1).compareValueTo(cards.get(2), vira) >= 0){
+                        return CardToPlay.of(cards.get(1));
+                    }else{
+                        return CardToPlay.of(cards.get(2));
+                    }
                 }
             }
 
             for (TrucoCard card : cards) {
-                if (!card.isManilha(vira) && card != cardOne){
+                if (!card.isManilha(vira)){
                     cardTwo = card;
                 }
             }
-            return (cardOne.compareValueTo(cardTwo, vira) >= 0) ? CardToPlay.of(cardOne) : CardToPlay.of(cardTwo);
+
+            for (TrucoCard card : cards) {
+                if (!card.isManilha(vira) && card != cardTwo){
+                    cardThree = card;
+                }
+            }
+            return (cardTwo.compareValueTo(cardThree, vira) >= 0) ? CardToPlay.of(cardTwo) : CardToPlay.of(cardThree);
         }
     },
     
