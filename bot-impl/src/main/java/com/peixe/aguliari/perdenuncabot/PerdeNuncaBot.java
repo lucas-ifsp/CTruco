af15d13edc0f851d789e18b6bedbead7b918f46b
Eduardo0608 <eduardoaguliari0608@gmail.com>
2023-10-09 15:22:53 -0300

    feat(bot-impl): implementing new methods on PerdeNuncabot

--- bot-impl/src/main/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBot.java
+++ bot-impl/src/main/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBot.java
@@ -22,9 +22,12 @@ package com.peixe.aguliari.perdenuncabot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
 
-public class PerdeNuncaBot implements BotServiceProvider{
+import java.util.Optional;
+
+public class PerdeNuncaBot implements BotServiceProvider {
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
@@ -37,7 +40,62 @@ public class PerdeNuncaBot implements BotServiceProvider{
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        // Choose the card with the lowest relative value to the opponent's card.
+        TrucoCard smallestCardThatCanWin = chooseSmallestCardThatCanWin(intel);
+
+        // If there is no card that can win, choose the card with the lowest relative value to the vira card.
+        if (smallestCardThatCanWin == null) {
+            smallestCardThatCanWin = getSmallestCardInHand(intel);
+        }
+
+        // Return the chosen card.
+        return CardToPlay.of(smallestCardThatCanWin);
+    }
+
+    private TrucoCard chooseSmallestCardThatCanWin(GameIntel intel) {
+        // Get the opponent's card, if any.
+        Optional<TrucoCard> opponentCard = intel.getOpponentCard();
+
+        // If there is no opponent's card, return null.
+        if (opponentCard.isEmpty()) {
+            return null;
+        }
+
+        // Get the vira card.
+        TrucoCard vira = intel.getVira();
+
+        // Initialize the card with the lowest relative value to the vira card.
+        TrucoCard smallestCardThatCanWin = null;
+
+        // Iterate over all the cards in the player's hand.
+        for (TrucoCard card : intel.getCards()) {
+            // If the current card is greater than the opponent's card, update the card with the lowest relative value to the vira card.
+            if (card.relativeValue(vira) > opponentCard.get().relativeValue(vira)) {
+                smallestCardThatCanWin = card;
+            }
+        }
+
+        // Return the card with the lowest relative value to the vira card.
+        return smallestCardThatCanWin;
+    }
+
+    private TrucoCard getSmallestCardInHand(GameIntel intel) {
+        // Initialize the card with the lowest relative value to the vira card.
+        TrucoCard smallestCard = null;
+
+        // Get the vira card.
+        TrucoCard vira = intel.getVira();
+
+        // Iterate over all the cards in the player's hand.
+        for (TrucoCard card : intel.getCards()) {
+            // If the current card is less than the card with the lowest relative value to the vira card, update the card with the lowest relative value to the vira card.
+            if (smallestCard == null || card.relativeValue(vira) < smallestCard.relativeValue(vira)) {
+                smallestCard = card;
+            }
+        }
+
+        // Return the card with the lowest relative value to the vira card.
+        return smallestCard;
     }
 
     @Override
