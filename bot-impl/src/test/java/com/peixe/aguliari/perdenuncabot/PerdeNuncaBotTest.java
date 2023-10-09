5449638999fc77cc24702cb288ba41c380d24b11
Eduardo0608 <eduardoaguliari0608@gmail.com>
2023-10-09 15:23:58 -0300

    test(bot-impl): add the first wave of tests

--- bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
+++ bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
@@ -20,5 +20,149 @@
  */
 package com.peixe.aguliari.perdenuncabot;
 
+import com.bueno.spi.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PerdeNuncaBotTest {
+    private PerdeNuncaBot perdeNuncaBot;
+
+    private final List<TrucoCard> botCards = Arrays.asList(
+            TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
+            TrucoCard.of(CardRank.SIX, CardSuit.SPADES),
+            TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES));
+
+    @BeforeEach
+    public void setUp() {
+        perdeNuncaBot = new PerdeNuncaBot();
+    }
+
+    @Test
+    @DisplayName("Chooses the smallest card when no cards have been played")
+    void choosesTheSmallestCardWhenNoCardsHaveBeenPlayed() {
+        // Arrange
+        TrucoCard vira = TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS);
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), List.of(vira), vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(4);
+
+        // Act
+        CardToPlay card = perdeNuncaBot.chooseCard(stepBuilder.build());
+
+        // Assert
+        assertEquals(CardRank.FIVE, card.content().getRank());
+    }
+
+    @Test
+    @DisplayName("Bot discards lowest rank card when impossible to win.")
+    public void testBotDiscardsLowestRankCardWhenImpossibleToWin() {
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+        TrucoCard opponentCard = TrucoCard.of(CardRank.TWO, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.TWO, CardSuit.DIAMONDS));
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.DREW), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(3)
+                .opponentCard(opponentCard);
+
+        CardToPlay botCard = perdeNuncaBot.chooseCard(stepBuilder.build());
+
+        assertEquals(CardRank.FIVE, botCard.content().getRank());
+    }
+
+    @Test
+    @DisplayName("Should play the lowest manilha that beats the opponent, if possible")
+    public void shouldPlayTheLowestManilhaThatBeatsTheOpponentIfPossible() {
+        // Arrange
+        TrucoCard vira = TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS);
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS));
+        List<TrucoCard> botCards = Arrays.asList(
+                TrucoCard.of(CardRank.SIX, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.SIX, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.ACE, CardSuit.CLUBS));
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(4)
+                .opponentCard(TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS));
+
+        CardToPlay botCard = perdeNuncaBot.chooseCard(stepBuilder.build());
+
+        assertEquals(CardSuit.HEARTS, botCard.content().getSuit());
+    }
+
+    @Test
+    @DisplayName("Should play the lowest card according to the suit if the bot has no manilhas")
+    public void shouldPlayTheLowestCardAccordingToTheSuitIfTheBotHasNoManilhas() {
+        // Arrange
+        TrucoCard vira = TrucoCard.of(CardRank.FOUR, CardSuit.SPADES);
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.FOUR, CardSuit.SPADES),
+                TrucoCard.closed());
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(4)
+                .opponentCard(TrucoCard.closed());
+
+        CardToPlay botCard = perdeNuncaBot.chooseCard(stepBuilder.build());
+
+        assertEquals(CardSuit.SPADES, botCard.content().getSuit());
+    }
+
+    @Test
+    @DisplayName("Should play a manilha stronger than the player's if the bot has manilhas")
+    public void shouldPlayManilhaStrongerThanThePlayersIfTheBotHasManilhas() {
+        // Arrange
+        TrucoCard vira = TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS);
+        TrucoCard opponentCard = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS));
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(4)
+                .opponentCard(opponentCard);
+
+        CardToPlay botCard = perdeNuncaBot.chooseCard(stepBuilder.build());
+
+        assertEquals(CardRank.SIX, botCard.content().getRank());
+    }
+
+    @Test
+    @DisplayName("Should play the card with the lowest rank to win")
+    public void shouldPlayTheCardWithTheLowestRankToWinTest() {
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+        TrucoCard opponentCard = TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS);
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS));
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.DREW), openCards, vira, 1)
+                .botInfo(botCards, 9)
+                .opponentScore(3)
+                .opponentCard(opponentCard);
+
+        assertEquals(CardRank.FIVE, perdeNuncaBot.chooseCard(stepBuilder.build()).content().getRank());
+    }
 }
