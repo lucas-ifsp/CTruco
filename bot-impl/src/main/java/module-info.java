bc57fc8304896071468e157f911370a1edadd5ab
talitha <mlithhs@gmail.com>
2023-10-09 21:05:54 -0300

    feat(bot-impl): add leonardabot implementation

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -5,6 +5,7 @@ import com.cremonezzi.impl.carlsenbot.Carlsen;
 import com.hermespiassi.casados.marrecobot.MarrecoBot;
 import com.hideki.araujo.wrkncacnterbot.WrkncacnterBot;
 import com.indi.impl.addthenewsoul.AddTheNewSoul;
+import com.tatayrapha.leonardabot.LeonardaBot;
 
 module bot.impl {
     requires bot.spi;
@@ -15,6 +16,7 @@ module bot.impl {
     exports com.caueisa.destroyerbot;
     exports com.bonelli.noli.paulistabot;
     exports com.hideki.araujo.wrkncacnterbot;
-    provides com.bueno.spi.service.BotServiceProvider with
+    exports com.tatayrapha.leonardabot;
+    provides com.bueno.spi.service.BotServiceProvider with LeonardaBot,
             DummyBot, Carlsen, DestroyerBot, WrkncacnterBot, PaulistaBot, MarrecoBot, AddTheNewSoul;
 }
---
+++ bot-impl/src/test/java/com/tatayrapha/leonardabot/LeonardaBotTest.java
@@ -0,0 +1,28 @@
+package com.tatayrapha.leonardabot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeonardaBotTest {
+
+    @Test
+    void getMaoDeOnzeResponse() {
+    }
+
+    @Test
+    void decideIfRaises() {
+    }
+
+    @Test
+    void chooseCard() {
+    }
+
+    @Test
+    void getRaiseResponse() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
