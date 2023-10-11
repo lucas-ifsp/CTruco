3f0c5b5858ef8d8b2136c0635a60070c713be0a6
kpi-jps <jps.spj@gmail.com>
2023-10-10 23:18:41 -0300

    test: created the ShouldWinTheFirstRoundIfPossibleUsingTheMiddleCard unit test method

--- bot-impl/src/test/java/com/silvabrufato/impl/silvabrufatobot/SilvaBrufatoBotTest.java
+++ bot-impl/src/test/java/com/silvabrufato/impl/silvabrufatobot/SilvaBrufatoBotTest.java
@@ -93,6 +93,25 @@ public class SilvaBrufatoBotTest {
         }
 
         @Test
+        @DisplayName("Should win the first round if possible using the middle card")
+        public void ShouldWinTheFirstRoundIfPossibleUsingTheMiddleCard() {
+            when(gameIntel.getVira()).thenReturn(TrucoCard.of(CardRank.ACE, CardSuit.CLUBS));
+            when(gameIntel.getRoundResults()).thenReturn(List.of());
+            when(gameIntel.getOpponentCard()).thenReturn(Optional.of(
+                    TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS))
+            );
+            when(gameIntel.getCards()).thenReturn(List.of(
+                    TrucoCard.of(CardRank.TWO, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.QUEEN, CardSuit.DIAMONDS)
+            ));
+            
+            assertThat(sut.chooseCard(gameIntel).content()).isEqualTo(
+                TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS)
+            );
+        }
+
+        @Test
         @DisplayName("Should win the first round if possible using a manilha")
         public void ShouldWinTheFirstRoundIfPossibleUsingAManilha() {
             when(gameIntel.getVira()).thenReturn(TrucoCard.of(CardRank.ACE, CardSuit.CLUBS));
