0b0e99e3bbf4cebc713a94390cb35ad734704737
MuriloSdaSilva <murilor72003@outlook.com>
2023-10-12 14:32:14 -0300

    test(TecoNoMarreco): add tests Whether He Accepts Ahand From Eleven With A Casal Maior

--- bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
+++ bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
@@ -1,7 +1,46 @@
 package com.murilos.aline.teconomarrecobot;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+;
+import static com.bueno.spi.model.CardSuit.*;
+import static com.bueno.spi.model.CardRank.*;
+import static com.bueno.spi.model.CardSuit.*;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TecoNoMarrecoBotTest {
 
+    TecoNoMarrecoBot tecoNoMarrecoBot;
+
+    private List<TrucoCard> cards;
+
+    private TrucoCard cardVira;
+
+    private List<TrucoCard> hand;
+    private List<GameIntel.RoundResult> roundResult;
+
+    private GameIntel.StepBuilder stepBuilder;
+
+    @Test
+    @DisplayName("Testa se aceita mão de onze com casal maior")
+    void testsWhetherHeAcceptsAhandFromElevenWithABiggerCouple(){
+        hand = List.of(TrucoCard.of(FOUR, CLUBS), TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, SPADES));
+        cardVira = TrucoCard.of(THREE, SPADES);
+        roundResult = List.of();
+        cards = List.of();
+
+        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(0);
+
+        Boolean acceptMaoDeOnze = new TecoNoMarrecoBot().getMaoDeOnzeResponse(stepBuilder.build());
+        assertTrue(acceptMaoDeOnze);
+    }
 }
