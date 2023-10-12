746fd5fbcd30b98bc171daa9588c5bdb1b7504ab
MuriloSdaSilva <murilor72003@outlook.com>
2023-10-12 14:40:06 -0300

    feat(TecoNoMarreco): create method possuiCasalMaior

--- bot-impl/src/main/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBot.java
+++ bot-impl/src/main/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBot.java
@@ -2,12 +2,37 @@ package com.murilos.aline.teconomarrecobot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.List;
 
 public class TecoNoMarrecoBot implements BotServiceProvider {
 
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        TrucoCard cardVira = intel.getVira();
+        List<TrucoCard> cards = intel.getCards();
+        if(possuiCasalMaior(intel)){
+            return true;
+        }
+
+        return false;
+
+    }
+    public boolean possuiCasalMaior(GameIntel intel) {
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard cardVira = intel.getVira();
+
+        for(int i = 0; i < 3; i++){
+            if (cards.get(i).isZap(cardVira)) {
+                for (int k = 0; k < 3; k++) {
+                    if (cards.get(k).isCopas(cardVira)) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
@@ -22,7 +47,7 @@ public class TecoNoMarrecoBot implements BotServiceProvider {
     }
 
     @Override
-    public int getRaiseResponse(GameIntel intel) {
-        return 0;
-    }
+    public int getRaiseResponse(GameIntel intel) {return 0;}
+
+
 }
