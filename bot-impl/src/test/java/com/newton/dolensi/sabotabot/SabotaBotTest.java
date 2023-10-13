8acf08c7648eccf163ff5cca393b6ada972bc9b4
isaacnwt <isaacnewton02@hotmail.com>
2023-10-13 13:58:08 -0300

    refactor(bot-impl): organizing test nested classes

--- bot-impl/src/test/java/com/newton/dolensi/sabotabot/SabotaBotTest.java
+++ bot-impl/src/test/java/com/newton/dolensi/sabotabot/SabotaBotTest.java
@@ -5,7 +5,6 @@ import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -36,27 +35,47 @@ public class SabotaBotTest {
             var cards = CardsMock.cardList();
             when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
             when(intel.getCards()).thenReturn(cards);
+
             assertNotNull(sut.chooseCard(intel).content());
         }
 
-        @Test
-        @DisplayName("Should play a strong card if is first to play")
-        void shouldPlayAStrongCardIfIsFirstToPlay(){
-            var cards = CardsMock.cardList();
-            when(intel.getOpponentCard()).thenReturn(Optional.empty());
-            when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
-            when(intel.getCards()).thenReturn(cards);
-            assertEquals(sut.chooseCard(intel).content(), cards.get(2));
+        @Nested
+        @DisplayName("First Round Plays")
+        class FirstRoundTests{
+
+            @Test
+            @DisplayName("Should play a strong card if is first to play")
+            void shouldPlayAStrongCardIfIsFirstToPlay(){
+                var cards = CardsMock.cardList();
+                when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
+                when(intel.getCards()).thenReturn(cards);
+
+                when(intel.getOpponentCard()).thenReturn(Optional.empty());
+                assertEquals(sut.chooseCard(intel).content(), cards.get(2));
+            }
+
+            @Test
+            @DisplayName("Should play the weakest card if other player plays a 3")
+            void shouldPlayTheWeakestCardIfOtherPlayerPlaysA3(){
+                var cards = CardsMock.cardList();
+                when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
+                when(intel.getCards()).thenReturn(cards);
+
+                when(intel.getOpponentCard()).thenReturn(Optional.of(TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)));
+                assertEquals(sut.chooseCard(intel).content(), cards.get(1));
+            }
         }
 
-        @Test
-        @DisplayName("Should play the weakest card if other player plays a 3")
-        void shouldPlayTheWeakestCardIfOtherPlayerPlaysA3(){
-            var cards = CardsMock.cardList();
-            when(intel.getOpponentCard()).thenReturn(Optional.of(TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)));
-            when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
-            when(intel.getCards()).thenReturn(cards);
-            assertEquals(sut.chooseCard(intel).content(), cards.get(1));
+        @Nested
+        @DisplayName("Second Round Plays")
+        class SecondRoundTests{
+
+        }
+
+        @Nested
+        @DisplayName("Third Round Plays")
+        class ThirdRoundTests{
+
         }
 
     }
