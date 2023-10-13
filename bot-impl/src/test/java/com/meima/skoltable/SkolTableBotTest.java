09c2d96802ce5ea01787b9885f09ccd48bda0a34
Mateus Vieira <mateus.sly.mp@gmail.com>
2023-10-13 14:48:22 -0300

    test: add test for bot play strongest card in first round if it's stronger than opponent's

--- bot-impl/src/test/java/com/meima/skoltable/SkolTableBotTest.java
+++ bot-impl/src/test/java/com/meima/skoltable/SkolTableBotTest.java
@@ -1,5 +1,46 @@
 package com.meima.skoltable;
 
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.bueno.spi.model.CardRank.*;
+import static com.bueno.spi.model.CardSuit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SkolTableBotTest {
+
+    private SkolTable skolTable;
+
+    @BeforeEach
+    public void setUp(){
+        skolTable = new SkolTable();
+    }
+
+
+    @Test
+    @DisplayName("Should play strongest card in first round if it's stronger than opponent's")
+    void shouldPlayStrongestCardInFirstRoundIfStrongerThanOpponents() {
+
+        List<TrucoCard> botCards = List.of(TrucoCard.of(ACE, SPADES), TrucoCard.of(THREE, CLUBS), TrucoCard.of(FOUR, HEARTS));
+        TrucoCard vira = TrucoCard.of(THREE, HEARTS);
+        TrucoCard opponentCard = TrucoCard.of(ACE, CLUBS);
+
+        List<TrucoCard> openCards = List.of(vira);
+        List<GameIntel.RoundResult> results = List.of();
+
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(results, openCards, vira, 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0)
+                .opponentCard(opponentCard);
+
+
+        assertEquals(TrucoCard.of(FOUR, HEARTS), skolTable.chooseCard(stepBuilder.build()).content());
+    }
 
 }
