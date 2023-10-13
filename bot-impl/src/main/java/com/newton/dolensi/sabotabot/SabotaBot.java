27256db75434b6f38e6990e1cf9175dc7e3ba135
isaacnwt <isaacnewton02@hotmail.com>
2023-10-13 01:29:46 -0300

    feat(bot-impl): returning the greatest card on hand if is first player to play

--- bot-impl/src/main/java/com/newton/dolensi/sabotabot/SabotaBot.java
+++ bot-impl/src/main/java/com/newton/dolensi/sabotabot/SabotaBot.java
@@ -3,7 +3,10 @@ package com.newton.dolensi.sabotabot;
 import com.bueno.spi.model.*;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.List;
+
 public class SabotaBot implements BotServiceProvider {
+
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
@@ -17,26 +20,27 @@ public class SabotaBot implements BotServiceProvider {
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
         var hand = intel.getCards();
-        TrucoCard greatestCard;
 
-        if (intel.getOpponentCard().isEmpty()) {
-            if (hand.get(0).compareValueTo(hand.get(1), intel.getVira()) > 0) {
-                if (hand.get(0).compareValueTo(hand.get(2), intel.getVira()) > 0)
-                    greatestCard = hand.get(0);
-                else
-                    greatestCard = hand.get(2);
-            } else if (hand.get(1).compareValueTo(hand.get(2), intel.getVira()) > 0)
-                greatestCard = hand.get(1);
-            else
-                greatestCard = hand.get(2);
-        } else
-            greatestCard = hand.get(0);
-
-        return CardToPlay.of(greatestCard);
+        return CardToPlay.of(getGreatestCard(intel, hand));
     }
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
         return 0;
+    }
+
+    private TrucoCard getGreatestCard(GameIntel intel, List<TrucoCard> hand) {
+        if (intel.getOpponentCard().isEmpty()) {
+            if (hand.get(0).compareValueTo(hand.get(1), intel.getVira()) > 0) {
+                if (hand.get(0).compareValueTo(hand.get(2), intel.getVira()) > 0)
+                    return hand.get(0);
+                else
+                    return hand.get(2);
+            } else if (hand.get(1).compareValueTo(hand.get(2), intel.getVira()) > 0)
+                return hand.get(1);
+            else
+                return hand.get(2);
+        } else
+            return hand.get(0);
     }
 }
