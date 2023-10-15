94440165822b4684941f3263ba8081d6caac731a
GattiGabriel <ggattidev@gmail.com>
2023-10-15 03:48:11 -0300

    test(bot): create test class CaipirasBotTest

---
+++ bot-impl/src/test/java/com/gatti/casaque/caipirasbot/CaipirasBotTest.java
@@ -0,0 +1,14 @@
+package com.gatti.casaque.caipirasbot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CaipirasBotTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void decideIfRaises() {
+    }
+}
