6aa36e4c410d500b011bde7407a410f47ac4e1fb
MuriloSdaSilva <murilor72003@outlook.com>
2023-10-15 17:09:47 -0300

    refactor(TecoNoMarreco): refactor grouping tests

--- bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
+++ bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
@@ -33,139 +33,152 @@ class TecoNoMarrecoBotTest {
     @BeforeEach
     void setUp(){ tecoNoMarrecoBot = new TecoNoMarrecoBot();}
 
-    @Test
-    @DisplayName("Testa se aceita mão de onze com casal maior")
-    void testsWhetherHeAcceptsAhandFromElevenWithABiggerCouple(){
-        hand = List.of(TrucoCard.of(FOUR, CLUBS), TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, SPADES));
-        cardVira = TrucoCard.of(THREE, SPADES);
-        roundResult = List.of();
-        cards = List.of();
 
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(0);
+    @Nested
+    @DisplayName("getMaoDeOnzeResponse")
+    class GetMaoDeOnzeResponse{
+        @Test
+        @DisplayName("Testa se aceita mão de onze com casal maior")
+        void testsWhetherHeAcceptsAhandFromElevenWithABiggerCouple(){
+            hand = List.of(TrucoCard.of(FOUR, CLUBS), TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, SPADES));
+            cardVira = TrucoCard.of(THREE, SPADES);
+            roundResult = List.of();
+            cards = List.of();
 
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(0);
+
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mão de onze com mão completa de três")
+        void shouldAcceptHandOfElevenWithThreeCardsThree(){
+            hand = List.of(TrucoCard.of(THREE, CLUBS), TrucoCard.of(THREE, HEARTS), TrucoCard.of(THREE, SPADES));
+            cardVira = TrucoCard.of(ACE, DIAMONDS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(0);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mao de onze com pontuacao do oponente menor que 4")
+        void shouldAcceptHandOfElevenWithOpponentScoreLessThanFour(){
+            hand = List.of(TrucoCard.of(TWO, CLUBS), TrucoCard.of(ACE, HEARTS), TrucoCard.of(FOUR, SPADES));
+            cardVira = TrucoCard.of(SIX, CLUBS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(3);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mao de onze com casal menor")
+        void shouldAcceptHandOfElevenWithTwoManilhas(){
+            hand = List.of(TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(FIVE, SPADES));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mao de onze com manilha e tres")
+        void shouldAcceptHandOfElevenWithManilhaAndTres(){
+            hand = List.of(TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(THREE, SPADES));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mao de onze com manilha e duas cartas dois")
+        void shouldAcceptHandOfElevenWithManilhaAndTwoCardsTwo(){
+            hand = List.of(TrucoCard.of(TWO, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(TWO, SPADES));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
+
+        @Test
+        @DisplayName("Testa se aceita mao de onze com duas cartas tres")
+        void shouldAcceptHandOfElevenWithTwoCardsThree(){
+            hand = List.of(TrucoCard.of(THREE, HEARTS), TrucoCard.of(SIX, DIAMONDS), TrucoCard.of(THREE, SPADES));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of();
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(7);
+            Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
+            assertTrue(acceptMaoDeOnze);
+        }
     }
 
-    @Test
-    @DisplayName("Testa se aceita mão de onze com mão completa de três")
-    void shouldAcceptHandOfElevenWithThreeCardsThree(){
-        hand = List.of(TrucoCard.of(THREE, CLUBS), TrucoCard.of(THREE, HEARTS), TrucoCard.of(THREE, SPADES));
-        cardVira = TrucoCard.of(ACE, DIAMONDS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(0);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
+    @Nested
+    @DisplayName("decideIfRaises")
+    class DecideIfRaises{
+        @Test
+        @DisplayName("Testa se na segunda rodada possuir mão com valor maior que 15 pede truco")
+        void shouldRequestTrucoIfInTheSecondRoundHaveAHandWithAValueGreaterThan15(){
+            hand = List.of(TrucoCard.of(THREE, HEARTS), TrucoCard.of(SIX, DIAMONDS), TrucoCard.of(THREE, SPADES));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of(GameIntel.RoundResult.LOST);
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 6).opponentScore(4);
+            Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
+            assertTrue(requestTruco);
+        }
+
+        @Test
+        @DisplayName("Testa se na ultima rodada possuir mão com valor maior que 10 pede truco")
+        void shouldRequestTrucoIfInTheLastRoundHaveAHandWithAValueGreaterThan10(){
+            hand = List.of(TrucoCard.of(THREE, HEARTS));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of(GameIntel.RoundResult.LOST, GameIntel.RoundResult.WON);
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 6).opponentScore(4);
+            Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
+            assertTrue(requestTruco);
+        }
+
+        @Test
+        @DisplayName("Testa se ganhou a primeira rodada e possui carta com valor maior que 10")
+        void shouldRequestTrucoIfYouWonTheFirstRoundAndHaveACardWithValueGreaterThan10(){
+            hand = List.of(TrucoCard.of(FIVE, HEARTS), TrucoCard.of(FOUR, HEARTS));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of(GameIntel.RoundResult.WON);
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 2).opponentScore(3);
+            Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
+            assertTrue(requestTruco);
+        }
+
+        @Test
+        @DisplayName("Testa se empatou a primeira rodada e possui carta com valor maior que 11")
+        void shouldRequestTrucoIfYDrewTheFirstRoundAndHaveACardWithValueGreaterThan11(){
+            hand = List.of(TrucoCard.of(FIVE, HEARTS), TrucoCard.of(FOUR, HEARTS));
+            cardVira = TrucoCard.of(FOUR, CLUBS);
+            roundResult = List.of(GameIntel.RoundResult.DREW);
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 2).opponentScore(3);
+            Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
+            assertTrue(requestTruco);
+        }
     }
 
-    @Test
-    @DisplayName("Testa se aceita mao de onze com pontuacao do oponente menor que 4")
-    void shouldAcceptHandOfElevenWithOpponentScoreLessThanFour(){
-        hand = List.of(TrucoCard.of(TWO, CLUBS), TrucoCard.of(ACE, HEARTS), TrucoCard.of(FOUR, SPADES));
-        cardVira = TrucoCard.of(SIX, CLUBS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(3);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
-    }
 
-    @Test
-    @DisplayName("Testa se aceita mao de onze com casal menor")
-    void shouldAcceptHandOfElevenWithTwoManilhas(){
-        hand = List.of(TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(FIVE, SPADES));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
-    }
 
-    @Test
-    @DisplayName("Testa se aceita mao de onze com manilha e tres")
-    void shouldAcceptHandOfElevenWithManilhaAndTres(){
-        hand = List.of(TrucoCard.of(FOUR, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(THREE, SPADES));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
-    }
 
-    @Test
-    @DisplayName("Testa se aceita mao de onze com manilha e duas cartas dois")
-    void shouldAcceptHandOfElevenWithManilhaAndTwoCardsTwo(){
-        hand = List.of(TrucoCard.of(TWO, HEARTS), TrucoCard.of(FIVE, DIAMONDS), TrucoCard.of(TWO, SPADES));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(5);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
-    }
-
-    @Test
-    @DisplayName("Testa se aceita mao de onze com duas cartas tres")
-    void shouldAcceptHandOfElevenWithTwoCardsThree(){
-        hand = List.of(TrucoCard.of(THREE, HEARTS), TrucoCard.of(SIX, DIAMONDS), TrucoCard.of(THREE, SPADES));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of();
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 11).opponentScore(7);
-        Boolean acceptMaoDeOnze = tecoNoMarrecoBot.getMaoDeOnzeResponse(stepBuilder.build());
-        assertTrue(acceptMaoDeOnze);
-    }
-
-    @Test
-    @DisplayName("Testa se na segunda rodada possuir mão com valor maior que 15 pede truco")
-    void shouldRequestTrucoIfInTheSecondRoundHaveAHandWithAValueGreaterThan15(){
-        hand = List.of(TrucoCard.of(THREE, HEARTS), TrucoCard.of(SIX, DIAMONDS), TrucoCard.of(THREE, SPADES));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of(GameIntel.RoundResult.LOST);
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 6).opponentScore(4);
-        Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
-        assertTrue(requestTruco);
-    }
-
-    @Test
-    @DisplayName("Testa se na ultima rodada possuir mão com valor maior que 10 pede truco")
-    void shouldRequestTrucoIfInTheLastRoundHaveAHandWithAValueGreaterThan10(){
-        hand = List.of(TrucoCard.of(THREE, HEARTS));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of(GameIntel.RoundResult.LOST, GameIntel.RoundResult.WON);
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 6).opponentScore(4);
-        Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
-        assertTrue(requestTruco);
-    }
-
-    @Test
-    @DisplayName("Testa se ganhou a primeira rodada e possui carta com valor maior que 10")
-    void shouldRequestTrucoIfYouWonTheFirstRoundAndHaveACardWithValueGreaterThan10(){
-        hand = List.of(TrucoCard.of(FIVE, HEARTS), TrucoCard.of(FOUR, HEARTS));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of(GameIntel.RoundResult.WON);
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 2).opponentScore(3);
-        Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
-        assertTrue(requestTruco);
-    }
-
-    @Test
-    @DisplayName("Testa se empatou a primeira rodada e possui carta com valor maior que 10")
-    void shouldRequestTrucoIfYDrewTheFirstRoundAndHaveACardWithValueGreaterThan10(){
-        hand = List.of(TrucoCard.of(FIVE, HEARTS), TrucoCard.of(FOUR, HEARTS));
-        cardVira = TrucoCard.of(FOUR, CLUBS);
-        roundResult = List.of(GameIntel.RoundResult.DREW);
-        cards = List.of();
-        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 1).botInfo(hand, 2).opponentScore(3);
-        Boolean requestTruco = tecoNoMarrecoBot.decideIfRaises(stepBuilder.build());
-        assertTrue(requestTruco);
-    }
 
     @Nested
     @DisplayName("chooseCard")
