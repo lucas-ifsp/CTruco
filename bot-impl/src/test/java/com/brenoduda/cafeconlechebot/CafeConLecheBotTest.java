471d585342fa18fb3449230b6925b99d22bb723a
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2023-10-15 20:11:12 -0300

    test: should raise when has clubs and hearts

--- bot-impl/src/test/java/com/brenoduda/cafeconlechebot/CafeConLecheBotTest.java
+++ bot-impl/src/test/java/com/brenoduda/cafeconlechebot/CafeConLecheBotTest.java
@@ -54,5 +54,24 @@ public class CafeConLecheBotTest {
             boolean decideIfRaises = new CafeConLecheBot().decideIfRaises(stepBuilder.build());
             assertThat(decideIfRaises).isEqualTo(true);
         }
+
+        @Test
+        @DisplayName("Should raise when has clubs and hearts")
+        void shouldRaiseWhenHasClubsAndHearts() {
+            List<TrucoCard> botCards = List.of(
+                    TrucoCard.of(FOUR, HEARTS),
+                    TrucoCard.of(SEVEN, SPADES),
+                    TrucoCard.of(KING, CLUBS)
+            );
+            TrucoCard vira = TrucoCard.of(THREE, DIAMONDS);
+
+            GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(vira), vira, 1)
+                    .botInfo(botCards, 0)
+                    .opponentScore(0);
+
+            boolean decideIfRaises = new CafeConLecheBot().decideIfRaises(stepBuilder.build());
+            assertThat(decideIfRaises).isEqualTo(true);
+        }
     }
 }
