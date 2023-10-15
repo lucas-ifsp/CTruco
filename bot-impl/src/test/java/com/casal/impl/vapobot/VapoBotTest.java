e45ddd9ea015a8e6e0e5675d179cbe5b4ce77853
lauraluch <lauraluchiari@hotmail.com>
2023-10-15 17:16:03 -0300

    feat: configure bot instance in test class

--- bot-impl/src/test/java/com/casal/impl/vapobot/VapoBotTest.java
+++ bot-impl/src/test/java/com/casal/impl/vapobot/VapoBotTest.java
@@ -1,8 +1,16 @@
 package com.casal.impl.vapobot;
 
+import com.cremonezzi.impl.carlsenbot.Carlsen;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VapoBotTest {
+    private VapoBot vapoBot;
+
+    @BeforeEach
+    public void config() {
+        vapoBot = new VapoBot();
+    }
 }
