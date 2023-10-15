33f92ec23cce6b170075861c25ee57c79bffaf44
Juan Rossi <juan.lucas.rossi@gmail.com>
2023-10-15 13:24:04 -0300

    refactor (TrucoGuruUtilsTest): refatora organização dos testes para melhor legibilidade

--- bot-impl/src/test/java/com/rossi/lopes/trucoguru/TrucoGuruUtilsTest.java
+++ bot-impl/src/test/java/com/rossi/lopes/trucoguru/TrucoGuruUtilsTest.java
@@ -2,9 +2,9 @@ package com.rossi.lopes.trucoguru;
 
 import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardSuit;
-import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,75 +12,87 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TrucoGuruUtilsTest {
-    @Test
-    @DisplayName("Should return true if hand at least one manilha")
-    void shouldReturnTrueIfHandHasManilha() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.ACE, CardSuit.SPADES),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasManilha(cards, vira)).isTrue();
+    @Nested
+    @DisplayName("HasManilhaTest")
+    class HasManilhaTest {
+        @Test
+        @DisplayName("Should return true if hand at least one manilha")
+        void shouldReturnTrueIfHandHasManilha() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.ACE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasManilha(cards, vira)).isTrue();
+        }
+
+        @Test
+        @DisplayName("Should return false if hand has no manilha")
+        void shouldReturnFalseIfHandHasNoManilha() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasManilha(cards, vira)).isFalse();
+        }
     }
 
-    @Test
-    @DisplayName("Should return false if hand has no manilha")
-    void shouldReturnFalseIfHandHasNoManilha() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasManilha(cards, vira)).isFalse();
+    @Nested
+    @DisplayName("HasZapTest")
+    class HasZapTest {
+        @Test
+        @DisplayName("Should return true if hand has zap")
+        void shouldReturnTrueIfHandHasZap() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasZap(cards, vira)).isTrue();
+        }
+
+        @Test
+        @DisplayName("Should return false if hand has no zap")
+        void shouldReturnFalseIfHandHasNoZap() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasZap(cards, vira)).isFalse();
+        }
     }
 
-    @Test
-    @DisplayName("Should return true if hand has zap")
-    void shouldReturnTrueIfHandHasZap() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasZap(cards, vira)).isTrue();
-    }
+    @Nested
+    @DisplayName("HasStrongCardTest")
+    class HasStrongCardTest {
+        @Test
+        @DisplayName("Should return true if hand has at least one strong card")
+        void shouldReturnTrueIfHandHasStrongCard() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasStrongCard(cards, vira)).isTrue();
+        }
 
-    @Test
-    @DisplayName("Should return false if hand has no zap")
-    void shouldReturnFalseIfHandHasNoZap() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasZap(cards, vira)).isFalse();
-    }
-
-    @Test
-    @DisplayName("Should return true if hand has at least one strong card")
-    void shouldReturnTrueIfHandHasStrongCard() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasStrongCard(cards, vira)).isTrue();
-    }
-
-    @Test
-    @DisplayName("Should return false if hand has no strong card")
-    void shouldReturnTrueIfHandHasNoStrongCard() {
-        TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
-        List<TrucoCard> cards = List.of(
-                TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        );
-        assertThat(TrucoGuruUtils.hasStrongCard(cards, vira)).isFalse();
+        @Test
+        @DisplayName("Should return false if hand has no strong card")
+        void shouldReturnTrueIfHandHasNoStrongCard() {
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.SPADES);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.TWO, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            );
+            assertThat(TrucoGuruUtils.hasStrongCard(cards, vira)).isFalse();
+        }
     }
 }
