28cf78ba20fae021697132e9f5f7be12efcb4e46
YurIFSP <y.menon@aluno.ifsp.edu.br>
2023-10-15 22:26:40 -0300

    feat (bot): improved bot to pass test where bot should discard card if it is the first round and plays first and has two manilhas

--- bot-made-in-descalvado/src/main/java/com/yuri/impl/BotMadeInDescalvado.java
+++ bot-made-in-descalvado/src/main/java/com/yuri/impl/BotMadeInDescalvado.java
@@ -2,7 +2,11 @@ package com.yuri.impl;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BotMadeInDescalvado implements BotServiceProvider {
 
@@ -23,6 +27,48 @@ public class BotMadeInDescalvado implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
+        int round = intel.getRoundResults().size() + 1;
+
+        return switch (round) {
+            case 1 -> chooseCardFirstRound(intel);
+            case 2 -> chooseCardSecondRound(intel);
+            case 3 -> chooseCardThirdRound(intel);
+            default -> throw new RuntimeException("Invalid round");
+        };
+    }
+
+    private CardToPlay chooseCardFirstRound(GameIntel intel) {
+        TrucoCard vira = intel.getVira();
+
+        List<TrucoCard> cards = new ArrayList<>(intel.getCards());
+        cards.sort((c1, c2) -> c2.compareValueTo(c1, vira));
+
+        boolean playsFirst = intel.getOpponentCard().isEmpty();
+
+        if (playsFirst) {
+            int manilhaCount = 0;
+
+            for (var card : intel.getCards()) {
+                if (card.isManilha(intel.getVira())) {
+                    manilhaCount += 1;
+                }
+            }
+
+            if (manilhaCount >= 2) {
+                return CardToPlay.discard(intel.getCards().get(2));
+            } else {
+                return CardToPlay.of(intel.getCards().get(0));
+            }
+        } else {
+            return CardToPlay.of(intel.getCards().get(0));
+        }
+    }
+
+    private CardToPlay chooseCardSecondRound(GameIntel intel) {
+        return CardToPlay.of(intel.getCards().get(0));
+    }
+
+    private CardToPlay chooseCardThirdRound(GameIntel intel) {
         return CardToPlay.of(intel.getCards().get(0));
     }
 }
