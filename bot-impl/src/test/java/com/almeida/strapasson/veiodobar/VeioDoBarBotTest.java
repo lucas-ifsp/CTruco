af676686e315762422f28740ea093f1b4e0bf56b
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-15 23:10:00 -0300

    test: bot should refuse points raising and quit when all card are lower than jack and no manilhas

--- bot-impl/src/test/java/com/almeida/strapasson/veiodobar/VeioDoBarBotTest.java
+++ bot-impl/src/test/java/com/almeida/strapasson/veiodobar/VeioDoBarBotTest.java
@@ -1,5 +1,41 @@
 package com.almeida.strapasson.veiodobar;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class VeioDoBarBotTest {
-    
+    private VeioDoBarBot sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new VeioDoBarBot();
+    }
+
+    @Test
+    @DisplayName("Should refuse points raising if all cards are lower than jacks and no manilhas")
+    void shouldRefusePointsRaisingIfAllCardsAreLowerThanJacksAndNoManilhas(){
+        GameIntel intel = mock(GameIntel.class);
+
+        when(intel.getCards()).thenReturn(List.of(
+                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.HEARTS)
+        ));
+        when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.FOUR, CardSuit.SPADES));
+
+        assertThat(sut.getRaiseResponse(intel)).isEqualTo(-1);
+    }
 }
