706c51f4254bc025334cadddff0191d179b7c587
YurIFSP <y.menon@aluno.ifsp.edu.br>
2023-10-15 22:34:42 -0300

    fix: add GNU General Public License

--- bot-made-in-descalvado/src/main/java/com/yuri/impl/BotMadeInDescalvado.java
+++ bot-made-in-descalvado/src/main/java/com/yuri/impl/BotMadeInDescalvado.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2022 Yuri Soares Menon
+ *  Contact: y <dot> menon <at> aluno <dot> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 package com.yuri.impl;
 
 import com.bueno.spi.model.CardToPlay;
