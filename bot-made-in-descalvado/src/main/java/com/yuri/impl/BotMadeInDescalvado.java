0b6da4ce243b40f504a46fafedfd8f8c6664c184
YurIFSP <y.menon@aluno.ifsp.edu.br>
2023-10-15 21:06:11 -0300

    feat (bot): add first implementation of bot-made-in-descalvado

---
+++ bot-made-in-descalvado/src/main/java/com/yuri/impl/BotMadeInDescalvado.java
@@ -0,0 +1,28 @@
+package com.yuri.impl;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class BotMadeInDescalvado implements BotServiceProvider {
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return CardToPlay.of(intel.getCards().get(0));
+    }
+}
---
+++ bot-made-in-descalvado/src/test/java/com/yuri/impl/BotMadeInDescalvadoTest.java
@@ -0,0 +1,8 @@
+package com.yuri.impl;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+
+public class BotMadeInDescalvadoTest {
+
+}
