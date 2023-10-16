0aa257b935d3bc845cd10a83a68cbe63548df1a1
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-10-15 23:09:59 -0300

    feat: implemented checkExistanceDiamondManilha CaipirasBot method

--- bot-impl/src/main/java/com/gatti/casaque/caipirasbot/CaipirasBot.java
+++ bot-impl/src/main/java/com/gatti/casaque/caipirasbot/CaipirasBot.java
@@ -2,7 +2,10 @@ package com.gatti.casaque.caipirasbot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.List;
 
 public class CaipirasBot implements BotServiceProvider {
     @Override
@@ -23,5 +26,14 @@ public class CaipirasBot implements BotServiceProvider {
     @Override
     public int getRaiseResponse(GameIntel intel) {
         return 0;
+    }
+
+    public boolean checkExistanceDiamondManilha(List<TrucoCard> cards, TrucoCard vira) {
+        for (TrucoCard card : cards) {
+            if (card.isOuros(vira)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
