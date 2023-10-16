1107340eb0a8c4a5c39874dc9b0c5849833ae0f8
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-16 00:29:46 -0300

    feat: the bot will play the smallest card needed to win the round otherwise it discards the smallest one

--- bot-impl/src/main/java/com/almeida/strapasson/veiodobar/VeioDoBarBot.java
+++ bot-impl/src/main/java/com/almeida/strapasson/veiodobar/VeioDoBarBot.java
@@ -3,6 +3,7 @@ package com.almeida.strapasson.veiodobar;
 import com.bueno.spi.model.*;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public final class VeioDoBarBot implements BotServiceProvider {
@@ -18,7 +19,26 @@ public final class VeioDoBarBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        var cards = sortedCards(intel);
+        var refCard = intel.getOpponentCard().orElse(cards.get(0));
+        TrucoCard minCardToWin = null;
+        TrucoCard vira = intel.getVira();
+
+        if (cards.get(2).compareValueTo(refCard, vira) > 0)
+            minCardToWin = cards.get(2);
+        if (cards.get(1).compareValueTo(refCard, vira) > 0)
+            minCardToWin = cards.get(1);
+        if (cards.get(0).compareValueTo(refCard, vira) > 0)
+            minCardToWin = cards.get(0);
+
+        return minCardToWin == null ? CardToPlay.discard(cards.get(0)) : CardToPlay.of(minCardToWin);
+    }
+
+    private List<TrucoCard> sortedCards(GameIntel intel) {
+        return intel.getCards()
+                .stream()
+                .sorted((current, other) -> current.compareValueTo(other, intel.getVira()))
+                .toList();
     }
 
     @Override
