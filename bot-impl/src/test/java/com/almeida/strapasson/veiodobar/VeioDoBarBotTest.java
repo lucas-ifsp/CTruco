47bf649414d180aaf46d0a2422193d132af53d8b
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-10-16 15:47:29 -0300

    test: delete one teste because to strategy changes

--- bot-impl/src/test/java/com/almeida/strapasson/veiodobar/VeioDoBarBotTest.java
+++ bot-impl/src/test/java/com/almeida/strapasson/veiodobar/VeioDoBarBotTest.java
@@ -122,32 +122,6 @@ class VeioDoBarBotTest {
 
 
 
-    @Test
-    @DisplayName("Should accept points raising if all cards are upper than jacks and no manilhas")
-    void shouldAcceptPointsRaisingIfAllCardsAreUpperThanJacksAndNoManilhas() {
-        when(intel.getCards()).thenReturn(List.of(
-                TrucoCard.of(CardRank.KING, CardSuit.SPADES),
-                TrucoCard.of(CardRank.KING, CardSuit.HEARTS),
-                TrucoCard.of(CardRank.KING, CardSuit.CLUBS)
-        ));
-        when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.TWO, CardSuit.SPADES));
-
-        assertThat(sut.getRaiseResponse(intel)).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("should accept raise points if has two manilhas")
-    void shouldRaisePointsIfHasTwoManilhas() {
-        when(intel.getCards()).thenReturn(List.of(
-                TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.ACE, CardSuit.SPADES),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        ));
-        when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.KING, CardSuit.DIAMONDS));
-
-        assertThat(sut.getRaiseResponse(intel)).isEqualTo(0);
-    }
-
 
     @Test
     @DisplayName("Should accept raise points if has one manilha and one card equal or greater than jack")
@@ -175,19 +149,6 @@ class VeioDoBarBotTest {
         assertThat(sut.getRaiseResponse(intel)).isEqualTo(0);
     }
 
-
-    @Test
-    @DisplayName("Should refuse raise points if has just one manilha")
-    void shouldRefuseRaisePointsIfHasJustOneManilha() {
-        when(intel.getCards()).thenReturn(List.of(
-                TrucoCard.of(CardRank.TWO, CardSuit.SPADES),
-                TrucoCard.of(CardRank.FOUR, CardSuit.HEARTS),
-                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
-        ));
-        when(intel.getVira()).thenReturn(TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS));
-
-        assertThat(sut.getRaiseResponse(intel)).isEqualTo(-1);
-    }
 
     @Test
     @DisplayName("Should refuse points raising if all cards are lower than jacks and no manilhas")
