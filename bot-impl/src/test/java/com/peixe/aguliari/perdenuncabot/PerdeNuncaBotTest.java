2f9adce235a0254d840756315af9ca9d64d2f27d
RamonPeixe <r-amoncp@hotmail.com>
2023-10-17 17:43:35 -0300

    test(bot): adding raiseResponseIfBothScoresAre0 test for getRaiseResponse method

--- bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
+++ bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
@@ -793,4 +793,23 @@ public class PerdeNuncaBotTest {
         assertThat(perdeNuncaBot.getRaiseResponse(stepBuilder.build())).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("Raise response if both scores are 0")
+    public void raiseResponseIfBothScoresAre0() {
+        TrucoCard vira = TrucoCard.of(CardRank.TWO, CardSuit.CLUBS);
+
+        List<TrucoCard> openCards = Collections.singletonList(
+                TrucoCard.of(CardRank.TWO, CardSuit.CLUBS));
+        List<TrucoCard> botCards = Arrays.asList(
+                TrucoCard.of(CardRank.KING, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.ACE, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.SPADES));
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.WON), openCards, vira, 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0);
+
+        assertThat(perdeNuncaBot.getRaiseResponse(stepBuilder.build())).isEqualTo(1);
+    }
+
 }
