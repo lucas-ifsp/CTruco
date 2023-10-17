1a2568e93521a1afb08e5cdf1396e75879f02083
Eduardo <eduardocorreass25@gmail.com>
2023-10-16 23:03:42 -0300

    test: add ShouldChooseACard test

---
+++ bot-impl/src/main/java/com/correacarini/impl/trucomachinebot/TrucoMachineBot.java
@@ -0,0 +1,32 @@
+package com.correacarini.impl.trucomachinebot;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class TrucoMachineBot implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return CardToPlay.of(intel.getCards().get(0));
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return BotServiceProvider.super.getName();
+    }
+}
