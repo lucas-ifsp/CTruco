654ca6b7f97b380698eaad01b85d19b7738911cd
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2023-10-16 23:28:53 -0300

    test: should raise when has clubs and three

--- bot-impl/src/test/java/com/brenoduda/cafeconlechebot/CafeConLecheBotTest.java
+++ bot-impl/src/test/java/com/brenoduda/cafeconlechebot/CafeConLecheBotTest.java
@@ -276,5 +276,24 @@ public class CafeConLecheBotTest {
             int raiseResponse = new CafeConLecheBot().getRaiseResponse(stepBuilder.build());
             assertThat(raiseResponse).isEqualTo(1);
         }
+
+        @Test
+        @DisplayName("Should raise when has clubs and three")
+        void shouldRaiseWhenHasClubsAndThree() {
+            List<TrucoCard> botCards = List.of(
+                    TrucoCard.of(THREE, HEARTS),
+                    TrucoCard.of(ACE, SPADES),
+                    TrucoCard.of(JACK, CLUBS)
+            );
+            TrucoCard vira = TrucoCard.of(KING, DIAMONDS);
+
+            GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(vira), vira, 1)
+                    .botInfo(botCards, 0)
+                    .opponentScore(0);
+
+            int raiseResponse = new CafeConLecheBot().getRaiseResponse(stepBuilder.build());
+            assertThat(raiseResponse).isEqualTo(1);
+        }
     }
 }
