fff4252eda4c84edb5bb8f8c418efbe5f722dfc2
Mateus Vieira <mateus.sly.mp@gmail.com>
2023-10-17 14:47:27 -0300

    chore:  Add GPL 3.O License to SkolTable and SkolTableBotTest classes

--- bot-impl/src/main/java/com/meima/skoltable/SkolTable.java
+++ bot-impl/src/main/java/com/meima/skoltable/SkolTable.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright (C) 2023 Mateus Vieira and Stefhani Alkin - IFSP/SCL
+ *  Contact: vieira <dot> mateus <at> aluno <dot> ifsp <dot> edu <dot> br
+ *  Contact: s <dot> alkin <at> aluno <dot> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 package com.meima.skoltable;
 
 import com.bueno.spi.model.CardRank;
