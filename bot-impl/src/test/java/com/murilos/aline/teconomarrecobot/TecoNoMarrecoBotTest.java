341bda3781725a866f689b075882a079be76962f
alinirv <alini.vicente@yahoo.com.br>
2023-10-16 23:21:44 -0300

    test(TecoNoMarreco): ask for a raise if there was a draw and you have zap.

--- bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
+++ bot-impl/src/test/java/com/murilos/aline/teconomarrecobot/TecoNoMarrecoBotTest.java
@@ -324,6 +324,17 @@ class TecoNoMarrecoBotTest {
 
             assertThat(tecoNoMarrecoBot.getRaiseResponse(stepBuilder.build())).isOne();
         }
+        @Test
+        @DisplayName("Testa pede aumento se teve empate  tem zap")
+        void askForRaiseIfThereWasDrawAndHaveZap() {
+            hand = List.of(TrucoCard.of(ACE,HEARTS), TrucoCard.of(KING, CLUBS), TrucoCard.of(TWO, CLUBS));
+            cardVira = TrucoCard.of(JACK, SPADES);
+            roundResult = List.of(GameIntel.RoundResult.DREW);
+            cards = List.of();
+            stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cards, cardVira, 3).botInfo(hand, 1).opponentScore(0);
+
+            assertThat(tecoNoMarrecoBot.getRaiseResponse(stepBuilder.build())).isOne();
+        }
 
     }
 }
