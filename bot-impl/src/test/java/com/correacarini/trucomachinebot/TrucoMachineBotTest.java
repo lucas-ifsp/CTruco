79050b653aeac81096a4c6df22e3db83cc51659f
Eduardo <eduardocorreass25@gmail.com>
2023-10-16 23:56:34 -0300

    test: add ShouldReturnLowestCardIfNoneBeatsOpponent test

--- bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
+++ bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
@@ -25,7 +25,6 @@ public class TrucoMachineBotTest {
         CardToPlay cardToPlay = new TrucoMachineBot().chooseCard(stepBuilder.build());
         assertNotNull(cardToPlay);
     }
-
     @Test
     @DisplayName("Should return greatest card in round 1 if is first to play")
     void ShouldReturnGreatestCardInRound1IfIsFirstToPlay() {
@@ -41,7 +40,6 @@ public class TrucoMachineBotTest {
         CardToPlay cardToPlay = new TrucoMachineBot().chooseCard(stepBuilder.build());
         assertEquals(CardToPlay.of(TrucoCard.of(THREE, CLUBS)), cardToPlay);
     }
-
     @Test
     @DisplayName("Should return minimal greater card If is second to play")
     void ShouldReturnMinimalGreaterCardIfIsSecondToPlay() {
@@ -58,5 +56,22 @@ public class TrucoMachineBotTest {
 
         CardToPlay cardToPlay = new TrucoMachineBot().chooseCard(stepBuilder.build());
         assertEquals(CardToPlay.of(TrucoCard.of(SEVEN, CLUBS)), cardToPlay);
+    }
+    @Test
+    @DisplayName("Should return lowest card if none beats opponent")
+    void ShouldReturnLowestCardIfNoneBeatsOpponent() {
+        List<TrucoCard> botCards = List.of(
+                TrucoCard.of(SEVEN, CLUBS),
+                TrucoCard.of(FIVE, DIAMONDS),
+                TrucoCard.of(THREE, CLUBS)
+        );
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(), List.of(), TrucoCard.of(ACE, SPADES), 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0)
+                .opponentCard(TrucoCard.of(TWO, SPADES));
+
+        CardToPlay cardToPlay = new TrucoMachineBot().chooseCard(stepBuilder.build());
+        assertEquals(CardToPlay.of(TrucoCard.of(FIVE, DIAMONDS)), cardToPlay);
     }
 }
