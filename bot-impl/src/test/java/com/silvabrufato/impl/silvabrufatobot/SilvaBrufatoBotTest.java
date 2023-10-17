35cf67c69905f197377e5fcc61a7ef6d46a17f87
ReBrufato <brufato17.renan@gmail.com>
2023-10-17 00:29:46 -0300

    test: bot must ask for truco if bot has copas

--- bot-impl/src/test/java/com/silvabrufato/impl/silvabrufatobot/SilvaBrufatoBotTest.java
+++ bot-impl/src/test/java/com/silvabrufato/impl/silvabrufatobot/SilvaBrufatoBotTest.java
@@ -773,6 +773,17 @@ public class SilvaBrufatoBotTest {
                         TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS)));
                 assertThat(sut.decideIfRaises(gameIntel)).isTrue();
             }
+
+            @Test
+            @DisplayName("shouldReturnTrueIfBotHasCopas")
+            void shouldReturnTrueIfBotHasCopas() {
+                when(gameIntel.getRoundResults()).thenReturn(List.of(RoundResult.WON));
+                when(gameIntel.getVira()).thenReturn(TrucoCard.of(CardRank.ACE, CardSuit.CLUBS));
+                when(gameIntel.getCards()).thenReturn(List.of(
+                        TrucoCard.of(CardRank.TWO, CardSuit.HEARTS),
+                        TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS)));
+                assertThat(sut.decideIfRaises(gameIntel)).isTrue();
+            }
         }
     }
 }
