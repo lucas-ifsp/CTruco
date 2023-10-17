240062b39092e2f51c7af430c95db356fe2e7290
RamonPeixe <r-amoncp@hotmail.com>
2023-10-17 17:51:58 -0300

    test(bot): adding dontRaiseIfOurScoreIs3AndOpponentScoreIs6OrHigherAndDoesNotHaveZapManilha2or3 test for getRaiseResponse method

--- bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
+++ bot-impl/src/test/java/com/peixe/aguliari/perdenuncabot/PerdeNuncaBotTest.java
@@ -1021,4 +1021,23 @@ public class PerdeNuncaBotTest {
         assertThat(perdeNuncaBot.getRaiseResponse(stepBuilder.build())).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("Don't raise if our score is 3 and opponent score is 6 or higher and does not have zap, manilha, 2 or 3")
+    public void dontRaiseIfOurScoreIs3AndOpponentScoreIs6OrHigherAndDoesNotHaveZapManilha2or3() {
+        TrucoCard vira = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Collections.singletonList(
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS));
+        List<TrucoCard> botCards = Arrays.asList(
+                TrucoCard.of(CardRank.QUEEN, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES),
+                TrucoCard.of(CardRank.SEVEN, CardSuit.CLUBS));
+        GameIntel.StepBuilder stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.WON), openCards, vira, 1)
+                .botInfo(botCards, 3)
+                .opponentScore(6);
+
+        assertThat(perdeNuncaBot.getRaiseResponse(stepBuilder.build())).isEqualTo(0);
+    }
+
 }
