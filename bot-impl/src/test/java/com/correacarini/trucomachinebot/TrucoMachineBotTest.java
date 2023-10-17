68809f45327c96cfa903df8e7c5251c94e6464ad
Marcos Vinicius <marcos.carini22@gmail.com>
2023-10-17 18:13:38 -0300

    test: add shouldAcceptMaoDeOnzeWhenHaving3StrongCardsIncludingManilha test

--- bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
+++ bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
@@ -260,4 +260,21 @@ public class TrucoMachineBotTest {
         boolean playMaoDeOnze = new TrucoMachineBot().getMaoDeOnzeResponse(stepBuilder.build());
         assertFalse(playMaoDeOnze);
     }
+    
+    @Test
+    @DisplayName("Should accept mao de onze when having 3 strong cards including manilha ")
+    void shouldAcceptMaoDeOnzeWhenHaving3StrongCardsIncludingManilha(){
+    TrucoCard manilha = TrucoCard.of(TWO,CLUBS);
+    List<TrucoCard> botCards = List.of(
+            TrucoCard.of(THREE,SPADES),
+            TrucoCard.of(THREE, DIAMONDS),
+            manilha
+    );
+    GameIntel.StepBuilder stepBuilder = (GameIntel.StepBuilder) GameIntel.StepBuilder.with()
+            .gameInfo(List.of(), List.of(), TrucoCard.of(ACE, SPADES), 1)
+            .botInfo(botCards, 0);
+
+    boolean playMaoDeOnze = new TrucoMachineBot().getMaoDeOnzeResponse(stepBuilder.build());
+    assertTrue(playMaoDeOnze);
+    }
 }
