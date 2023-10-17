69cad7b048a55244c3405dab21d01582e8bfb6a4
Everton Lourenco <promo.everton@gmail.com>
2023-10-16 23:52:00 -0300

    test: implementa teste ShouldAcceptMaoDeOnzeWhenOpponentHavesNineOrMorePointsbutHaveTwos

--- bot-impl/src/test/java/com/everton/ronaldo/arrebentabot/ArrebentaBotTest.java
+++ bot-impl/src/test/java/com/everton/ronaldo/arrebentabot/ArrebentaBotTest.java
@@ -285,6 +285,26 @@ class ArrebentaBotTest {
             assertTrue(arrebentaBot.getMaoDeOnzeResponse(intel));
 
         }
+
+        @Test
+        @DisplayName("Should accept mao de onze when opponent has nine or more points but have twos")
+        void ShouldAcceptMaoDeOnzeWhenOpponentHasNineOrMorePointsButHaveTwos() {
+            GameIntel intel = mock(GameIntel.class);
+
+            List<TrucoCard> cards = Arrays.asList(
+                    TrucoCard.of(CardRank.TWO, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.KING, CardSuit.CLUBS));
+            TrucoCard vira = TrucoCard.of(CardRank.KING, CardSuit.CLUBS);
+
+
+            when(intel.getVira()).thenReturn(vira);
+            when(intel.getCards()).thenReturn(cards);
+            when(intel.getOpponentScore()).thenReturn(9);
+
+            assertTrue(arrebentaBot.getMaoDeOnzeResponse(intel));
+
+        }
     }
 /////////////////////////////////////////////////////////////////////////////////////////////////////
     @Nested
