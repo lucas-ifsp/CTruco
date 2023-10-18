96a5897042f5d9e447cdd365ca98963144ddade6
Marcos Vinicius <marcos.carini22@gmail.com>
2023-10-17 21:30:53 -0300

    test: add shouldAcceptTrucoIfRoundIs0HavingZeroManilhasButThreeTernos test

--- bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
+++ bot-impl/src/test/java/com/correacarini/trucomachinebot/TrucoMachineBotTest.java
@@ -476,4 +476,25 @@ public class TrucoMachineBotTest {
 
     }
 
+    @Test
+    @DisplayName("should accept truco if round is 0 having zero manilhas but three ternos")
+    void shouldAcceptTrucoIfRoundIs0HavingZeroManilhasButThreeTernos(){
+        List<GameIntel.RoundResult> roundResults = List.of(
+        );
+
+        List<TrucoCard> botCards = List.of(
+                TrucoCard.of(THREE,CLUBS),
+                TrucoCard.of(THREE, DIAMONDS),
+                TrucoCard.of(THREE, SPADES)
+        );
+        GameIntel.StepBuilder stepBuilder = (GameIntel.StepBuilder) GameIntel.StepBuilder.with()
+                .gameInfo(roundResults, List.of(), TrucoCard.of(FIVE, SPADES), 1)
+                .botInfo(botCards, 0)
+                .opponentScore(0);
+
+        int raiseResponse = new TrucoMachineBot().getRaiseResponse(stepBuilder.build());
+        assertThat(raiseResponse).isZero();
+
+    }
+
 }
