31ba33ee9502c1a74dce3e5e41a8e94711b7f061
Guilherme <guilhermelopes035@gmail.com>
2023-10-17 22:03:25 -0300

    feature (TrucoGuru): add feat for equals score mao de onze

--- bot-impl/src/main/java/com/rossi/lopes/trucoguru/TrucoGuru.java
+++ bot-impl/src/main/java/com/rossi/lopes/trucoguru/TrucoGuru.java
@@ -39,6 +39,7 @@ public class TrucoGuru implements BotServiceProvider {
         int opponentScore = intel.getOpponentScore();
         int botScore = intel.getScore();
         int advantageOfScore = botScore - opponentScore;
+        if(advantageOfScore == 0) return true;
 
         Boolean hasCasalMaior = TrucoGuruUtils.hasCasalMaior(cards, vira);
         if(hasCasalMaior) return true;
