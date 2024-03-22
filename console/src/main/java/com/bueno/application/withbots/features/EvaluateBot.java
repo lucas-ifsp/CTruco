78246d51eaea057e1e1ee734af20f56cd70d5eac
gcontiero11 <gustcontiero@gmail.com>
2024-03-22 18:26:57 -0300

    evaluateBot update

--- console/src/main/java/com/bueno/application/withbots/features/EvaluateBot.java
+++ console/src/main/java/com/bueno/application/withbots/features/EvaluateBot.java
@@ -5,11 +5,8 @@ import com.bueno.domain.usecases.bot.providers.BotProviders;
 import com.bueno.domain.usecases.game.dtos.EvaluateResultsDto;
 import com.bueno.domain.usecases.game.usecase.EvaluateBotUseCase;
 
-import java.util.UUID;
-
 public class EvaluateBot {
     public static final int TIMES = 31;
-    private final UUID uuidBotToEvaluate = UUID.randomUUID();
 
     public void againstAll() {
         final var prompt = new UserPrompt();
@@ -24,8 +21,4 @@ public class EvaluateBot {
 
         prompt.printResultEvaluateBot(results.numberOfGames(), (results.end() - results.start()), botToEvaluateName, results.evaluatedBotWins(), results.winRate(), results.percentil());
     }
-
-
-
-
 }
