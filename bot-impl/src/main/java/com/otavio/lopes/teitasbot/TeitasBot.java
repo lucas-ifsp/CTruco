a0edddf70119cb0cdc788db5f92c28f6a85beb99
otaviopl <otavio.pereira.lopes@gmail.com>
2024-04-20 13:51:03 -0300

    FEAT: add teitasBotFunctions and implements new function, hasManilha. going for test

--- bot-impl/src/main/java/com/otavio/lopes/teitasbot/TeitasBot.java
+++ bot-impl/src/main/java/com/otavio/lopes/teitasbot/TeitasBot.java
@@ -1,4 +1,38 @@
 package com.otavio.lopes.teitasbot;
 
-public class TeitasBot {
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.List;
+
+public class TeitasBot implements BotServiceProvider {
+    private List<TrucoCard> myCards;
+    private TrucoCard vira;
+    private TrucoCard bestCard;
+    private TrucoCard secondBestCard;
+    private TrucoCard worstCard;
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+
+
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
 }
---
+++ bot-impl/src/main/java/com/otavio/lopes/teitasbot/TeitasBotFunctions.java
@@ -0,0 +1,14 @@
+package com.otavio.lopes.teitasbot;
+
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.TrucoCard;
+
+import java.util.List;
+
+public class TeitasBotFunctions {
+    static Boolean hasManilha(List<TrucoCard> cards, TrucoCard vira) {
+        return cards.stream().anyMatch(card -> card.isManilha(vira));
+    }
+
+   
+}
