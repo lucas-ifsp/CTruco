76812e3193921aa82b591a61ab4c90d0a75dd584
renatoxd152 <renatomorillo@gmail.com>
2024-04-29 15:56:36 -0300

    feat: Added check to the method to check if you have two or more strong cards

--- bot-impl/src/main/java/com/renato/DarthVader/DarthVader.java
+++ bot-impl/src/main/java/com/renato/DarthVader/DarthVader.java
@@ -1,8 +1,12 @@
 package com.renato.DarthVader;
 
+import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+
+import java.util.List;
 
 public class DarthVader implements BotServiceProvider {
 
@@ -13,6 +17,12 @@ public class DarthVader implements BotServiceProvider {
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
+
+        if(countManilhasAndHighCards(intel) >= 2)
+        {
+            return true;
+        }
+
         return false;
     }
 
@@ -25,4 +35,25 @@ public class DarthVader implements BotServiceProvider {
     public int getRaiseResponse(GameIntel intel) {
         return 0;
     }
+
+
+    public boolean isHighCard(TrucoCard card) {
+        CardRank rank = card.getRank();
+        return rank == CardRank.THREE || rank == CardRank.TWO || rank == CardRank.ACE;
+    }
+
+    public int countManilhasAndHighCards(GameIntel intel) {
+        int count = 0;
+        TrucoCard vira = intel.getVira();
+        List<TrucoCard> cards = intel.getCards();
+
+        for (TrucoCard card : cards) {
+            if (card.isManilha(vira) || isHighCard(card)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
