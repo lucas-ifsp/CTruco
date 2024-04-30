a217653a4147806be4f1b5515013494005f5b035
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-04-30 16:17:34 -0300

    test: add generate functions: random round resuld and random card

--- bot-impl/src/test/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecidaTest.java
+++ bot-impl/src/test/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecidaTest.java
@@ -1,7 +1,65 @@
 package com.barbara.lucasCruz.patriciaAparecida;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Random;
 
 class PatriciaAparecidaTest {
+
+    private PatriciaAparecida patricia;
+    private GameIntel.StepBuilder stepBuilder;
+
+    @BeforeEach
+    void setUp() {
+        patricia = new PatriciaAparecida();
+    }
+
+    public GameIntel.RoundResult generateRandomRoundResult() {
+        GameIntel.RoundResult[] values = GameIntel.RoundResult.values();
+
+        Random random = new Random();
+        int index = random.nextInt(values.length);
+
+        return values[index];
+    }
+
+    public CardRank generateRandomCardRank() {
+        CardRank[] values = CardRank.values();
+        Random random = new Random();
+        int index = random.nextInt(values.length);
+        return values[index];
+    }
+
+    public CardSuit generateRandomCardSuit() {
+        CardSuit[] values = CardSuit.values();
+        Random random = new Random();
+        int index = random.nextInt(values.length);
+
+        while (values[index] == CardSuit.HIDDEN) {
+            index = random.nextInt(values.length);
+        }
+
+        return values[index];
+    }
+
+    public TrucoCard generateRandomCard(){ //assegura que CardRank.HIDDEN só receba CardSuit.HIDDEN
+        CardRank cardRank = generateRandomCardRank();
+        if(cardRank == CardRank.HIDDEN){
+            return TrucoCard.of(cardRank,CardSuit.HIDDEN);
+        }
+        return TrucoCard.of(cardRank,generateRandomCardSuit());
+    }
+
+    public TrucoCard GenerateRandomCardToPlay(){ //sem cartas viradas (hidden)
+        TrucoCard cardToPlay = generateRandomCard();
+        while (cardToPlay.getSuit() == CardSuit.HIDDEN) {
+            cardToPlay = generateRandomCard();
+        }
+        return cardToPlay;
+    }
 
 }
