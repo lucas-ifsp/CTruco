5963be62910d5c98efbfcc0e8ac861f894c849b9
Estevan Sousa <estevanmartins03@gmail.com>
2024-05-03 14:53:20 -0300

    :tada: start jakaredumatubot

---
+++ bot-impl/src/main/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBot.java
@@ -0,0 +1,34 @@
+package com.miguelestevan.jakaredumatubot;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class JakareDuMatuBot implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return BotServiceProvider.super.getName();
+    }
+    // The correct way to say is JakaréDuMatuBóty
+
+}
