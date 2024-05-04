284f3b4877cabce57f785cb519cd5edc5e4e9532
Pedro-Avz <pedroalvarez96@hotmail.com>
2024-05-04 10:34:59 -0300

    test(PauladaSecaBotTest): test accept truco if has minor couple

--- bot-impl/src/test/java/com/pedrocagiovane/pauladasecabot/PauladaSecaBotTest.java
+++ bot-impl/src/test/java/com/pedrocagiovane/pauladasecabot/PauladaSecaBotTest.java
@@ -365,4 +365,16 @@ public class PauladaSecaBotTest {
         int resultado = pauladaSecaBot.getRaiseResponse(stepBuilder.build());
         assertThat(resultado).isOne();
     }
+
+    @Test
+    @DisplayName("Aceita truco se casal menor e perdeu a primeira")
+    void aceitarApostaSeTiverCasalMenor() {
+        maoPlayer = List.of(TrucoCard.of(JACK,CLUBS), TrucoCard.of(TWO, SPADES), TrucoCard.of(TWO, DIAMONDS));
+        vira = TrucoCard.of(ACE, SPADES);
+        roundResult = List.of(GameIntel.RoundResult.LOST);
+        cartas = List.of();
+        stepBuilder = GameIntel.StepBuilder.with().gameInfo(roundResult, cartas, vira, 3).botInfo(maoPlayer, 1).opponentScore(0);
+        int resultado = pauladaSecaBot.getRaiseResponse(stepBuilder.build());
+        assertThat(resultado).isOne();
+    }
 }
