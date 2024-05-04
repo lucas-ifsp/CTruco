bce405e1e0c0551a1c10f36ae58e6a18c1d79d01
lucas da silva cruz <lucas6jau@gmail.com>
2024-05-04 15:54:49 -0300

    fix: correction of invalid decideIfRaises logic

--- bot-impl/src/main/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecida.java
+++ bot-impl/src/main/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecida.java
@@ -11,85 +11,7 @@ import java.util.Optional;
 
 public class PatriciaAparecida implements BotServiceProvider {
 
-    /* TIP quando a gente tem que começar:
-    //é a primeira rodada?
-    //sim:
 
-    //a probabilidade de o oponente ter cartas maiores que cada uma das minhas é:
-    //baixa para pelo menos 2 cartas: - definir quanto
-    //pedir truco
-
-    //qual(is) a(s) carta(s) com MENOR probabilidade do oponente ter carta maior?
-    //se tiver 2 com a mesma probabilidade: jogar a menor
-        //se nao: jogar a carta com menor probabilidade do oponente ter carta maior
-
-    //nao:
-
-    //é a segunda rodada? sempre que a gente que começa jogando a segunda é pq ganhamos a primeira
-    //sim:
-    //a probabilidade de o oponente ter cartas maiores que cada uma das minhas é:
-    //baixa para pelo menos uma das restantes: -defifinir quanto
-    //pedir truco, aumentar
-    //jogar a menor, virada para baixo
-    //nao: começar a 3a rodada significa que perdemos a primeira e ganhamos a segunda:
-    //a probabilidade de o ter a carta maior que a minha é:
-    //baixa: -defifinir quanto
-    //pedir truco, aumentar
-    //jogar a carta
-
-    //quando o oponente joga e a gente tem que tornar:
-    //é a primeira rodada:
-    //sim:
-    //tenho uma carta que empata?
-    //sim:
-    //qual a probabilidade do oponente ter uma carta maior que a minha melhor carta?
-    //probabilidade baixa: - a ser definida
-    //pede truco, aumenta
-    //jogo a carta que empata
-    //probabilidade alta: - a ser definida
-    //tenho uma carta que ganha?
-    //sim:
-    //joga a minha menor carta que ganha da que o oponente jogou
-    //nao:
-    //joga a menor carta, virada
-    //nao:
-    //tenho uma carta que ganha?
-    //sim:
-    //qual a probabilidade do oponente ter uma carta maior que as minhas cartas - retirando a menor carta que ganha
-    //baixa: pede truco, aumenta
-    //jogo a menor carta que ganha
-    // nao:
-    //é a segunda rodada?
-    //sim: o oponente começar a segunda rodada significa que perdemos a primeira
-    //tenho uma carta que ganha?
-    // sim: joga a carta
-    // nao: jogo a menor carta da minha mao - PERDEMOS!
-    //nao: é a terceira rodada, ganhamos a primeira e o oponente a segunda
-    //tenho uma carta que empata ou ganha?
-    //sim:
-    //pede truco, aumenta
-    //joga a carta
-
-    //para pensar: se o oponente inicia o jogo, ele joga geralmente sua maior carta,
-    // talvez seja bom guardar as cartas que ele jogou numa lista
-    //para pensar: é bom jogar carta virada para baixo pro oponente nao ter referencial
-    //quando jogamos uma carta virada para baixo, perdemos seu valor,
-    // talvez seja bom guardar as cartas que jogamos numa lista
-
-    //quando aceitar truco, aceitar que aumente?
-    //ideia: aumentar sempre que a gente ja ia pedir
-    //
-    //quando jogar mao de 11?
-    //ideias:
-    //se o oponente tem menos de 8 pontos da pra escolher uma mao "menos melhor"
-    ////se o oponentem tem 8 ou mais pontos tem que ser uma mao "mais melhor"
-    */
-
-
-
-    //decide se o bot joga uma "mão de onze".
-    // Retornar false significa sair da mão.
-    //Retornar true significa aceitar e jogar uma mão de três pontos;
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
             if(intel.getScore() != 11) throw new IllegalArgumentException("Hand of Eleven can't be called without 11 Points");
@@ -103,7 +25,9 @@ public class PatriciaAparecida implements BotServiceProvider {
     //Retornar true significa solicitar um aumento de ponto;
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        if(intel.getHandPoints() > 1) throw new IllegalArgumentException("Points above limit of hand (12 points) ");
+
+        if(intel.getHandPoints() > 12) throw new IllegalArgumentException("Points above limit of hand (12 points) ");
+        else if(intel.getHandPoints() > 1) return false;
 
         return false;
     }
@@ -118,7 +42,9 @@ public class PatriciaAparecida implements BotServiceProvider {
               Optional<TrucoCard> weakestCardThatWins = getWeakestCardThatWins(intel.getCards(),intel);
               if(weakestCardThatWins.isPresent()) return CardToPlay.of(weakestCardThatWins.get());
               Optional<TrucoCard> cardThatDraws = getCardThatDraws(intel.getCards(),intel);
-              if(cardThatDraws.isPresent()) return CardToPlay.of(cardThatDraws.get());
+            if(intel.getRoundResults().isEmpty()) {
+                if (cardThatDraws.isPresent()) return CardToPlay.of(cardThatDraws.get());
+            }
               else return CardToPlay.discard(sortCards(intel.getCards(),intel).stream().findFirst().get());
         }
         return CardToPlay.of(intel.getCards().get(0));
