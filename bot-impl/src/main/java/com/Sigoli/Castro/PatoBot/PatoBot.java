d1db3a4a9bb191460f2f526fd1f1f9b4a3c2c3e8
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-04 17:39:24 -0300

    test: Implement ShouldReturnTrueIfOpponentIsFirstToPlay test

--- bot-impl/src/main/java/com/Sigoli/Castro/PatoBot/PatoBot.java
+++ bot-impl/src/main/java/com/Sigoli/Castro/PatoBot/PatoBot.java
@@ -1,4 +1,27 @@
 package com.Sigoli.Castro.PatoBot;
 
-public class PatoBot {
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class PatoBot implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
 }
--- bot-impl/src/test/java/com/Sigoli/Castro/PatoBot/PatoBotTest.java
+++ bot-impl/src/test/java/com/Sigoli/Castro/PatoBot/PatoBotTest.java
@@ -1,4 +1,36 @@
 package com.Sigoli.Castro.PatoBot;
+import com.bueno.spi.model.*;
+import com.bueno.spi.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class PatoBotTest {
+    PatoBot patoBot;
+    private GameIntel.StepBuilder stepBuilder;
+    @Test
+    @DisplayName("Should return true id opponent is first to play")
+    void shoulReturnTrueIfOpponentIsFirstToPlay() {
+        TrucoCard opponentCard = TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS);
+        boolean opponentPlay = true;
+        assertThat(patoBot.checkIfOpponentIsFirstToPlay(opponentCard).equals(opponentPlay));
+    }
 }
+
+
