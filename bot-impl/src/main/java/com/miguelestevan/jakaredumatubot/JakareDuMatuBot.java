800e0376aa3553243c94475a8705c9e921141582
miguel <miguelsilvalves.2015@gmail.com>
2024-05-04 14:22:24 -0300

    :sparkles: feat: add the rule on decideIfRaises if the hand contains two manilhas

--- bot-impl/src/main/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBot.java
+++ bot-impl/src/main/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBot.java
@@ -27,6 +27,8 @@ public class JakareDuMatuBot implements BotServiceProvider {
                 if(getManilhas(intel.getCards(), intel.getVira()).containsAll(List.of(CardSuit.CLUBS, CardSuit.HEARTS))){
                     // Hand contains zap and copas
                     return true;
+                } else if (getManilhas(intel.getCards(), intel.getVira()).size() == 2) {
+                    return true;
                 }
             }
             case 1 -> {
