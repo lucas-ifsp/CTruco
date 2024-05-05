26ba113a4d991992eb3e8e6cbbcf9f99c93b2acb
SauloFernando99 <saulofernando114@gmail.com>
2024-05-04 21:36:08 -0300

    faet: exported ChatgptBot

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -8,6 +8,7 @@ import com.hermespiassi.casados.marrecobot.MarrecoBot;
 import com.hideki.araujo.wrkncacnterbot.WrkncacnterBot;
 import com.indi.impl.addthenewsoul.AddTheNewSoul;
 import com.newton.dolensi.sabotabot.SabotaBot;
+import com.soares.gibim.chatgptbot.Chatgptbot;
 import com.zampieri.rissatti.impl.UncleBobBot.UncleBobBot;
 import com.meima.skoltable.SkolTable;
 import com.rossi.lopes.trucoguru.TrucoGuru;
@@ -43,6 +44,7 @@ module bot.impl {
     exports com.yuri.impl;
     exports com.gatti.casaque.caipirasbot;
     exports com.gustavo.contiero.lazybot;
+    exports com.soares.gibim.chatgptbot;
 
     provides com.bueno.spi.service.BotServiceProvider with
             ArrebentaBot,
@@ -67,5 +69,6 @@ module bot.impl {
             PaulistaBot,
             MarrecoBot,
             AddTheNewSoul,
-            LazyBot;
+            LazyBot,
+            Chatgptbot;
 }
