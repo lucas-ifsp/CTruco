19182511bed0acf0e8029d2b507d495342c90090
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-05 12:45:41 -0300

    test: Implement  shouldRejectMaoDeOnzeIfStrongerCardIsTwotest

--- bot-impl/src/test/java/com/Sigoli/Castro/PatoBot/PatoBotTest.java
+++ bot-impl/src/test/java/com/Sigoli/Castro/PatoBot/PatoBotTest.java
@@ -285,15 +285,27 @@ public class PatoBotTest {
         TrucoCard card3 = TrucoCard.of(CardRank.ACE,CardSuit.CLUBS);
         TrucoCard vira = TrucoCard.of (CardRank.ACE, CardSuit.SPADES);
         int opponentScore = 8;
-
         when(intel.getOpponentScore()).thenReturn(opponentScore);
         when(intel.getCards()).thenReturn(Arrays.asList(card1,card2,card3));
         when(intel.getVira()).thenReturn(vira);
 
         assertFalse(patoBot.checkIfAcceptMaoDeOnze(intel));
-
     }
+    @Test
+    @DisplayName("Should Reject 'Mão de Onze' if stronger card is a two")
+    public void shouldRejectMaoDeOnzeIfStrongerCardIsTwo(){
+        GameIntel intel = mock(GameIntel.class);
+        TrucoCard card1 = TrucoCard.of(CardRank.TWO, CardSuit.SPADES);
+        TrucoCard card2 = TrucoCard.of(CardRank.QUEEN,CardSuit.CLUBS);
+        TrucoCard card3 = TrucoCard.of(CardRank.ACE,CardSuit.CLUBS);
+        TrucoCard vira = TrucoCard.of (CardRank.SIX, CardSuit.SPADES);
+        int opponentScore = 5;
+        when(intel.getOpponentScore()).thenReturn(opponentScore);
+        when(intel.getCards()).thenReturn(Arrays.asList(card1,card2,card3));
+        when(intel.getVira()).thenReturn(vira);
 
+        assertFalse(patoBot.checkIfAcceptMaoDeOnze(intel));
+    }
 }
 
 
