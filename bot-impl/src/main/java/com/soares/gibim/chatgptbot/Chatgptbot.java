10412309f21e3cd78adde52c94608e67e6de2e80
SauloFernando99 <saulofernando114@gmail.com>
2024-05-04 21:34:59 -0300

    feat: implemented the class with BotServiceProvider

--- bot-impl/src/main/java/com/soares/gibim/chatgptbot/Chatgptbot.java
+++ bot-impl/src/main/java/com/soares/gibim/chatgptbot/Chatgptbot.java
@@ -1,4 +1,27 @@
 package com.soares.gibim.chatgptbot;
 
-public class Chatgptbot {
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class Chatgptbot implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
 }
