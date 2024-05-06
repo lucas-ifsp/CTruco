cdec737c4cfd279273cc1260055ee03bc9d38cba
User <gicagarcia58@gmail.com>
2024-05-06 10:17:07 -0300

    feat: merge the utils file from gica branch to get new functions

--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
@@ -9,4 +9,30 @@ public class SlayerBotUtils {
     public List<TrucoCard> getManilhas(List<TrucoCard> cards, TrucoCard vira){
         return cards.stream().filter(card -> card.isManilha(vira)).toList();
     }
+
+    public TrucoCard getStrongestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard strongerCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(strongerCard, vira);
+            if (comparison > 0) {
+                strongerCard = card;
+            }
+        }
+
+        return strongerCard;
+    }
+
+    public TrucoCard getWeakestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard weakerCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(weakerCard, vira);
+            if (comparison < 0) {
+                weakerCard = card;
+            }
+        }
+
+        return weakerCard;
+    }
 }
