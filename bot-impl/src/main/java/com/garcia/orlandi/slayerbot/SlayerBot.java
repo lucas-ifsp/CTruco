edcc8b3107a76ec98ace9243cdc5aea21a9e0fbd
guizinsx <guizin51@gmail.com>
2024-05-06 03:10:06 -0300

    feat: one more util method created playWeakestManilha and created logic for chooseCard

--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
@@ -10,6 +10,9 @@ import java.util.List;
 import java.util.Optional;
 
 public class SlayerBot implements BotServiceProvider {
+
+    SlayerBotUtils utils = new SlayerBotUtils();
+
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
@@ -22,6 +25,28 @@ public class SlayerBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        List<TrucoCard> openCards = intel.getOpenCards();
+
+        if (!openCards.isEmpty()) {
+            TrucoCard opponentCard = openCards.get(0);
+            List<TrucoCard> manilhas = utils.getManilhas(cards, vira);
+
+            // encontrar nao manilha que possa ganhar
+            Optional<TrucoCard> winningNonManilha = cards.stream()
+                    .filter(card -> !card.isManilha(vira) && card.compareValueTo(opponentCard, vira) > 0)
+                    .findFirst();
+
+            if (winningNonManilha.isPresent()) {
+                // Se existe uma nao manilha que pode ganhar, joga ela
+                return CardToPlay.of(winningNonManilha.get());
+            } else {
+                // Caso contrario, joga a manilha mais fraca
+                return utils.playWeakestManilha(manilhas);
+            }
+        }
+
         return null;
     }
 
--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
@@ -1,12 +1,26 @@
 package com.garcia.orlandi.slayerbot;
 
+import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.TrucoCard;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SlayerBotUtils {
 
-    public List<TrucoCard> getManilhas(List<TrucoCard> cards, TrucoCard vira){
-        return cards.stream().filter(card -> card.isManilha(vira)).toList();
+    public List<TrucoCard> getManilhas(List<TrucoCard> cards, TrucoCard vira) {
+        List<TrucoCard> manilhas = cards.stream()
+                .filter(card -> card.isManilha(vira))
+                .collect(Collectors.toList());
+        System.out.println("Manilhas encontradas: " + manilhas);
+        return manilhas;
+    }
+
+    public CardToPlay playWeakestManilha(List<TrucoCard> manilhas) {
+        return manilhas.stream()
+                .min(Comparator.comparing(TrucoCard::getRank))
+                .map(CardToPlay::of)
+                .orElse(CardToPlay.of(manilhas.get(0)));
     }
 }
