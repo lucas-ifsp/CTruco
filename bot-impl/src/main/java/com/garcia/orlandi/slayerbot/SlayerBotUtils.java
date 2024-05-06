d804249b7194cde6e77bad5436ef1248b5ecb739
User <gicagarcia58@gmail.com>
2024-05-06 09:28:35 -0300

    feat: add functions to get strongest and weakest card

--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBotUtils.java
@@ -9,4 +9,30 @@ public class SlayerBotUtils {
     public List<TrucoCard> getManilhas(List<TrucoCard> cards, TrucoCard vira){
         return cards.stream().filter(card -> card.isManilha(vira)).toList();
     }
+
+    private TrucoCard getStrongestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard strongerCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(strongerCard, vira);
+            if (comparison > 0) {
+                strongerCard = card;
+            }
+        }
+
+        return strongerCard;
+    }
+
+    private TrucoCard getWeakestCard( List<TrucoCard> cards, TrucoCard vira){
+        TrucoCard weakerCard = cards.get(0);
+
+        for (TrucoCard card : cards) {
+            int comparison = card.compareValueTo(weakerCard, vira);
+            if (comparison < 0) {
+                weakerCard = card;
+            }
+        }
+
+        return weakerCard;
+    }
 }
