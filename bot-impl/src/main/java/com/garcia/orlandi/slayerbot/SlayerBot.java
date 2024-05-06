43fc261c33d0ac9518d6e8c81c2f7eb0981ba65c
User <gicagarcia58@gmail.com>
2024-05-05 21:41:14 -0300

    feat: minimum implementation to not play zap in first hand

--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
@@ -2,9 +2,17 @@ package com.garcia.orlandi.slayerbot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
+import com.garcia.orlandi.slayerbot.SlayerBotUtils.*;
+
+import java.util.List;
+import java.util.Optional;
 
 public class SlayerBot implements BotServiceProvider {
+
+    SlayerBotUtils utils = new SlayerBotUtils();
+
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
@@ -17,6 +25,22 @@ public class SlayerBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        List<TrucoCard> openCards = intel.getOpenCards();
+        List<GameIntel.RoundResult> roundResult = intel.getRoundResults();
+
+        List<TrucoCard> manilhas = utils.getManilhas(cards, vira);
+
+        if(!manilhas.isEmpty()){
+            if(openCards.size() == 1){
+                TrucoCard chosenCard = cards.stream()
+                        .filter(card -> !card.isZap(vira))
+                        .findFirst()
+                        .orElse(null);
+                return CardToPlay.of(chosenCard);
+            }
+        }
         return null;
     }
 
