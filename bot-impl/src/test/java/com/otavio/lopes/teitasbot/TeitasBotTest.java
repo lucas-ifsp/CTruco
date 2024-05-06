ce6166814846b0c696e1957274af272e09698351
otaviopl <otavio.pereira.lopes@gmail.com>
2024-05-05 22:30:45 -0300

    TEST: add new raise test ... raise with we got advantage

--- bot-impl/src/test/java/com/otavio/lopes/teitasbot/TeitasBotTest.java
+++ bot-impl/src/test/java/com/otavio/lopes/teitasbot/TeitasBotTest.java
@@ -1,4 +1,19 @@
 package com.otavio.lopes.teitasbot;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 public class TeitasBotTest {
+    @Nested
+    @DisplayName("DecidedIfRaises")
+    class DecideIfRaises{
+        TeitasBot teitasBot = new TeitasBot();
+
+        @Test
+        @DisplayName("Should raise if we got advantage")
+        void shouldRaiseIfAdvantage(){
+            
+        }
+    }
 }
