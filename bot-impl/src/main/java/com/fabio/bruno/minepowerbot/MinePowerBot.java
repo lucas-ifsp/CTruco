f5248f392abc9d69a5f466f605b88a9b52a219b3
Brun0HC <bruno.cobuci@biotronica.tech>
2024-05-06 20:30:04 -0300

    feat: implement logic to dont raise bet when is mao de 11

--- bot-impl/src/main/java/com/fabio/bruno/minepowerbot/MinePowerBot.java
+++ bot-impl/src/main/java/com/fabio/bruno/minepowerbot/MinePowerBot.java
@@ -26,17 +26,19 @@ public class MinePowerBot implements BotServiceProvider {
         var botScore = intel.getScore();
         var opponentScore = intel.getOpponentScore();
         var countManilhas = listManilhas(intel).size();
-        if(intel.getScore() > opponentScore + 3)
-            return true;
-        if(opponentScore <= 5)
-            return true;
-        if (countManilhas >=2)
-            return true;
-        if (getCardAboveRank(intel, CardRank.TWO).size() >= 2)
-            return true;
-        if (manilhaFraca != null)
-            if (botScore == opponentScore || botScore > opponentScore) {
+        if(intel.getScore()<11) {
+            if (intel.getScore() > opponentScore + 3)
                 return true;
+            if (opponentScore <= 5)
+                return true;
+            if (countManilhas >= 2)
+                return true;
+            if (getCardAboveRank(intel, CardRank.TWO).size() >= 2)
+                return true;
+            if (manilhaFraca != null)
+                if (botScore == opponentScore || botScore > opponentScore) {
+                    return true;
+                }
         }
         return false;
     }
