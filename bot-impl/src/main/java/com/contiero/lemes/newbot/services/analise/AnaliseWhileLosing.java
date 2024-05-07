91bbad7cd08e9f477268fa534ab76b1516629364
gcontiero11 <gustcontiero@gmail.com>
2024-05-07 14:34:36 -0300

    feature: AnaliseWhileLosing implemented

--- bot-impl/src/main/java/com/contiero/lemes/newbot/services/analise/AnaliseWhileLosing.java
+++ bot-impl/src/main/java/com/contiero/lemes/newbot/services/analise/AnaliseWhileLosing.java
@@ -17,6 +17,82 @@ public class AnaliseWhileLosing implements Analise {
     @Override
     public HandStatus myHand() {
         List<TrucoCard> myCards = intel.getCards();
-        return HandStatus.GOD;
+        if (myCards.size() == 3){
+            return threeCardsHandler();
+        }
+        if (myCards.size() == 2){
+            return twoCardsHandler();
+        }
+        else{
+            return oneCardHandler();
+        }
+    }
+
+    private HandStatus threeCardsHandler(){
+        if (haveAtLeastTwoManilhas()){
+            return HandStatus.GOD;
+        }
+        if (haveAtLeastOneManilha()){
+            return HandStatus.GOOD;
+        }
+        else{
+            long handPower = powerOfTheTwoBestCards();
+            if (handPower >= 16){
+                return HandStatus.GOOD;
+            }
+            if (handPower >= 9) {
+                return HandStatus.MEDIUM;
+            }
+            return HandStatus.BAD;
+        }
+    }
+
+    private HandStatus twoCardsHandler(){
+        if (wonFirstRound()){
+            long handPower = powerOfTheTwoBestCards();
+            if (haveAtLeastOneManilha()){
+                return HandStatus.GOD;
+            }
+            if(handPower >= 12){
+                return HandStatus.GOOD ;
+            }
+            if (handPower >= 8){
+                return HandStatus.MEDIUM;
+            }
+            return HandStatus.BAD;
+        }
+        return HandStatus.MEDIUM;
+    }
+
+    private HandStatus oneCardHandler(){
+        return HandStatus.MEDIUM;
+    }
+
+    private boolean haveAtLeastTwoManilhas(){
+        return getManilhaAmount() >= 2;
+    }
+
+    private boolean haveAtLeastOneManilha(){
+        return getManilhaAmount() >= 1;
+    }
+
+    private long getManilhaAmount() {
+        List<TrucoCard> myCards = intel.getCards();
+        return myCards.stream()
+                .filter(card-> card.isManilha(intel.getVira()))
+                .count();
+    }
+
+    private long powerOfTheTwoBestCards(){
+        List<TrucoCard> myCards = intel.getCards();
+        return myCards.stream()
+                .sorted()
+                .mapToLong(card -> card.relativeValue(intel.getVira()))
+                .limit(2)
+                .sum();
+    }
+
+    private boolean wonFirstRound(){
+        return intel.getRoundResults().get(0) == GameIntel.RoundResult.WON;
     }
 }
