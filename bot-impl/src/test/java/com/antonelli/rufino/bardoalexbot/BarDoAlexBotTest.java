2f95db9067d3ebaa5cf364832db8e24cd05c3240
Mauricio Antonelli <pablojuanang@gmail.com>
2024-05-07 12:49:07 -0300

    added a test to verify if the BarDoAlexBot responds correctly to different game "scenarios"

--- bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
+++ bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import static com.bueno.spi.model.CardSuit.*;
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardSuit;
@@ -39,5 +40,31 @@ public class BarDoAlexBotTest {
                 new TrucoCard(4, ESPADA)
         ));
         assertThat(bot.chooseCard(intel)).isEqualTo(new TrucoCard(4, ESPADA));
+    }
+
+    @Test
+    public void testGetRaiseResponse() {
+        BarDoAlexBot bot = new BarDoAlexBot();
+        GameIntel intel = new GameIntel();
+        intel.setBotScore(11); // Bot atinge 11 pontos
+        assertEquals(-1, bot.getRaiseResponse(intel));
+
+        intel.setBotScore(0);
+        intel.setOpponentScore(11);
+        intel.setCards(Arrays.asList(
+                new TrucoCard(7, COPAS),
+                new TrucoCard(7, ESPADA),
+                new TrucoCard(3, ESPADA),
+                new TrucoCard(4, ESPADA)
+        ));
+        assertEquals(1, bot.getRaiseResponse(intel));
+
+        intel.setCards(Arrays.asList(
+                new TrucoCard(4, ESPADA),
+                new TrucoCard(5, ESPADA),
+                new TrucoCard(6, ESPADA),
+                new TrucoCard(7, ESPADA)
+        ));
+        assertEquals(1, bot.getRaiseResponse(intel));
     }
 }
