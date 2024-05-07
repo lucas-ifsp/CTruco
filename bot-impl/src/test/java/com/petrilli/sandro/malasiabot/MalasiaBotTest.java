54f81d06a92d7f94183d0c78b70386a94183d30d
SandroFerreiraDev <sandro.ferreira.developer@gmail.com>
2024-05-07 13:08:33 -0300

    test: Added new should play the manilha card against opponent test

--- bot-impl/src/test/java/com/petrilli/sandro/malasiabot/MalasiaBotTest.java
+++ bot-impl/src/test/java/com/petrilli/sandro/malasiabot/MalasiaBotTest.java
@@ -1,4 +1,5 @@
 package com.petrilli.sandro.malasiabot;
+
 import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,75 +16,94 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MalasiaBotTest {
-        private MalasiaBot malasiaBot;
+    private MalasiaBot malasiaBot;
 
-        List<TrucoCard> Mao = Arrays.asList(
-                TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
-                TrucoCard.of(CardRank.FOUR, CardSuit.SPADES),
-                TrucoCard.of(CardRank.KING, CardSuit.HEARTS));
-        TrucoCard opponentCard = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+    TrucoCard ThreeOfSpades = TrucoCard.of(CardRank.THREE, CardSuit.SPADES);
 
-        TrucoCard QueenOfDiamonds = TrucoCard.of(CardRank.QUEEN, CardSuit.DIAMONDS);
+    TrucoCard FourOfDiamonds = TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS);
+
+    TrucoCard QueenOfDiamonds = TrucoCard.of(CardRank.QUEEN, CardSuit.DIAMONDS);
 
 
-        List<GameIntel.RoundResult> roundResultsFirstHand = List.of(
-        );
-        List<GameIntel.RoundResult> roundResults1Win = List.of(
-                GameIntel.RoundResult.WON
-        );
+    List<GameIntel.RoundResult> roundResultsFirstHand = List.of(
+    );
+    List<GameIntel.RoundResult> roundResults1Win = List.of(
+            GameIntel.RoundResult.WON
+    );
 
-        List<GameIntel.RoundResult> roundResults1Lose = List.of(
-                GameIntel.RoundResult.LOST
-        );
+    List<GameIntel.RoundResult> roundResults1Lose = List.of(
+            GameIntel.RoundResult.LOST
+    );
 
-        List<GameIntel.RoundResult> roundResultsTie = List.of(
-                GameIntel.RoundResult.DREW
-        );
+    List<GameIntel.RoundResult> roundResultsTie = List.of(
+            GameIntel.RoundResult.DREW
+    );
 
-        List<TrucoCard> openCardsLow = Arrays.asList(
-                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
-                TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
+    List<GameIntel.RoundResult> roundResultsWinLose = List.of(
+            GameIntel.RoundResult.WON,
+            GameIntel.RoundResult.LOST
+    );
+    List<GameIntel.RoundResult> roundResultsLoseWin = List.of(
+            GameIntel.RoundResult.LOST,
+            GameIntel.RoundResult.WON
+    );
 
-        private GameIntel.StepBuilder stepBuilder;
+    List<TrucoCard> openCardsLow = Arrays.asList(
+            TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
+            TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS));
 
-        @BeforeEach
-        public void setUp(){
-            malasiaBot = new MalasiaBot();
-        }
+    List<TrucoCard> openCardsEmpty = List.of();
 
-        /*@Test
-        @DisplayName("Can kill opponent card")
-        void canKillOpponentcard() {
-            TrucoCard Vira = TrucoCard.of(CardRank.FIVE, CardSuit.SPADES);
 
-            List<TrucoCard> botCards;
+    private GameIntel.StepBuilder stepBuilder;
 
-            stepBuilder  = GameIntel.StepBuilder.with()
-                    .gameInfo(roundResultsFirstHand, openCardsLow, Vira, 1)
+    @BeforeEach
+    public void setUp() {
+        malasiaBot = new MalasiaBot();
+    }
+
+    @Nested
+    @DisplayName("choose card tests")
+    class ChooseCardTests {
+        @Test
+        @DisplayName("Should play the lowest winning card against opponent")
+        void shouldPlayTheLowestWinningCardAgainstOpponentTest() {
+            TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+            List<TrucoCard> Mao = Arrays.asList(
+                    TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.FOUR, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.HEARTS));
+
+            stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(roundResultsFirstHand, openCardsEmpty, vira, 1)
                     .botInfo(Mao, 0)
-                    .opponentScore(0);
+                    .opponentScore(0)
+                    .opponentCard(FourOfDiamonds);
 
-        }*/
-
-        @Nested
-        @DisplayName("choose card tests")
-        class ChooseCardTests {
-            @Test
-            @DisplayName("Should play the lowest winning card against opponent")
-            void shouldPlayTheLowestWinningCardAgainstOpponentTest(){
-                TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
-
-                List<TrucoCard> openCards = Arrays.asList(
-                        TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS),
-                        TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS));
-
-                stepBuilder = GameIntel.StepBuilder.with()
-                        .gameInfo(List.of(GameIntel.RoundResult.DREW), openCards, vira, 1)
-                        .botInfo(Mao, 9)
-                        .opponentScore(3)
-                        .opponentCard(opponentCard);
-
-                assertEquals(CardRank.KING, malasiaBot.chooseCard(stepBuilder.build()).content().getRank());
-            }
+            assertEquals(CardRank.THREE, malasiaBot.chooseCard(stepBuilder.build()).content().getRank());
         }
+
+
+        @Test
+        @DisplayName("Should play the lowest manilha card against opponent")
+        void shouldPlayTheLowestManilhaCardAgainstOpponentTest() {
+            TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
+
+            List<TrucoCard> Mao = Arrays.asList(
+                    TrucoCard.of(CardRank.TWO, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.THREE, CardSuit.HEARTS));
+
+            stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(roundResultsFirstHand, openCardsEmpty, vira, 1)
+                    .botInfo(Mao, 0)
+                    .opponentScore(0)
+                    .opponentCard(ThreeOfSpades);
+
+            assertEquals(CardRank.TWO, malasiaBot.chooseCard(stepBuilder.build()).content().getRank());
+            assertEquals(CardSuit.SPADES, malasiaBot.chooseCard(stepBuilder.build()).content().getSuit());
+        }
+
+    }
 }
