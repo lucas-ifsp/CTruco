01236564d770adf784fdcf7954167af2df4bc51d
Mauricio Antonelli <pablojuanang@gmail.com>
2024-05-07 12:18:08 -0300

    first method.

--- bot-impl/src/main/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBot.java
+++ bot-impl/src/main/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBot.java
@@ -1,4 +1,18 @@
 package com.antonelli.rufino.bardoalexbot;
 
+import com.bueno.spi.model.*;
+import java.util.List;
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
 public class BarDoAlexBot {
+
+    public boolean decideIfRaises(GameIntel intel) {
+        TrucoCard vira = intel.getVira();
+        List<TrucoCard> cards = intel.getCards();
+        long manilhasCount = cards.stream().filter(card -> card.isManilha(vira)).count();
+        return manilhasCount >= 3;
+    }
+
 }
