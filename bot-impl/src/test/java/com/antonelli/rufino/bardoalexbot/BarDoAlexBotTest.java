ac873c7d5f193c649d14179a59e261c844882884
Mauricio Antonelli <pablojuanang@gmail.com>
2024-05-07 12:12:08 -0300

    the first test checks if the BarDoAlexBot is smart enough to up the ante when it's got three or more of those special "manilha" cards in its hand.

--- bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
+++ bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
@@ -1,4 +1,24 @@
 package com.antonelli.rufino.bardoalexbot;
 
+import com.bueno.spi.model.*;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static com.bueno.spi.model.CardSuit.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BarDoAlexBotTest {
+
+    @Test
+    public void testDecideIfRaises() {
+        BarDoAlexBot bot = new BarDoAlexBot();
+        GameIntel intel = new GameIntel();
+        intel.setVira(new TrucoCard(1, ESPADA)); // Define a carta vira
+        intel.setCards(Arrays.asList(
+                new TrucoCard(7, COPAS),
+                new TrucoCard(7, ESPADA),
+                new TrucoCard(3, ESPADA),
+                new TrucoCard(4, ESPADA)
+        ));
+        assertTrue(bot.decideIfRaises(intel));
+    }
 }
