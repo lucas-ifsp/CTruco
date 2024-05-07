7b518feacb4f1d26787e91db0c0d59b4d2b827e3
User <gicagarcia58@gmail.com>
2024-05-07 17:54:33 -0300

    test: should play a three if has no manilhas and two threes being the first to play

--- bot-impl/src/test/java/com/garcia/orlandi/slayerbot/SlayerBotTest.java
+++ bot-impl/src/test/java/com/garcia/orlandi/slayerbot/SlayerBotTest.java
@@ -151,4 +151,25 @@ public class SlayerBotTest {
         assertThat(chosenCard).isEqualTo(TrucoCard.of(SEVEN, SPADES));
     }
 
+    @Test
+    @DisplayName("If first to play and with no manilha but has two 3s at hand, play one of them")
+    void shouldPlayThreeIfFirstToPlayWithNoManilhaButHasTwoThrees(){
+        roundResults = List.of();
+        vira = TrucoCard.of(FIVE, HEARTS);
+        cards = List.of(
+                TrucoCard.of(THREE, DIAMONDS),
+                TrucoCard.of(THREE, HEARTS),
+                TrucoCard.of(SEVEN, SPADES));
+        openCards = List.of(vira);
+
+        stepBuilder = GameIntel.StepBuilder
+                .with()
+                .gameInfo(roundResults, openCards, vira, 1)
+                .botInfo(cards, 0).opponentScore(0);
+
+        CardToPlay card = new SlayerBot().chooseCard(stepBuilder.build());
+        TrucoCard chosenCard = card.value();
+        assertThat(chosenCard).isNotEqualTo(TrucoCard.of(SEVEN, SPADES));
+    }
+
 }
