cbf54533699179acca43be4dd157585849641d80
Mauricio Antonelli <pablojuanang@gmail.com>
2024-05-07 15:08:55 -0300

    implements

--- bot-impl/src/main/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBot.java
+++ bot-impl/src/main/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBot.java
@@ -6,7 +6,7 @@ import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.service.BotServiceProvider;
 
-public class BarDoAlexBot {
+public class BarDoAlexBot implements BotServiceProvider {
 
     public boolean decideIfRaises(GameIntel intel) {
         TrucoCard vira = intel.getVira();
