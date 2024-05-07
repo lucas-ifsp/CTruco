35e20791252f1fa86bc2317676c285c3536be134
Mauricio Antonelli <pablojuanang@gmail.com>
2024-05-07 12:42:14 -0300

    added a test to verify if the BarDoAlexBot chooses the last card correctly

--- bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
+++ bot-impl/src/test/java/com/antonelli/rufino/bardoalexbot/BarDoAlexBotTest.java
@@ -4,7 +4,12 @@ import com.bueno.spi.model.*;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import static com.bueno.spi.model.CardSuit.*;
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 
 public class BarDoAlexBotTest {
 
@@ -20,5 +25,19 @@ public class BarDoAlexBotTest {
                 new TrucoCard(4, ESPADA)
         ));
         assertTrue(bot.decideIfRaises(intel));
+    }
+
+    @Test
+    public void testChooseCard() {
+        BarDoAlexBot bot = new BarDoAlexBot();
+        GameIntel intel = new GameIntel();
+        intel.setVira(new TrucoCard(1, ESPADA)); // Define a carta vira
+        intel.setCards(Arrays.asList(
+                new TrucoCard(7, COPAS),
+                new TrucoCard(7, ESPADA),
+                new TrucoCard(3, ESPADA),
+                new TrucoCard(4, ESPADA)
+        ));
+        assertThat(bot.chooseCard(intel)).isEqualTo(new TrucoCard(4, ESPADA));
     }
 }
