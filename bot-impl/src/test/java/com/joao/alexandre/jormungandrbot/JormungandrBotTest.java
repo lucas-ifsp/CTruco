f97e51b10910877c313355147434c008509ccccd
keilo <keilogamimg@gmail.com>
2024-05-08 15:02:04 -0300

    chore: setting up config method for testing

--- bot-impl/src/test/java/com/joao/alexandre/jormungandrbot/JormungandrBotTest.java
+++ bot-impl/src/test/java/com/joao/alexandre/jormungandrbot/JormungandrBotTest.java
@@ -1,7 +1,18 @@
 package com.joao.alexandre.jormungandrbot;
 
+import com.bueno.spi.model.GameIntel;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JormungandrBotTest {
+    private JormungandrBot jormungandrBot;
+    private GameIntel.StepBuilder stepBuilder;
+
+    @BeforeEach
+    public void config() {
+        jormungandrBot = new JormungandrBot();
+
+    }
 
 }
