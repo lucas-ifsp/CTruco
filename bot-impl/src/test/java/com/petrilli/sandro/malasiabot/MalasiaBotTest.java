a0b551dc2f9ef22064aa3d837b63f64a3e01c7f9
Pedro Petrilli <ppetrilli96@gmail.com>
2024-05-08 11:09:28 -0300

    test: add test to check if accepts mao de onze with MaoZapOuCopasEFiguras  hand

--- bot-impl/src/test/java/com/petrilli/sandro/malasiabot/MalasiaBotTest.java
+++ bot-impl/src/test/java/com/petrilli/sandro/malasiabot/MalasiaBotTest.java
@@ -81,6 +81,24 @@ public class MalasiaBotTest {
 
             assertTrue(malasiaBot.getMaoDeOnzeResponse(stepBuilder.build()));
         }
+
+        @Test
+        @DisplayName("Should accept mao de onze with MaoZapOuCopasEFiguras hand")
+        void shouldAcceptMaoDeOnzeWithMaoZapOuCopasEFigurasHand() {
+            TrucoCard vira = TrucoCard.of(CardRank.THREE, CardSuit.SPADES);
+
+            List<TrucoCard> Mao = Arrays.asList(
+                    TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.JACK, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.TWO, CardSuit.HEARTS));
+
+            stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(roundResultsFirstHand, openCardsEmpty, vira, 1)
+                    .botInfo(Mao, 0)
+                    .opponentScore(0);
+
+            assertTrue(malasiaBot.getMaoDeOnzeResponse(stepBuilder.build()));
+        }
     }
 
     @Nested
