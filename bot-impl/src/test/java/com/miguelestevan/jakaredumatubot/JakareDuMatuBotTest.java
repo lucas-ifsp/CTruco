b87de263421f0e2fa63eda6decc8dfdb29ce29eb
Estevan Sousa <estevanmartins03@gmail.com>
2024-05-08 13:52:08 -0300

    refactor: refactor test name to ShouldPlayTheMoreWeakestCardPossibleIfCanWinOpponentFirstCardInFirstHand

--- bot-impl/src/test/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBotTest.java
+++ bot-impl/src/test/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBotTest.java
@@ -218,8 +218,8 @@ class JakareDuMatuBotTest {
         //Adversario primeiro
         //Se consigo matar a primeira carta do adversario, matar com a mais fraca possivel
         @Test
-        @DisplayName("Should play the weakest card if can win opponent first card in first hand")
-        public void ShouldPlayTheWeakestCardIfCanWinOpponentFirstCardInFirstHand(){
+        @DisplayName("Should play the more weakest card possible if can win opponent first card in first hand")
+        public void ShouldPlayTheMoreWeakestCardPossibleIfCanWinOpponentFirstCardInFirstHand(){
             TrucoCard vira = TrucoCard.of(CardRank.JACK, CardSuit.DIAMONDS);
 
             // Game info
