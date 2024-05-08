0635a31e7e3b832c4dfa0a631b094a7fabed3f58
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-05-08 08:56:44 -0300

    add license

--- bot-impl/src/main/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecida.java
+++ bot-impl/src/main/java/com/barbara/lucasCruz/patriciaAparecida/PatriciaAparecida.java
@@ -1,3 +1,25 @@
+/*
+ *  Copyright (C) 2024 Bárbara Branc oGasques - IFSP/SCL and Lucas da Silva Cruz - IFSP/SCL
+ *  Contact: barbara <dot> branco <at> aluno <dot> ifsp <dot> edu <dot> br
+ *  Contact: a <dot> strapasson <at> aluno <dot> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+
 package com.barbara.lucasCruz.patriciaAparecida;
 
 import com.bueno.spi.model.*;
