3984bb327bb2f1442367763ea8a6d7c12af3f0bb
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-05-08 15:11:37 -0300

    test: created test for checking if has manilha and a high card

--- bot-impl/src/test/java/com/lucasmurilo/akkosocorrompido/AkkosocorrompidoTest.java
+++ bot-impl/src/test/java/com/lucasmurilo/akkosocorrompido/AkkosocorrompidoTest.java
@@ -1,16 +1,57 @@
 package com.lucasmurilo.akkosocorrompido;
 
 
+import static com.bueno.spi.model.CardRank.THREE;
+import static com.bueno.spi.model.CardSuit.HEARTS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
-
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.lucasmurilo.m.lazarinipodenciano.Akkosocorrompido;
 
 public class AkkosocorrompidoTest {
     private Akkosocorrompido bot;
+    @Mock
+    GameIntel intel;
 
     @BeforeEach
     public void setup() {
         bot = new Akkosocorrompido();
     }
+
+    //truca ao invez disso
+    @Test
+    @DisplayName("Should start with second best card if has high card and manilha")
+    public void shouldReturnsSecondBestCardIfHaveManilhaAndHigh() {        
+        TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS); 
+
+        List<TrucoCard> botCards = Arrays.asList(
+                    TrucoCard.of(CardRank.THREE, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.JACK, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.QUEEN, CardSuit.CLUBS)
+            );
+
+        List<TrucoCard> openCards = List.of(vira);
+
+        GameIntel intel = GameIntel.StepBuilder.with()
+            .gameInfo(List.of(), openCards, vira, 1)
+            .botInfo(botCards, 5)
+            .opponentScore(3)
+            .build();
+
+        assertEquals(TrucoCard.of(THREE, HEARTS), bot.chooseCard(intel).value().toString());
+    }
+
+    
 }
