b7a92a35cf9200c3df318e92bb62d9d7bff2e9d8
User <gicagarcia58@gmail.com>
2024-05-07 21:27:48 -0300

    feat(refactor): adjusted code to work for new test

--- bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
+++ bot-impl/src/main/java/com/garcia/orlandi/slayerbot/SlayerBot.java
@@ -48,7 +48,7 @@ public class SlayerBot implements BotServiceProvider {
                 TrucoCard strongestCard = utils.getStrongestCard(cards, vira);
                 TrucoCard secondStrongestCard = utils.getSecondStrongestCard(cards, strongestCard, vira);
                 return CardToPlay.of(secondStrongestCard);
-            } else if (roundResult.size() == 1 && roundResult.contains(GameIntel.RoundResult.WON)) {
+            } else  {
                 for (TrucoCard card : cards) {
                     //Testing in order to play best manilha first, if found
                     if (card.isZap(vira) || card.isCopas(vira) || card.isEspadilha(vira) || card.isOuros(vira)) {
@@ -59,54 +59,6 @@ public class SlayerBot implements BotServiceProvider {
                 return CardToPlay.of(utils.getStrongestCard(cards, vira));
             }
         }
-//        if(openCards.size() == 1 && roundResult.isEmpty()){
-//            if(!manilhas.isEmpty()){
-//                List<TrucoCard> cardsWithoutZap = cards.stream()
-//                        .filter(card -> !card.isZap(vira))
-//                        .toList();
-//
-//                if(cardsWithoutZap.size() == 2){
-//                    for(TrucoCard card : cardsWithoutZap){
-//                        if(card.isCopas(vira) || card.isEspadilha(vira) || card.isOuros(vira)){
-//                            return CardToPlay.of(card);
-//                        }
-//                    }
-//                    TrucoCard secondStrongestCard = utils.getStrongestCard(cardsWithoutZap, vira);
-//                    return CardToPlay.of(secondStrongestCard);
-//                } else if (cardsWithoutZap.size() == 3) {
-//                    if(manilhas.size() == 3 || manilhas.size() ==1){
-//                        TrucoCard strongestManilha = utils.getStrongestCard(cards, vira);
-//                        TrucoCard secondStrongestManilha = utils.getSecondStrongestCard(cards, strongestManilha, vira);
-//                        return CardToPlay.of(secondStrongestManilha);
-//                    } else if(manilhas.size() == 2){
-//                        TrucoCard weakestManilha = utils.getWeakestCard(manilhas, vira);
-//                        return CardToPlay.of((weakestManilha));
-//                    }
-//                }
-//
-//                TrucoCard chosenCard = cards.stream()
-//                        .filter(card -> !card.isZap(vira))
-//                        .findFirst()
-//                        .orElse(null);
-//
-//                return CardToPlay.of(chosenCard);
-//            }else{
-//                List<TrucoCard> threes = utils.getThreesAtHand(cards);
-//                if(!threes.isEmpty()){
-//                    if(threes.size() == 2){
-//                        return CardToPlay.of(threes.get(0));
-//                    }else if(threes.size() == 1) {
-//                        TrucoCard secondStrongestCard = utils.getSecondStrongestCard(cards, threes.get(0), vira);
-//                        return CardToPlay.of(secondStrongestCard);
-//
-//                    }
-//                }else{
-//                    TrucoCard strongestCard = utils.getStrongestCard(cards, vira);
-//                    TrucoCard secondStrongestCard = utils.getSecondStrongestCard(cards, strongestCard, vira);
-//                    return CardToPlay.of(secondStrongestCard);
-//                }
-//            }
-//        }
         return null;
     }
 
