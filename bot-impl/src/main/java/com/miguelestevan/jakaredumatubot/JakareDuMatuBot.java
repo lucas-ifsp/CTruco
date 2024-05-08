de29811c07c96b71222e189781d01531bbba7073
Estevan Sousa <estevanmartins03@gmail.com>
2024-05-08 18:04:33 -0300

    :sparkles: refactor: refactor chooseCard to return the strongest card in first hand when our bot play first

--- bot-impl/src/main/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBot.java
+++ bot-impl/src/main/java/com/miguelestevan/jakaredumatubot/JakareDuMatuBot.java
@@ -58,8 +58,8 @@ public class JakareDuMatuBot implements BotServiceProvider {
         switch (intel.getRoundResults().size()) {
             case 0 -> {
                 // First Hand
+                List<TrucoCard> list = sortedListCards(intel, intel.getVira());
                 if (intel.getOpponentCard().isPresent()){
-                    List<TrucoCard> list = sortedListCards(intel, intel.getVira());
                     if (hasCardHigherThan(intel, intel.getOpponentCard().get())){
                         if (list.get(2).compareValueTo(intel.getOpponentCard().get(), intel.getVira()) > 0){
                             return CardToPlay.of(list.get(2));
@@ -77,10 +77,8 @@ public class JakareDuMatuBot implements BotServiceProvider {
                             return CardToPlay.of(list.get(2));
                         }
                     }
-                }else {
-
                 }
-                return null;
+                return CardToPlay.of(list.get(0));
             }
             case 1->{
                 // Second Hand
