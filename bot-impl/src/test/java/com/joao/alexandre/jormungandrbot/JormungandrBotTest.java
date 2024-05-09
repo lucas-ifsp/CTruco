34fa60704107bb5cfa6443ff5331e24079ad931f
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2024-05-08 23:44:30 -0300

    test: testing hasCopasAndZapInHand with shouldReturnFalseIfHandHasTwoLowManilhas test

--- bot-impl/src/test/java/com/joao/alexandre/jormungandrbot/JormungandrBotTest.java
+++ bot-impl/src/test/java/com/joao/alexandre/jormungandrbot/JormungandrBotTest.java
@@ -1496,5 +1496,22 @@ class JormungandrBotTest {
 
             assertTrue(jormungandrBot.hasCopasAndZapInHand(stepBuilder.build()));
         }
+        @Test
+        @DisplayName("Should return false if hand has two random non zap&copas manilhas")
+        void shouldReturnFalseIfHandHasTwoLowManilhas(){
+            TrucoCard vira = TrucoCard.of(CardRank.JACK, CardSuit.DIAMONDS);
+
+            List<TrucoCard> myCards = List.of(
+                    TrucoCard.of(CardRank.KING, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.KING, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.ACE, CardSuit.CLUBS));
+
+            stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(),List.of(), vira, 1)
+                    .botInfo(myCards, 1)
+                    .opponentScore(0);
+
+            assertFalse(jormungandrBot.hasCopasAndZapInHand(stepBuilder.build()));
+        }
     }
 }
