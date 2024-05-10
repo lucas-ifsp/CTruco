350d60ec59b919b88f08da4f99efb5e4f07984e1
gcontiero11 <gustcontiero@gmail.com>
2024-05-10 18:52:35 -0300

    refact: refactoring AgressieChoosing implementation adding MyCards class on the constructor

--- bot-impl/src/main/java/com/contiero/lemes/newbot/interfaces/Analise.java
+++ bot-impl/src/main/java/com/contiero/lemes/newbot/interfaces/Analise.java
@@ -1,9 +1,5 @@
 package com.contiero.lemes.newbot.interfaces;
 
-import com.bueno.spi.model.TrucoCard;
-
-import java.util.List;
-
 public interface Analise {
 
     enum HandStatus {BAD,MEDIUM,GOOD,GOD};
--- bot-impl/src/main/java/com/contiero/lemes/newbot/services/choose_card/AgressiveChoosing.java
+++ bot-impl/src/main/java/com/contiero/lemes/newbot/services/choose_card/AgressiveChoosing.java
@@ -4,81 +4,80 @@ import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 import com.contiero.lemes.newbot.interfaces.Choosing;
+import com.contiero.lemes.newbot.services.utils.MyCards;
 
 import java.util.List;
 
 public class AgressiveChoosing implements Choosing {
 
     private final GameIntel intel;
-    private TrucoCard vira;
-    private TrucoCard bestCard;
-    private TrucoCard secondBestCard;
-    private TrucoCard worstCard;
-    public AgressiveChoosing(GameIntel intel){
-        this.intel = intel;
-    }
+    private final TrucoCard vira;
+    private final TrucoCard bestCard;
+    private final TrucoCard secondBestCard;
+    private final TrucoCard worstCard;
 
-    public CardToPlay firstRoundChooseCard(GameIntel intel){
-        int bestCardValue = bestCard.relativeValue(vira);
-        int secondBestCardValue = secondBestCard.relativeValue(vira);
-        int worstCardValue = worstCard.relativeValue(vira);
-        CardToPlay playedCard = CardToPlay.of(bestCard);
-        if (intel.getOpponentCard().isPresent()){
-            int opponentCardOnTableValue = intel.getOpponentCard().get().relativeValue(vira);
-            if (worstCardValue >= opponentCardOnTableValue) playedCard = CardToPlay.of(worstCard);
-            else if (secondBestCardValue >= opponentCardOnTableValue) playedCard = CardToPlay.of(secondBestCard);
-            else if (bestCardValue > opponentCardOnTableValue) playedCard = CardToPlay.of(bestCard);
-        }
-        else {
-            if (haveAtLeastTwoManilhas()){
-                if (secondBestCardValue == 12 || secondBestCardValue == 11){
-                    playedCard = CardToPlay.of(worstCard);
-                }
-                else if (secondBestCardValue == 10){
-                    playedCard = CardToPlay.of(secondBestCard);
-                }
-            }
-            else if (haveAtLeastOneManilha()){
-                if (bestCardValue >= 11){
-                    playedCard = CardToPlay.of(secondBestCard);
-                }
-                else if (bestCardValue == 10){
-                    playedCard = CardToPlay.of(bestCard);
-                }
-            }
-            else {
-                long handPower = powerOfTheTwoBestCards();
-                if (handPower >= 16 && secondBestCardValue >= 8){
-                    playedCard = CardToPlay.of(secondBestCard);
-                }
-                else{
-                    playedCard = CardToPlay.of(bestCard);
-                }
-            }
-        }
-        return playedCard;
-    }
     @Override
     public CardToPlay choose(GameIntel intel) {
         return null;
     }
 
-    private boolean haveAtLeastTwoManilhas(){
+    public AgressiveChoosing(GameIntel intel) {
+        this.intel = intel;
+        vira = intel.getVira();
+
+        MyCards myCards = new MyCards(intel.getCards(),intel.getVira());
+
+        bestCard = myCards.getBestCard();
+        secondBestCard = myCards.getSecondBestCard();
+        worstCard = myCards.getWorstCard();
+    }
+
+    public CardToPlay firstRoundChooseCard(GameIntel intel) {
+        int bestCardValue = bestCard.relativeValue(vira);
+        int secondBestCardValue = secondBestCard.relativeValue(vira);
+        int worstCardValue = worstCard.relativeValue(vira);
+
+        if (intel.getOpponentCard().isPresent()) {
+            int opponentCardOnTableValue = intel.getOpponentCard().get().relativeValue(vira);
+
+            if (worstCardValue >= opponentCardOnTableValue) return CardToPlay.of(worstCard);
+            if (secondBestCardValue >= opponentCardOnTableValue) return CardToPlay.of(secondBestCard);
+            if (bestCardValue > opponentCardOnTableValue) return CardToPlay.of(bestCard);
+        }
+
+        if (haveAtLeastTwoManilhas()) {
+            if (secondBestCardValue == 12 || secondBestCardValue == 11) return CardToPlay.of(worstCard);
+            if (secondBestCardValue == 10) return CardToPlay.of(secondBestCard);
+        }
+
+        if (haveAtLeastOneManilha()) {
+            if (bestCardValue >= 11) return CardToPlay.of(secondBestCard);
+            if (bestCardValue == 10) return CardToPlay.of(bestCard);
+
+        }
+        long handPower = powerOfTheTwoBestCards();
+
+        if (handPower >= 16 && secondBestCardValue >= 8) return CardToPlay.of(secondBestCard);
+        return CardToPlay.of(bestCard);
+
+    }
+
+    private boolean haveAtLeastTwoManilhas() {
         return getManilhaAmount() >= 2;
     }
 
-    private boolean haveAtLeastOneManilha(){
+    private boolean haveAtLeastOneManilha() {
         return getManilhaAmount() >= 1;
     }
 
     private long getManilhaAmount() {
         List<TrucoCard> myCards = intel.getCards();
         return myCards.stream()
-                .filter(card-> card.isManilha(intel.getVira()))
+                .filter(card -> card.isManilha(intel.getVira()))
                 .count();
     }
 
-    private long powerOfTheTwoBestCards(){
+    private long powerOfTheTwoBestCards() {
         List<TrucoCard> myCards = intel.getCards();
         return myCards.stream()
                 .mapToLong(card -> card.relativeValue(intel.getVira()))
