85b5c9545eeb8ef1587b0c412e3e5269a809137c
Fernando Castro <129780150+wolferfoxx@users.noreply.github.com>
2024-05-12 15:37:00 -0300

    Update UnamedBot.java

--- bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
+++ bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
@@ -9,13 +9,34 @@ import java.util.List;
 public class UnamedBot implements BotServiceProvider{
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
-        return false;
+        return true;
     }
 
     @Override
-    public boolean decideIfRaises(GameIntel intel) {
-        return false;
+   public boolean decideIfRaises(GameIntel intel) {
+    sortHand(intel);
+    List<TrucoCard> cards = intel.getCards();
+    int sum = cards.stream().mapToInt(TrucoCard::relativeValue).sum();
+    int highValueCards = (int) cards.stream().filter(card -> card.relativeValue() > 9).count();
+    int currentRound = intel.getCurrentRound();
+
+    int raiseThreshold = 25;
+
+    if (currentRound > 0) {
+        raiseThreshold += currentRound * 5; 
     }
+
+    raiseThreshold += highValueCards * 2;
+
+    if (sum > raiseThreshold) {
+        return 1; // Re-raise or call
+    } else if (sum > 20) {
+        return 0; // Accept raise
+    } else {
+        return -1; // Quit
+    }
+}
+
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
