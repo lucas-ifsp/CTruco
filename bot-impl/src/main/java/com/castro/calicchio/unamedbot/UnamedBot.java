27dc7d5cc08232da71db55bb3c91958ee2aa73c6
Vitor0389 <134299283+Vitor0389@users.noreply.github.com>
2024-05-13 17:10:04 -0300

    arrumando bugs e formulando a logica

--- bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
+++ bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
@@ -4,89 +4,101 @@ import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class UnamedBot implements BotServiceProvider{
+    List <TrucoCard> cards = new ArrayList<>();
+    TrucoCard vira;
+
+    List<GameIntel.RoundResult> resultados = new ArrayList<>();
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
-        int totalPoints = intel.getScore();
+        vira = intel.getVira();
 
-        return totalPoints == 11;
+        return hasStrongCards(intel);
     }
 
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        TrucoCard vira = intel.getVira();
-        List<TrucoCard> cards = intel.getCards();
-        sortHand(intel);
-
-        int sum = cards.stream().mapToInt(card -> card.relativeValue(vira)).sum();
-        
-        long highValueCards = cards.stream().filter(card -> card.relativeValue(vira) > 9).count();
-        
-        List<GameIntel.RoundResult> roundResults = intel.getRoundResults();
-        int currentRound = roundResults.size();
-        
-        int raiseThreshold = 25;
-        
-        raiseThreshold += currentRound * 5;
-        raiseThreshold += highValueCards * 2;
-        
-        return sum > raiseThreshold;
+        return hasStrongCards(intel) || hasManilha(intel);
     }
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        List<TrucoCard> cards = intel.getCards();
-        TrucoCard vira = intel.getVira();
+        cards = intel.getCards();
+        vira = intel.getVira();
         Optional<TrucoCard> opponentCard = intel.getOpponentCard();
+        resultados = intel.getRoundResults();
 
-        List<GameIntel.RoundResult> roundResults = intel.getRoundResults();
-        int currentRound = roundResults.size() + 1;
 
+        int currentRound = resultados.size() + 1;
         sortHand(intel);
         TrucoCard strongestCard = cards.get(cards.size() - 1);
+        TrucoCard lowestCard = cards.get(0);
 
-        if(currentRound == 1){
-            if(opponentCard.isPresent()){
-                if(strongestCard.relativeValue(vira) > opponentCard.get().relativeValue(vira)){
-                    return CardToPlay.of(strongestCard);
-                }
-                else{
-                    return CardToPlay.discard(cards.get(0));
-                }
+        if(hasMoreThanOneManilha(intel)){
+            if(currentRound == 1){
+                CardToPlay.of(lowestCard);
+            } else{
+                CardToPlay.of(strongestCard);
             }
-            else{
-                if(strongestCard.relativeValue(vira) >= 8 && !strongestCard.isZap(vira)){
-                    return CardToPlay.of(strongestCard);
+        }
+        else{
+            if (currentRound == 1) {
+                if (opponentCard.isPresent()) {
+                    if (lowestCard.relativeValue(vira) > opponentCard.get().relativeValue(vira)) {
+                        return CardToPlay.of(lowestCard);
+                    }
+                    else{
+                        if(cards.get(1).relativeValue(vira) > opponentCard.get().relativeValue(vira)){
+                            return CardToPlay.of(cards.get(1));
+                        }
+                    }
                 }
                 else{
+                    if(!strongestCard.isZap(vira) || !strongestCard.isCopas(vira)){
+                        return CardToPlay.of(strongestCard);
+                    }
+                }
+            } else if (currentRound == 2) {
+                if (resultados.get(0) == GameIntel.RoundResult.WON) {
                     return CardToPlay.discard(cards.get(0));
+                }
+                else{
+                    if(opponentCard.isPresent()){
+                        if(cards.get(0).relativeValue(vira) > opponentCard.get().relativeValue(vira)){
+                            return CardToPlay.of(cards.get(0));
+                        }
+                        else{
+                            return CardToPlay.of(cards.get(1));
+                        }
+                    }
+                    else{
+                        return CardToPlay.of(cards.get(1));
+                    }
                 }
             }
         }
 
-
-        return CardToPlay.of(strongestCard);
+        return CardToPlay.of(cards.get(0));
     }
 
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
-        TrucoCard vira = intel.getVira();
-        List<TrucoCard> cards = intel.getCards();
-        sortHand(intel);
-        int sum = cards.stream().mapToInt(card -> card.relativeValue(vira)).sum();
-        if (sum > 20) {
+        if (hasStrongCards(intel) && hasManilha(intel)){
             return 1;
-        } else if (sum >= 10) {
-            return 0;
-        } else {
-            return -1;
         }
+        else{
+            if(hasOneStrongCard(intel)){
+                return 0;
+            };
+        }
+        return - 1;
     }
 
     public void sortHand(GameIntel intel){
@@ -95,8 +107,38 @@ public class UnamedBot implements BotServiceProvider{
         if (!cards.isEmpty()) {
             cards.sort(Comparator.comparingInt(card -> card.relativeValue(vira)));
         }
-        else{
-            throw new IllegalStateException("Sem cartas por enquanto!");
-        }
+    }
+
+    public boolean hasMoreThanOneManilha(GameIntel intel){
+        List <TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        cards = cards.stream().filter(card -> card.isManilha(vira)).toList();
+        return cards.size() == 2;
+    }
+
+    public boolean hasManilha(GameIntel intel){
+        List <TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        cards = cards.stream().filter(card -> card.isManilha(vira)).toList();
+
+        return !cards.isEmpty();
+    }
+    public boolean hasStrongCards(GameIntel intel){
+        cards = intel.getCards();
+        vira = intel.getVira();
+        cards = cards.stream().filter(card -> card.isManilha(vira) || card.relativeValue(vira) >= 7).toList();
+        return cards.size() >= 2;
+    }
+
+    public boolean hasOneStrongCard(GameIntel intel){
+        cards = intel.getCards();
+        vira = intel.getVira();
+        cards = cards.stream().filter(card -> card.getRank().value() > 9).toList();
+
+        return !cards.isEmpty();
+    }
+    public boolean ShitHand(GameIntel intel){
+        cards = intel.getCards();
+        vira = intel.getVira();
     }
 }
