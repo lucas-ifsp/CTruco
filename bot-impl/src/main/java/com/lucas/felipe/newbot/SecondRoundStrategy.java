2c368799cd1f58f29fbf5575e7d90d62df4fc5d3
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-05-12 21:30:02 -0300

    add copyright text

--- bot-impl/src/main/java/com/lucas/felipe/newbot/SecondRoundStrategy.java
+++ bot-impl/src/main/java/com/lucas/felipe/newbot/SecondRoundStrategy.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright (C) 2023 Lucas Pereira dos Santos and Felipe Santos Lourenço
+ *  Contact: lucas <dot> pereira3 <at> aluno <dot> ifsp <dot> edu <dot> br
+ *  Contact: santos <dot> lourenco <at> aluno <dot> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 package com.lucas.felipe.newbot;
 
 import com.bueno.spi.model.CardToPlay;
