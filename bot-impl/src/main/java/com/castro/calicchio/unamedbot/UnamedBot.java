87372e77349cb724db7b02d11ab4a3ad92191d16
Fernando Castro <129780150+wolferfoxx@users.noreply.github.com>
2024-05-13 00:04:35 -0300

    Update UnamedBot.java

--- bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
+++ bot-impl/src/main/java/com/castro/calicchio/unamedbot/UnamedBot.java
@@ -9,37 +9,61 @@ import java.util.List;
 public class UnamedBot implements BotServiceProvider{
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
-        return true;
+        List<TrucoCard> cards = intel.getCards();
+        
+        int totalPoints = calculateTotalPoints(cards);
+    
+        if (totalPoints == 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int calculateTotalPoints(List<TrucoCard> cards) {
+        int totalPoints = 0;
+        for (TrucoCard card : cards) {
+            totalPoints += card.getPoints();
+        }
+        return totalPoints;
     }
 
     @Override
-   public boolean decideIfRaises(GameIntel intel) {
-    sortHand(intel);
-    List<TrucoCard> cards = intel.getCards();
-    int sum = cards.stream().mapToInt(TrucoCard::relativeValue).sum();
-    int highValueCards = (int) cards.stream().filter(card -> card.relativeValue() > 9).count();
-    int currentRound = intel.getCurrentRound();
-
-    int raiseThreshold = 25;
-
-    if (currentRound > 0) {
-        raiseThreshold += currentRound * 5; 
+    public boolean decideIfRaises(GameIntel intel) {
+        sortHand(intel);
+        List<TrucoCard> cards = intel.getCards();
+        
+        int sum = cards.stream().mapToInt(TrucoCard::relativeValue).sum();
+        
+        long highValueCards = cards.stream().filter(card -> card.relativeValue() > 9).count();
+        
+        List<RoundResult> roundResults = intel.getRoundResults();
+        int currentRound = roundResults.size();
+        
+        int raiseThreshold = 25;
+        
+        raiseThreshold += currentRound * 5;
+        raiseThreshold += highValueCards * 2;
+        
+        return sum > raiseThreshold;
     }
-
-    raiseThreshold += highValueCards * 2;
-
-    if (sum > raiseThreshold) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        sortHand(intel);
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        
+        for (TrucoCard card : cards) {
+            if (card.beats(vira)) {
+                return new CardToPlay(card, PlayType.PLAY);
+            }
+        }
+    
+        TrucoCard lowestCard = cards.get(0);
+        return new CardToPlay(lowestCard, PlayType.DISCARD);
     }
+
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
@@ -57,6 +81,6 @@ public class UnamedBot implements BotServiceProvider{
 
     public void sortHand(GameIntel intel){
         List<TrucoCard> cards = intel.getCards();
-        cards.sort(TrucoCard::relativeValue);
+        cards.sort(Comparator.comparingInt(TrucoCard::relativeValue));
     }
 }
