1142f332eb72fd64a532a8c67329cb4b1c3eba48
fictorl <fictorlps@gmail.com>
2024-09-22 13:34:11 -0300

    chore: configure bot package following readme instructions

---
+++ bot-impl/src/main/java/com/adivic/octopus/Octopus.java
@@ -0,0 +1,28 @@
+package com.adivic.octopus;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class Octopus implements BotServiceProvider {
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+}
