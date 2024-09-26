fb2dd8a8e4a8d6046f5fe0006871fd28428c0f10
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-09-26 13:33:44 -0300

    feat: applying conditions to getMaoDeOnzeResponse

--- bot-impl/src/main/java/com/bianca/joaopedro/lgtbot/Lgtbot.java
+++ bot-impl/src/main/java/com/bianca/joaopedro/lgtbot/Lgtbot.java
@@ -3,10 +3,23 @@ package com.bianca.joaopedro.lgtbot;
 import com.bueno.spi.model.*;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.List;
+
 
 public class Lgtbot implements BotServiceProvider{
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        if (intel.getOpponentScore() < 7 && getStrongCards(intel, CardRank.JACK).size() == 3 &&
+                findManilhas(intel).size() > 0) {
+            return true;
+        }
+        if (intel.getOpponentScore() < 11 && getStrongCards(intel, CardRank.ACE).size() == 3 &&
+                findManilhas(intel).size() > 0){
+            return true;
+        }
+        if (intel.getOpponentScore() == 11){
+            return true;
+        }
         return false;
     }
 
@@ -24,5 +37,20 @@ public class Lgtbot implements BotServiceProvider{
     public int getRaiseResponse(GameIntel intel) {
         return 0;
     }
+
+    public List<TrucoCard> getStrongCards(GameIntel intel, CardRank referenceRank){
+        TrucoCard vira = intel.getVira();
+        return intel.getCards().stream()
+                .filter(card -> card.getRank().compareTo(referenceRank) > 0 || card.isManilha(vira))
+                .toList();
+    }
+
+    private List<TrucoCard> findManilhas(GameIntel intel) {
+        TrucoCard viraCard = intel.getVira();
+        return intel.getCards().stream()
+                .filter(carta -> carta.isManilha(viraCard))
+                .toList();
+    }
+
 }
 
