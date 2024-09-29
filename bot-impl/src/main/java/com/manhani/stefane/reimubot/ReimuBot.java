9b6685db972c72c6393e48039ba82315bf8933e7
gabrmanh <gmanhani@yahoo.com>
2024-09-29 17:39:04 -0300

    Initial creation of base ReimuBot

---
+++ bot-impl/src/main/java/com/manhani/stefane/reimubot/ReimuBot.java
@@ -0,0 +1,27 @@
+package com.manhani.stefane.reimubot;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class ReimuBot implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+}
