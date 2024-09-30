e194b0fbf3fb4590d56b4980606585a7de192adb
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-29 22:31:57 -0300

    feat: add new 'BotEco' implementation

---
+++ bot-impl/src/main/java/com/macena/mauricio/boteco/BotEco.java
@@ -0,0 +1,27 @@
+package com.macena.mauricio.boteco;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class BotEco implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+}
