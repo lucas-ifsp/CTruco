b80a71129aac64d1c6038c65c759f59283b01b4c
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-10-02 16:09:21 -0300

    feat: create test package and Bot test class structure.

---
+++ bot-impl/src/test/java/com/daniel/therover/TheRoverTest.java
@@ -0,0 +1,11 @@
+package com.daniel.therover;
+
+import com.bueno.spi.model.GameIntel;
+
+
+public class TheRoverTest {
+    private TheRover theRover;
+    private GameIntel.StepBuilder stepBuilder;
+
+
+}
