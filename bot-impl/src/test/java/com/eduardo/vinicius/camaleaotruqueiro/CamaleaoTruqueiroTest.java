c9939d16237753560c49ff9b778102068c11e399
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-02 16:23:34 -0300

    test: added test of method to return numbers of manilhas

--- bot-impl/src/test/java/com/eduardo/vinicius/camaleaotruqueiro/CamaleaoTruqueiroTest.java
+++ bot-impl/src/test/java/com/eduardo/vinicius/camaleaotruqueiro/CamaleaoTruqueiroTest.java
@@ -3,6 +3,7 @@ package com.eduardo.vinicius.camaleaotruqueiro;
 import com.bueno.spi.model.CardRank;
 import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.TrucoCard;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
@@ -12,6 +13,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CamaleaoTruqueiroTest {
 
@@ -39,9 +42,6 @@ public class CamaleaoTruqueiroTest {
     }
 
 
-
-
-
     //menor carta
     @Test
     @DisplayName("Should return the lowest card")
@@ -57,16 +57,41 @@ public class CamaleaoTruqueiroTest {
         TrucoCard lowest = camaleao.getLowestCard(cards, vira);
         assertThat(lowest).isEqualTo(TrucoCard.of(CardRank.QUEEN,CardSuit.CLUBS));
     }
-    
-    //estamos ganhando
 
-    //estamos perdendo
 
-    //temos manilha
-        // temos 0 manilha
-        // temos 1 manilha
-        // temos 2 manilha
-        // temos 3 manilha
+    @Test
+    @DisplayName("Shoult returno number of manilhas")
+    void shoultReturnoNumberOfManilhas() {
+        SoftAssertions softly = new SoftAssertions();
+        //Given
+        TrucoCard vir = TrucoCard.of(CardRank.THREE, CardSuit.HEARTS);
+        //When
+        List<TrucoCard> zeroManilhasCards = Arrays.asList(
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.QUEEN, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)
+        );
+        List<TrucoCard> oneManilhasCards = Arrays.asList(
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)
+        );
+        List<TrucoCard> twoManilhasCards = Arrays.asList(
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS)
+        );
+        List<TrucoCard> threeManilhasCards = Arrays.asList(
+                TrucoCard.of(CardRank.FOUR, CardSuit.SPADES),
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS)
+        );
+        //Then
+        softly.assertAll(zeroManilhasCards.numberOfManilhas()).isEqualTo(0);
+        softly.assertAll(oneManilhasCards.numberOfManilhas()).isEqualTo(1);
+        softly.assertAll(twoManilhasCards.numberOfManilhas()).isEqualTo(2);
+        softly.assertAll(threeManilhasCards.numberOfManilhas()).isEqualTo(3);
+    }
 
     //temos carta alta
         // temos 0 carta alta
@@ -75,4 +100,11 @@ public class CamaleaoTruqueiroTest {
         // temos 3 carta alta
 
     //temos carta baixa
+
+
+
+    //estamos ganhando
+
+    //estamos perdendo
+
 }
