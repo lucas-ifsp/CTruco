a1a129bc47de1cc5f0e683a9dde35a5c00664552
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-10-02 16:00:16 -0300

    feat: create packege and Bot class structure

---
+++ bot-impl/src/main/java/com/daniel/therover/TheRover.java
@@ -0,0 +1,34 @@
+package com.daniel.therover;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class TheRover implements BotServiceProvider {
+
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+
+        return CardToPlay.of(intel.getCards().get(0));
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "The Rover";
+    }
+}
