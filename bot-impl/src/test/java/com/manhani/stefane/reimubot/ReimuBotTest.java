578fd7f4e2363d3225478cd7e44b0686ab003763
gabrmanh <gmanhani@yahoo.com>
2024-10-02 21:41:56 -0300

    test: weakest card when cannot win

--- bot-impl/src/test/java/com/manhani/stefane/reimubot/ReimuBotTest.java
+++ bot-impl/src/test/java/com/manhani/stefane/reimubot/ReimuBotTest.java
@@ -1,7 +1,42 @@
 package com.manhani.stefane.reimubot;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.assertj.core.api.ZonedDateTimeAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReimuBotTest {
+
+    private ReimuBot reimuBot;
+    
+    @BeforeEach
+    void setUp(){ reimuBot = new ReimuBot(); }
+ 
+    @Test
+    @DisplayName("Should select weakest card if cannot defeat opponent")
+    void selectWeakestIfLose(){
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.CLUBS);
+        List<TrucoCard> reimuCards = List.of(
+                TrucoCard.of(CardRank.QUEEN, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS)
+        );
+        var step = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(), List.of(), vira, 1).botInfo(reimuCards, 0)
+                .opponentScore(0).opponentCard(TrucoCard.of(CardRank.TWO, CardSuit.CLUBS))
+                .build();
+        var selectedCard = reimuBot.chooseCard(step).content();
+        assertThat(selectedCard).isEqualTo(TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS));
+    }
+    
 
 }
