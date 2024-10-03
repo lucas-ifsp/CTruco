0a91886962b16137ae529b03aaad7407a07a650e
gabrmanh <gmanhani@yahoo.com>
2024-10-02 22:15:49 -0300

    feat: getRaiseResponse constants for readability

--- bot-impl/src/main/java/com/manhani/stefane/reimubot/ReimuBot.java
+++ bot-impl/src/main/java/com/manhani/stefane/reimubot/ReimuBot.java
@@ -5,6 +5,10 @@ import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.service.BotServiceProvider;
 
 public class ReimuBot implements BotServiceProvider {
+    public static final int REFUSE = -1;
+    public static final int ACCEPT = 0;
+    public static final int RERAISE = 1;
+    
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         return false;
