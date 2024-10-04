8954194eca0af1f9dcd3e56aed95a57749147bd8
PolyStefane <polyvstefane@gmail.com>
2024-10-04 03:15:53 -0300

    test: accept mao de onze if has one manilha and one three in hand

--- bot-impl/src/test/java/com/manhani/stefane/reimubot/ReimuBotTest.java
+++ bot-impl/src/test/java/com/manhani/stefane/reimubot/ReimuBotTest.java
@@ -251,6 +251,25 @@ class ReimuBotTest {
             assertThat(reimuBot.getMaoDeOnzeResponse(step)).isTrue();
         }
 
+        @Test
+        @DisplayName("Should accept mão de onze if has one manilha and one three in hand")
+        void acceptsMaoDeOnzeIfHasOneManilhaAndOneThree() {
+            TrucoCard vira = TrucoCard.of(CardRank.QUEEN, CardSuit.HEARTS);
+            List<TrucoCard> reimuCards = List.of(
+                    TrucoCard.of(CardRank.JACK, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.SEVEN, CardSuit.HEARTS)
+            );
+            var step = GameIntel.StepBuilder.with()
+                    .gameInfo(
+                            List.of(),
+                            List.of(),
+                            vira, 1)
+                    .botInfo(reimuCards, 11)
+                    .opponentScore(4)
+                    .build();
+            assertThat(reimuBot.getMaoDeOnzeResponse(step)).isTrue();
+        }
 
 
 
