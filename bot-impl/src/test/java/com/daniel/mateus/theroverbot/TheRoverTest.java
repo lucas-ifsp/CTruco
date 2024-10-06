0016d340d7698598c4961d7cd1114b106fe9104f
Mateus Ferrari <mateusfaa@hotmail.com>
2024-10-06 20:21:18 -0300

    test: create WhenHasTwoEqualsCardsShouldReturnEither() test for getHighestCardInHand().

--- bot-impl/src/test/java/com/daniel/mateus/theroverbot/TheRoverTest.java
+++ bot-impl/src/test/java/com/daniel/mateus/theroverbot/TheRoverTest.java
@@ -867,5 +867,27 @@ public class TheRoverTest {
 
             assertEquals(TrucoCard.of(CardRank.ACE, CardSuit.SPADES),theRover.getHighestCardInHand(stepBuilder.build()));
         }
+
+        @Test
+        @DisplayName("When has two equals cards should return either")
+        void WhenHasTwoEqualsCardsShouldReturnEither(){
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
+
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.THREE, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.JACK, CardSuit.CLUBS)
+            );
+
+            stepBuilder = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(cards, 0)
+                    .opponentScore(0);
+
+            assertThat(theRover.getHighestCardInHand(stepBuilder.build())).isIn(
+                    TrucoCard.of(CardRank.THREE, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.THREE, CardSuit.HEARTS)
+            );
+        }
     }
 }
