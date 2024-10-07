7568503b1f7b8f1619c9acf5ac98494285694c4f
mbrito <britomauricio2003@gmail.com>
2024-10-07 01:28:02 -0300

    feat: declares `BotEco`

---
+++ bot-impl/src/main/java/com/brito/macena/boteco/BotEco.java
@@ -0,0 +1,32 @@
+package com.brito.macena.boteco;
+
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.service.BotServiceProvider;
+
+public class BotEco implements BotServiceProvider {
+    @Override
+    public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public boolean decideIfRaises(GameIntel intel) {
+        return false;
+    }
+
+    @Override
+    public CardToPlay chooseCard(GameIntel intel) {
+        return null;
+    }
+
+    @Override
+    public int getRaiseResponse(GameIntel intel) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "BotEco :)";
+    }
+}
