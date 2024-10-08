fd5c4034b6e66f4c7c43ac5a7ac93c62b79bd69b
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-08 00:06:31 -0300

    test: before all method in gameTests

---
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
@@ -0,0 +1,14 @@
+package com.brito.macena.boteco.utils;
+
+import com.bueno.spi.model.GameIntel;
+import org.junit.jupiter.api.BeforeAll;
+
+public class GameTest {
+
+    private GameIntel intel;
+
+    @BeforeAll
+    static void setupAll() {
+        System.out.println("Starting Game tests...");
+    }
+}
