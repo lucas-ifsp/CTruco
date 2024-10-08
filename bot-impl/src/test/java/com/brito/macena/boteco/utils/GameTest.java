4ba792c392c43d86d54d59c7a853ec181fa160eb
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-08 00:12:52 -0300

    test: instantiating setup before each methods

--- bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
@@ -1,8 +1,14 @@
 package com.brito.macena.boteco.utils;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
 
 public class GameTest {
 
@@ -14,5 +20,18 @@ public class GameTest {
     @AfterAll
     static void tearDownAll() {
         System.out.println("Finishing Game tests...");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES);
+        TrucoCard card1 = TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS);
+        TrucoCard card2 = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+        TrucoCard card3 = TrucoCard.of(CardRank.KING, CardSuit.CLUBS);
+        intel = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(), List.of(card1, card2, card3), vira, 2)
+                .botInfo(List.of(card1, card2, card3), 0)
+                .opponentScore(0)
+                .build();
     }
 }
