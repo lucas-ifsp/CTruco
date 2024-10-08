004cac59fd862df5209e3625abf543718e85fabb
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-08 00:07:14 -0300

    test: after all method in gameTests

--- bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
@@ -1,6 +1,7 @@
 package com.brito.macena.boteco.utils;
 
 import com.bueno.spi.model.GameIntel;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class GameTest {
@@ -8,7 +9,10 @@ public class GameTest {
     private GameIntel intel;
 
     @BeforeAll
-    static void setupAll() {
-        System.out.println("Starting Game tests...");
+    static void setupAll() { System.out.println("Starting Game tests..."); }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("Finishing Game tests...");
     }
 }
