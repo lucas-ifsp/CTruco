24a26eaf806455d9fd7886463e86a47f1aa1b52a
Lucas Selin <lucas.selin@aluno.ifsp.edu.br>
2024-10-09 19:19:49 -0300

    feat (inicialização): Inicio das classes de logica do bot e classe de testes

---
+++ bot-impl/src/test/java/com/Selin/Bonelli/zeTruqueroTests.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (C) 2022 Lucas B. R. de Oliveira - IFSP/SCL
+ *  Contact: lucas <dot> oliveira <at> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+/* 'zeTruquero' bot with didactic propose. Code by Lucas Selin and Pedro Bonelli */
+
+package com.Selin.Bonelli;
+
+import com.bueno.spi.model.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class zeTruqueroTests
+{
+}
