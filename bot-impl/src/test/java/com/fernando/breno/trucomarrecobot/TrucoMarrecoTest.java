a814f66a22b2304e6c9e9f5a3b73e00a740a8132
brenoaissa <brenoaugusto05@hotmail.com.br>
2024-10-10 21:27:19 -0300

    test: create test class for functionality verification

--- bot-impl/src/test/java/com/fernando/breno/trucomarrecobot/TrucoMarrecoTest.java
+++ bot-impl/src/test/java/com/fernando/breno/trucomarrecobot/TrucoMarrecoTest.java
@@ -1,7 +1,24 @@
 package com.fernando.breno.trucomarrecobot;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrucoMarrecoTest {
 
+    @Test
+    void getMaoDeOnzeResponse() {
+    }
+
+    @Test
+    void decideIfRaises() {
+    }
+
+    @Test
+    void chooseCard() {
+    }
+
+    @Test
+    void getRaiseResponse() {
+    }
 }
