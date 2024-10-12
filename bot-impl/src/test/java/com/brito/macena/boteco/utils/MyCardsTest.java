4ac62feede204f9fd83fae4a918b2fcd5acf9cdb
mbrito <britomauricio2003@gmail.com>
2024-10-12 18:57:28 -0300

    test: add test for getting the best card with three cards in hand

---
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/MyCardsTest.java
@@ -0,0 +1,27 @@
+package com.brito.macena.boteco.utils;
+
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MyCardsTest {
+    @Test
+    @DisplayName("Should return the best card when the hand has 3 cards")
+    void testGetBestCardWithThreeCards() {
+        TrucoCard card1 = TrucoCard.of(CardRank.KING, CardSuit.HEARTS);
+        TrucoCard card2 = TrucoCard.of(CardRank.QUEEN, CardSuit.HEARTS);
+        TrucoCard card3 = TrucoCard.of(CardRank.JACK, CardSuit.HEARTS);
+        TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.CLUBS);
+
+        MyCards myCards = new MyCards(List.of(card1, card2, card3), vira);
+
+        TrucoCard bestCard = myCards.getBestCard();
+        assertThat(bestCard).isEqualTo(card1);
+    }
+}
