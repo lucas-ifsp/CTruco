6fd587f2b410255b99b96e6d0b8d9a0ecafe0d8b
mbrito <britomauricio2003@gmail.com>
2024-10-12 19:48:07 -0300

    feat: add method to get the best card from hand based on value

--- bot-impl/src/main/java/com/brito/macena/boteco/utils/MyCards.java
+++ bot-impl/src/main/java/com/brito/macena/boteco/utils/MyCards.java
@@ -7,17 +7,31 @@ import java.util.List;
 public class MyCards {
 
     List<TrucoCard> myHand;
+    List<Integer> myHandValuesSorted;
     TrucoCard vira;
 
 
     public MyCards(List<TrucoCard> myHand, TrucoCard vira) {
         this.myHand = myHand;
         this.vira = vira;
+        myHandValuesSorted = this.myHand.stream()
+                .map(trucoCard -> trucoCard.relativeValue(vira))
+                .sorted()
+                .toList();
     }
 
 
     public TrucoCard getBestCard() {
-        return null;
+
+        Integer valueOfBestCard;
+        if (myHand.size() == 3) valueOfBestCard = myHandValuesSorted.get(2);
+        else if (myHand.size() == 2) valueOfBestCard = myHandValuesSorted.get(1);
+        else valueOfBestCard = myHandValuesSorted.get(0);
+
+        return myHand.stream()
+                .filter(trucoCard -> trucoCard.relativeValue(vira) == valueOfBestCard)
+                .findAny()
+                .orElse(myHand.get(0));
     }
 
     public TrucoCard getSecondBestCard() {
