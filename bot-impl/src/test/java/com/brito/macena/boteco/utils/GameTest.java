c1c51503f293b64acb0396d5065d861884688fd2
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-12 22:59:35 -0300

    test: test lostFirstRound when first round is lost

--- bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
@@ -70,5 +70,20 @@ public class GameTest {
     @Nested
     @DisplayName("Tests for lostFirstRound method")
     class LostFirstRoundTests {
+        @Test
+        @DisplayName("Test lostFirstRound when first round is lost")
+        public void testLostFirstRoundWhenFirstRoundIsLost() {
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES);
+            TrucoCard card1 = TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS);
+            TrucoCard card2 = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+            TrucoCard card3 = TrucoCard.of(CardRank.KING, CardSuit.CLUBS);
+            intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(GameIntel.RoundResult.LOST), List.of(card1, card2, card3), vira, 2)
+                    .botInfo(List.of(card1, card2, card3), 0)
+                    .opponentScore(0)
+                    .build();
+            boolean result = Game.lostFirstRound(intel);
+            assertThat(result).isTrue();
+        }
     }
 }
