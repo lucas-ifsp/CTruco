0e5f8715f02895acc3a8f2817311ed16e7c36b13
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-12 23:24:50 -0300

    refactor: lostFirstRound method now uses round results

--- bot-impl/src/main/java/com/brito/macena/boteco/utils/Game.java
+++ bot-impl/src/main/java/com/brito/macena/boteco/utils/Game.java
@@ -9,6 +9,7 @@ public class Game {
     }
 
     public static boolean lostFirstRound(GameIntel intel) {
+        if (!intel.getRoundResults().isEmpty()) return intel.getRoundResults().get(0) == GameIntel.RoundResult.LOST;
         return false;
     }
 }
