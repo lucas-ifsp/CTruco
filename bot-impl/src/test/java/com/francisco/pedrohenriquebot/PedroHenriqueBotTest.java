1ccd7e8ec40f1b92ed022318b5b13848cb8b4c76
ciscodeto <netofcisco@gmail.com>
2024-10-13 15:38:09 -0300

    test: Spassed test hould accept mmaoDeOnze with two manilhas

--- bot-impl/src/test/java/com/francisco/pedrohenriquebot/PedroHenriqueBotTest.java
+++ bot-impl/src/test/java/com/francisco/pedrohenriquebot/PedroHenriqueBotTest.java
@@ -1,16 +1,22 @@
 package com.francisco.pedrohenriquebot;
 
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static com.bueno.spi.model.CardRank.*;
+import static com.bueno.spi.model.CardSuit.*;
 import static com.bueno.spi.model.GameIntel.RoundResult.LOST;
 import static com.bueno.spi.model.GameIntel.RoundResult.WON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PedroHenriqueBotTest {
 
@@ -35,6 +41,19 @@ class PedroHenriqueBotTest {
         @Test
         @DisplayName("Should accept mao de onze with 2 manilhas")
         void shouldAcceptMaoDeOnzeWith2Manilhas() {
+            TrucoCard vira = TrucoCard.of(ACE, SPADES);
+            List<TrucoCard> botCards = Arrays.asList(
+                    TrucoCard.of(TWO, HEARTS),
+                    TrucoCard.of(THREE, CLUBS),
+                    TrucoCard.of(ACE, DIAMONDS)
+            );
+
+            intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), Collections.singletonList(vira), vira,0)
+                    .botInfo(botCards,11)
+                    .opponentScore(8);
+
+            assertTrue(sut.getMaoDeOnzeResponse(intel.build()));
         }
 
     }
