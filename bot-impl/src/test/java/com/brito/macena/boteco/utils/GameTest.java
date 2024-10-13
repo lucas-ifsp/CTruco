604126a30e16ea010d0d59b0aac34256f9f6e2dc
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-12 23:22:38 -0300

    test: test lostFirstRound when no rounds are played

--- bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/GameTest.java
@@ -101,5 +101,21 @@ public class GameTest {
             boolean result = Game.lostFirstRound(intel);
             assertThat(result).isFalse();
         }
+
+        @Test
+        @DisplayName("Test lostFirstRound when no rounds are played")
+        public void testLostFirstRoundWhenNoRoundsArePlayed() {
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES);
+            TrucoCard card1 = TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS);
+            TrucoCard card2 = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+            TrucoCard card3 = TrucoCard.of(CardRank.KING, CardSuit.CLUBS);
+            intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(card1, card2, card3), vira, 2)
+                    .botInfo(List.of(card1, card2, card3), 0)
+                    .opponentScore(0)
+                    .build();
+            boolean result = Game.lostFirstRound(intel);
+            assertThat(result).isFalse();
+        }
     }
 }
