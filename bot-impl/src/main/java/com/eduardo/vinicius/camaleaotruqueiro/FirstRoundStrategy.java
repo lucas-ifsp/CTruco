768989cab5764279f411411801667a64aa43dd5b
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-14 01:01:42 -0300

    feat(CamaleaoTruqueiro): implement concrete methods 'decideIfRaises' and 'chooseCard' of FirstRoundStrategy class

--- bot-impl/src/main/java/com/eduardo/vinicius/camaleaotruqueiro/FirstRoundStrategy.java
+++ bot-impl/src/main/java/com/eduardo/vinicius/camaleaotruqueiro/FirstRoundStrategy.java
@@ -2,6 +2,10 @@ package com.eduardo.vinicius.camaleaotruqueiro;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+
+import java.util.List;
+
 import static com.eduardo.vinicius.camaleaotruqueiro.TrucoUtils.*;
 
 public class FirstRoundStrategy implements RoundStrategy{
@@ -17,12 +21,28 @@ public class FirstRoundStrategy implements RoundStrategy{
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        return false;
+        HandsCardSituation situation = HandsCardSituation.evaluateHandSituation(intel);
+        return situation == HandsCardSituation.ALMOST_ABSOLUTE_VICTORY ||
+                isHighChangesOpponentRunFromTruco(intel);
     }
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        TrucoCard selectedCard;
+
+        HandsCardSituation situation = HandsCardSituation.evaluateHandSituation(intel);
+        if (    situation == HandsCardSituation.ALMOST_CERTAIN_VICTORY ||
+                situation == HandsCardSituation.BLUFF_TO_GET_POINTS) {
+            selectedCard = getLowestCard(cards, vira);
+        }
+        else {
+            selectedCard = getGreatestCard(cards, vira);
+
+        }
+
+        return CardToPlay.of(selectedCard);
     }
 
     @Override
