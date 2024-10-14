c7520c8cc9fcab387081413eafc89e6e1039769e
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 19:27:32 -0300

    test: implementing beforeAll method in StatusTest

--- bot-impl/src/test/java/com/brito/macena/boteco/utils/StatusTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/utils/StatusTest.java
@@ -1,4 +1,9 @@
 package com.brito.macena.boteco.utils;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
 public class StatusTest {
+    @BeforeAll
+    static void setupAll() { System.out.println("Starting Status tests..."); }
 }
