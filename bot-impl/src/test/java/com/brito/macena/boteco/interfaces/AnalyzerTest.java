bbce010dd50b6c82d4d8c8820b4a286e3c17fd8b
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 20:36:02 -0300

    test: implementing nested group tests for myHand method

--- bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
@@ -1,8 +1,16 @@
 package com.brito.macena.boteco.interfaces;
 
 import com.brito.macena.boteco.utils.Status;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 
 public class AnalyzerTest {
+
+    @Nested
+    @DisplayName("myHand method tests")
+    class MyHandMethodTests {
+    }
+
     private static class AnalyzerImpl extends Analyzer {
         @Override
         public Status threeCardsHandler() {
