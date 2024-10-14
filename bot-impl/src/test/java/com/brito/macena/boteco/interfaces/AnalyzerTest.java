995ad8eb7a8e42ad9ea8b25168f58ebfd62018e2
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 20:31:47 -0300

    test: implement AnalyzerImpl with status handlers for different card counts

--- bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
@@ -1,4 +1,22 @@
 package com.brito.macena.boteco.interfaces;
 
+import com.brito.macena.boteco.utils.Status;
+
 public class AnalyzerTest {
+    private static class AnalyzerImpl extends Analyzer {
+        @Override
+        public Status threeCardsHandler() {
+            return Status.GOOD;
+        }
+
+        @Override
+        public Status twoCardsHandler() {
+            return Status.MEDIUM;
+        }
+
+        @Override
+        public Status oneCardHandler() {
+            return Status.BAD;
+        }
+    }
 }
