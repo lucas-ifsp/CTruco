b77eb475b3e9c17bdb177fb67137b45d110516b8
mbrito <britomauricio2003@gmail.com>
2024-10-15 19:32:40 -0300

    feat: initialize `AggressiveTrucoCaller`

---
+++ bot-impl/src/main/java/com/brito/macena/boteco/intel/trucoCaller/AggressiveTrucoCaller.java
@@ -0,0 +1,13 @@
+package com.brito.macena.boteco.intel.trucoCaller;
+
+import com.brito.macena.boteco.interfaces.TrucoCaller;
+import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.GameIntel;
+
+public class AggressiveTrucoCaller implements TrucoCaller {
+
+    @Override
+    public boolean shouldCallTruco(GameIntel intel, Status status) {
+        return false;
+    }
+}
