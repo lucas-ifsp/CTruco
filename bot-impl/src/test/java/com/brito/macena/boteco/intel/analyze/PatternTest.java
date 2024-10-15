7cb2f67ab83d63a353735e86411437e798ca4856
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 22:47:32 -0300

    test: implementing the afterAll method

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
@@ -1,10 +1,12 @@
 package com.brito.macena.boteco.intel.analyze;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class PatternTest {
     @BeforeAll
-    static void setupAll() {
-        System.out.println("Starting Pattern tests...");
-    }
+    static void setupAll() { System.out.println("Starting Pattern tests..."); }
+
+    @AfterAll
+    static void tearDownAll() { System.out.println("Finishing Pattern tests..."); }
 }
