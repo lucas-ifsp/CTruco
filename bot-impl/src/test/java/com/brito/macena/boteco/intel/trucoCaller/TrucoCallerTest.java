502972000d90f4d11d82279395135fdf3e913fd6
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 18:47:41 -0300

    test: beforeAll method implementation

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
@@ -1,4 +1,13 @@
 package com.brito.macena.boteco.intel.trucoCaller;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("TrucoCaller tests")
 public class TrucoCallerTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        System.out.println("Starting TrucoCaller tests...");
+    }
 }
