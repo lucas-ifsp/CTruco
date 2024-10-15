62423fe5f40ae9aa6b021a3223088ce99e270070
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 19:43:34 -0300

    test: implementing test for returns EXCELLENT when having at least two manilhas with three cards

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/TrucadorTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/TrucadorTest.java
@@ -1,8 +1,15 @@
 package com.brito.macena.boteco.intel.analyze;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Trucador tests")
 public class TrucadorTest {
@@ -11,4 +18,25 @@ public class TrucadorTest {
 
     @AfterAll
     static void tearDownAll() { System.out.println("Finishing Trucador tests..."); }
+
+    @Nested
+    @DisplayName("Three Cards Handler Tests")
+    class ThreeCardsHandlerTests {
+
+        @Test
+        @DisplayName("Returns EXCELLENT when having at least two manilhas with three cards")
+        void returnsExcellentWhenHavingAtLeastTwoManilhasWithThreeCards() {
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), TrucoCard.of(CardRank.ACE, CardSuit.CLUBS), 0)
+                    .botInfo(List.of(
+                            TrucoCard.of(CardRank.ACE, CardSuit.CLUBS),
+                            TrucoCard.of(CardRank.ACE, CardSuit.SPADES),
+                            TrucoCard.of(CardRank.THREE, CardSuit.DIAMONDS)
+                    ), 0)
+                    .opponentScore(0)
+                    .build();
+            Trucador trucador = new Trucador(intel);
+            assertEquals(Status.EXCELLENT, trucador.threeCardsHandler(intel.getCards()));
+        }
+    }
 }
