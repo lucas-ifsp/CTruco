ca99646d4b4553ce46754e4c9296e4a940b45945
mbrito <britomauricio2003@gmail.com>
2024-10-15 02:16:49 -0300

    test: create Aggressive bot when score difference is not less than -6

--- bot-impl/src/test/java/com/brito/macena/boteco/factories/InstanceFactoryTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/factories/InstanceFactoryTest.java
@@ -9,6 +9,7 @@ import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,24 +18,49 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class InstanceFactoryTest {
 
-    @Test
-    @DisplayName("should create Passive bot when opponent score is high and score difference is less than -6")
-    void shouldCreatePassiveBotWhenOpponentScoreIsHigh() {
-        List<TrucoCard> botEcoHand = List.of(
-                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
-                TrucoCard.of(CardRank.JACK, CardSuit.SPADES),
-                TrucoCard.of(CardRank.KING, CardSuit.CLUBS)
-        );
-        TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
+    @Nested
+    @DisplayName("ProfileBot Creation Tests")
+    class CreateProfileBotTests {
+        @Test
+        @DisplayName("should create Passive bot when opponent score is high and score difference is less than -6")
+        void shouldCreatePassiveBotWhenOpponentScoreIsHigh() {
+            List<TrucoCard> botEcoHand = List.of(
+                    TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.JACK, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.KING, CardSuit.CLUBS)
+            );
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
 
-        GameIntel step = GameIntel.StepBuilder.with()
-                .gameInfo(List.of(), List.of(), vira, 1)
-                .botInfo(botEcoHand, 0)
-                .opponentScore(9)
-                .build();
+            GameIntel step = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(botEcoHand, 0)
+                    .opponentScore(9)
+                    .build();
 
-        ProfileBot bot = InstanceFactory.createProfileBot(step, Status.MEDIUM);
+            ProfileBot bot = InstanceFactory.createProfileBot(step, Status.MEDIUM);
 
-        assertThat(bot).isInstanceOf(Passive.class);
+            assertThat(bot).isInstanceOf(Passive.class);
+        }
+
+        @Test
+        @DisplayName("should create Aggressive bot when score difference is not less than -6")
+        void shouldCreateAggressiveBotWhenScoreDifferenceIsNotLessThanNegativeSix() {
+            List<TrucoCard> botEcoHand = List.of(
+                    TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.JACK, CardSuit.SPADES),
+                    TrucoCard.of(CardRank.KING, CardSuit.CLUBS)
+            );
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
+
+            GameIntel step = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(botEcoHand, 0)
+                    .opponentScore(5)
+                    .build();
+
+            ProfileBot bot = InstanceFactory.createProfileBot(step, Status.MEDIUM);
+
+            assertThat(bot).isInstanceOf(Agressive.class);
+        }
     }
 }
