29e76f1ed25959b5512920de9bc23c45609810e6
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 22:11:54 -0300

    feat: implementing myHand

--- bot-impl/src/main/java/com/brito/macena/boteco/interfaces/Analyzer.java
+++ bot-impl/src/main/java/com/brito/macena/boteco/interfaces/Analyzer.java
@@ -1,8 +1,21 @@
 package com.brito.macena.boteco.interfaces;
 
 import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+
+import java.util.List;
 
 public abstract class Analyzer {
+    public Status myHand(GameIntel intel) {
+        List<TrucoCard> myCards = intel.getCards();
+        int size = myCards.size();
+        if (size == 3) return threeCardsHandler(myCards);
+        if (size == 2) return twoCardsHandler(myCards);
+        if (size == 1) return oneCardHandler();
+        return Status.EXCELLENT;
+    }
+
     public abstract Status threeCardsHandler();
     public abstract Status twoCardsHandler();
     public abstract Status oneCardHandler();
