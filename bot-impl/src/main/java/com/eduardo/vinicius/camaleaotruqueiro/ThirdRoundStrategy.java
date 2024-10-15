22f4b50aae759faf7cbd362d1632549c83e12847
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-15 16:44:25 -0300

    refactor(ThirdRoundStrategy): removed unecessary ifs in logic

--- bot-impl/src/main/java/com/eduardo/vinicius/camaleaotruqueiro/ThirdRoundStrategy.java
+++ bot-impl/src/main/java/com/eduardo/vinicius/camaleaotruqueiro/ThirdRoundStrategy.java
@@ -26,8 +26,10 @@ public class ThirdRoundStrategy implements RoundStrategy{
         else if((winFistRound(intel) || drewFistRound(intel)) &&
                 situation == HandsCardSituation.ALMOST_CERTAIN_VICTORY) return true;
         else return false;
-        //todo pedir truco quando oponent jogar carta vencível, não pedir quando carta é invencível (mesma ganhando primeiro round)
+        //todo
         // pedir truco quando tem carta forte, mesmo não ganhando ou empatando a primeira
+        // pedir truco quando oponent jogar carta vencível,
+        // não pedir quando carta é invencível (mesma ganhando primeiro round)
     }
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
@@ -42,8 +44,6 @@ public class ThirdRoundStrategy implements RoundStrategy{
         HandsCardSituation situation = HandsCardSituation.evaluateHandSituation(intel);
         if(situation == HandsCardSituation.ALMOST_ABSOLUTE_VICTORY) return 1;
         else if(situation == HandsCardSituation.ALMOST_CERTAIN_VICTORY) return 0;
-        else if(isWinning(intel.getScore(),intel.getOpponentScore()) && situation == HandsCardSituation.BLUFF_TO_GET_POINTS) return 0;
-        else if(isWinning(intel.getScore(),intel.getOpponentScore()) && situation == HandsCardSituation.BLUFF_TO_INTIMIDATE) return 0;
         else return -1;
         //todo nunca ocorrerá situacao de Bluff_to_get_points ou Bluff_to_intimidate
     }
