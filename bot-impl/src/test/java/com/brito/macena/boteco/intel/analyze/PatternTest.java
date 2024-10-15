02d9e975169dfdca51afbc36c8fef6ea081fe3cd
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 12:35:26 -0300

    test: implementing test for returns GOOD when having one manilha and second best card value is at least 5

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
@@ -40,4 +40,23 @@ public class PatternTest {
             assertThat(result).isEqualTo(Status.EXCELLENT);
         }
     }
+
+    @Test
+    @DisplayName("Returns GOOD when having one manilha and second best card value is at least 5")
+    void returnsGoodWhenHavingOneManilhaAndSecondBestCardValueIsAtLeast5() {
+        GameIntel intel = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(), List.of(), TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS), 0)
+                .botInfo(List.of(
+                        TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
+                        TrucoCard.of(CardRank.THREE, CardSuit.HEARTS),
+                        TrucoCard.of(CardRank.TWO, CardSuit.SPADES)
+                ), 0)
+                .opponentScore(0)
+                .build();
+
+        Pattern pattern = new Pattern(intel);
+        Status result = pattern.threeCardsHandler(intel.getCards());
+
+        assertThat(result).isEqualTo(Status.GOOD);
+    }
 }
