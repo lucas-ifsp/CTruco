d89aa60b8c60929581cf8060472f22630b31cb5c
mbrito <britomauricio2003@gmail.com>
2024-10-15 02:00:32 -0300

    feat: initialize `InstanceFactory`

---
+++ bot-impl/src/main/java/com/brito/macena/boteco/factories/InstanceFactory.java
@@ -0,0 +1,11 @@
+package com.brito.macena.boteco.factories;
+
+import com.brito.macena.boteco.interfaces.ProfileBot;
+import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.GameIntel;
+
+public class InstanceFactory {
+    public static ProfileBot createProfileBot(GameIntel intel, Status status) {
+        return null;
+    }
+}
