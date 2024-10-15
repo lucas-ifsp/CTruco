5f7a5e8da2d92df9c2b37e337246374638be9ab1
mbrito <britomauricio2003@gmail.com>
2024-10-15 02:24:13 -0300

    feat: implements `createProfileBot` in `InstaceFactory`

--- bot-impl/src/main/java/com/brito/macena/boteco/factories/InstanceFactory.java
+++ bot-impl/src/main/java/com/brito/macena/boteco/factories/InstanceFactory.java
@@ -1,11 +1,21 @@
 package com.brito.macena.boteco.factories;
 
+import com.brito.macena.boteco.intel.profiles.Agressive;
+import com.brito.macena.boteco.intel.profiles.Passive;
 import com.brito.macena.boteco.interfaces.ProfileBot;
 import com.brito.macena.boteco.utils.Status;
 import com.bueno.spi.model.GameIntel;
 
 public class InstanceFactory {
     public static ProfileBot createProfileBot(GameIntel intel, Status status) {
-        return null;
+        int myScore = intel.getScore();
+        int oppScore = intel.getOpponentScore();
+        int scoreDifference = myScore - oppScore;
+
+        if (oppScore > myScore && scoreDifference < -6) {
+            return new Passive(intel, status);
+        } else {
+            return new Agressive(intel, status);
+        }
     }
 }
