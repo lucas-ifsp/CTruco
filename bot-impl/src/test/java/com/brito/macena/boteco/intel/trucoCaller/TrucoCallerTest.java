1a8657abacfec83cce812e208a4cf279a22f886d
mbrito <britomauricio2003@gmail.com>
2024-10-15 20:06:21 -0300

    test: ensure truco is called when losing by 9 or more with medium or bad status and 3 cards

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
@@ -1,12 +1,16 @@
 package com.brito.macena.boteco.intel.trucoCaller;
 
-import com.brito.macena.boteco.intel.profiles.Agressive;
+import com.brito.macena.boteco.utils.MyHand;
 import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("TrucoCaller tests")
@@ -49,7 +53,27 @@ public class TrucoCallerTest {
     @Nested
     @DisplayName("PassiveTrucoCaller method tests")
     class PassiveTrucoCallerMethodTests {
+        @Test
+        @DisplayName("Should call truco if losing by 9 or more and status is medium or bad with 3 cards")
+        void shouldCallTrucoWhenLosingByNineOrMoreAndStatusIsMediumOrBad() {
+            List<TrucoCard> botHand = List.of(
+                    TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.SIX, CardSuit.HEARTS)
+            );
+            TrucoCard vira = TrucoCard.of(CardRank.ACE, CardSuit.DIAMONDS);
 
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(botHand, 0)
+                    .opponentScore(9)
+                    .build();
+
+            PassiveTrucoCaller trucoCaller = new PassiveTrucoCaller();
+            boolean shouldCall = trucoCaller.shouldCallTruco(intel, Status.MEDIUM);
+
+            assertTrue(shouldCall);
+        }
     }
 
     @Nested
