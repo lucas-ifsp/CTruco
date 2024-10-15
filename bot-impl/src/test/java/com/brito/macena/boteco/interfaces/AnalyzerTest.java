9df6dc8677599fe83a4d503f48f00715dab978d9
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 22:38:20 -0300

    test: removing unnecessary imports

--- bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class AnalyzerTest {
 
