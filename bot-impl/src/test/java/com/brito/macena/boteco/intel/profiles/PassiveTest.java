a64b65618bee8d0de8cffb94af290eb6fcd9ad46
mbrito <britomauricio2003@gmail.com>
2024-10-14 23:48:24 -0300

    test: should play weakest card possible to tie or kill in first round if have medium hand

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/profiles/PassiveTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/profiles/PassiveTest.java
@@ -4,6 +4,7 @@ import com.brito.macena.boteco.interfaces.ProfileBot;
 import com.brito.macena.boteco.utils.Status;
 import com.bueno.spi.model.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,27 +14,56 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PassiveTest {
 
-    @Test
-    @DisplayName("should kill the opponent's card if you have a medium hand")
-    void shouldPlayStrongestCardIfItWins() {
-        List<TrucoCard> botHand = List.of(
-                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
-                TrucoCard.of(CardRank.JACK, CardSuit.HEARTS),
-                TrucoCard.of(CardRank.THREE, CardSuit.CLUBS)
-        );
-        TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
-        TrucoCard opponentCard = TrucoCard.of(CardRank.TWO, CardSuit.HEARTS);
+    @Nested
+    @DisplayName("First Round")
+    class FirstRoundTests {
 
-        GameIntel intel = GameIntel.StepBuilder.with()
-                .gameInfo(List.of(), List.of(), vira, 1)
-                .botInfo(botHand, 0)
-                .opponentScore(0)
-                .opponentCard(opponentCard)
-                .build();
+        @Test
+        @DisplayName("should kill the opponent's card if you have a medium hand")
+        void shouldPlayStrongestCardIfItWins() {
+            List<TrucoCard> botHand = List.of(
+                    TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.JACK, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.THREE, CardSuit.CLUBS)
+            );
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
+            TrucoCard opponentCard = TrucoCard.of(CardRank.TWO, CardSuit.HEARTS);
 
-        ProfileBot passive = new Passive(intel, Status.MEDIUM);
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(botHand, 0)
+                    .opponentScore(0)
+                    .opponentCard(opponentCard)
+                    .build();
 
-        CardToPlay selectedCard = passive.firstRoundChoose();
-        assertThat(selectedCard).isEqualTo(CardToPlay.of(TrucoCard.of(CardRank.THREE, CardSuit.CLUBS)));
+            ProfileBot passive = new Passive(intel, Status.MEDIUM);
+
+            CardToPlay selectedCard = passive.firstRoundChoose();
+            assertThat(selectedCard).isEqualTo(CardToPlay.of(TrucoCard.of(CardRank.THREE, CardSuit.CLUBS)));
+        }
+
+        @Test
+        @DisplayName("should must play the weakest card possible to tie or kill in the first round if you have a medium hand")
+        void shouldPlayWeakestCardIfItLoses() {
+            List<TrucoCard> botHand = List.of(
+                    TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                    TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS),
+                    TrucoCard.of(CardRank.SEVEN, CardSuit.HEARTS)
+            );
+            TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.DIAMONDS);
+            TrucoCard opponentCard = TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS);
+
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), vira, 1)
+                    .botInfo(botHand, 0)
+                    .opponentScore(0)
+                    .opponentCard(opponentCard)
+                    .build();
+
+            ProfileBot passive = new Passive(intel, Status.MEDIUM);
+
+            CardToPlay selectedCard = passive.firstRoundChoose();
+            assertThat(selectedCard).isEqualTo(CardToPlay.of(TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS)));
+        }
     }
 }
