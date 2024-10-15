14243e012321640aabf41e817f6f2c2c8dc22c1c
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 14:06:24 -0300

    feat: instantiating constructor for Pattern class

--- bot-impl/src/main/java/com/brito/macena/boteco/intel/analyze/Pattern.java
+++ bot-impl/src/main/java/com/brito/macena/boteco/intel/analyze/Pattern.java
@@ -1,4 +1,44 @@
 package com.brito.macena.boteco.intel.analyze;
 
-public class Pattern {
+import com.brito.macena.boteco.interfaces.Analyzer;
+import com.brito.macena.boteco.utils.MyHand;
+import com.brito.macena.boteco.utils.Status;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+
+import java.util.List;
+
+public class Pattern extends Analyzer {
+
+    private final GameIntel intel;
+    private final TrucoCard vira;
+    private final int bestCardValue;
+    private final int secondBestCardValue;
+
+
+    public Pattern(GameIntel intel) {
+        this.intel = intel;
+        vira = intel.getVira();
+
+        MyHand myCards = new MyHand(intel.getCards(),vira);
+
+        bestCardValue = myCards.getBestCard().relativeValue(vira);
+        secondBestCardValue = myCards.getSecondBestCard().relativeValue(vira);
+    }
+
+
+    @Override
+    public Status threeCardsHandler(List<TrucoCard> myCards) {
+        return null;
+    }
+
+    @Override
+    public Status twoCardsHandler(List<TrucoCard> myCards) {
+        return null;
+    }
+
+    @Override
+    public Status oneCardHandler() {
+        return null;
+    }
 }
