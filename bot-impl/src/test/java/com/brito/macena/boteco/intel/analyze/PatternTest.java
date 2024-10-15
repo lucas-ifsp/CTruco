d4a1887ee41f7515a79bea5e429ac6a811a82d57
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 22:46:58 -0300

    test: implementing the beforeAll method

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
@@ -1,4 +1,10 @@
 package com.brito.macena.boteco.intel.analyze;
 
+import org.junit.jupiter.api.BeforeAll;
+
 public class PatternTest {
+    @BeforeAll
+    static void setupAll() {
+        System.out.println("Starting Pattern tests...");
+    }
 }
