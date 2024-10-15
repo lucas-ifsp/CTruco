7ad91a98dd5ac8bd7812339d6ca39a1d2c56ae57
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-14 22:36:50 -0300

    test: adjust Returns twoCardsHandler when two cards test to use StepBuilder for creating GameIntel instances

--- bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/interfaces/AnalyzerTest.java
@@ -58,9 +58,15 @@ public class AnalyzerTest {
         @Test
         @DisplayName("Returns twoCardsHandler when two cards")
         void returnsTwoCardsHandlerWhenTwoCards() {
-            GameIntel intel = mock(GameIntel.class);
-            List<TrucoCard> cards = List.of(mock(TrucoCard.class), mock(TrucoCard.class));
-            when(intel.getCards()).thenReturn(cards);
+            List<TrucoCard> cards = List.of(
+                    TrucoCard.of(CardRank.ACE, CardSuit.HEARTS),
+                    TrucoCard.of(CardRank.KING, CardSuit.SPADES)
+            );
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), cards, TrucoCard.of(CardRank.ACE, CardSuit.HEARTS), 0)
+                    .botInfo(cards, 0)
+                    .opponentScore(0)
+                    .build();
 
             Analyzer analyzer = new AnalyzerImpl();
             Status result = analyzer.myHand(intel);
