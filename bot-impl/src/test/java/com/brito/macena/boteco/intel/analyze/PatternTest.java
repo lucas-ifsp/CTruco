d2ebb34f08417bdb05c4cf309a2863a1909ce527
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 11:55:45 -0300

    test: implementing test for returns EXCELLENT when having at least two manilhas

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/analyze/PatternTest.java
@@ -1,9 +1,14 @@
 package com.brito.macena.boteco.intel.analyze;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PatternTest {
     @BeforeAll
@@ -15,5 +20,23 @@ public class PatternTest {
     @Nested
     @DisplayName("threeCardsHandler method tests")
     class ThreeCardsHandlerMethodTests {
+        @Test
+        @DisplayName("Returns EXCELLENT when having at least two manilhas")
+        void returnsExcellentWhenHavingAtLeastTwoManilhas() {
+            GameIntel intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), List.of(), TrucoCard.of(CardRank.FOUR, CardSuit.CLUBS), 0)
+                    .botInfo(List.of(
+                            TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
+                            TrucoCard.of(CardRank.FIVE, CardSuit.HEARTS),
+                            TrucoCard.of(CardRank.FIVE, CardSuit.SPADES)
+                    ), 0)
+                    .opponentScore(0)
+                    .build();
+
+            Pattern pattern = new Pattern(intel);
+            Status result = pattern.threeCardsHandler(intel.getCards());
+
+            assertThat(result).isEqualTo(Status.EXCELLENT);
+        }
     }
 }
