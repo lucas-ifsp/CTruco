b28e424a108384255a797bbcff916c7db912d210
brunor36 <bruno.ribaldo@aluno.ifsp.edu.br>
2024-10-15 16:22:33 -0300

    test: re-Raise With Two Manilhas

--- bot-impl/src/test/java/com/francisco/bruno/pedrohenriquebot/PedroHenriqueBotTest.java
+++ bot-impl/src/test/java/com/francisco/bruno/pedrohenriquebot/PedroHenriqueBotTest.java
@@ -486,5 +486,23 @@ class PedroHenriqueBotTest {
             int response = sut.getRaiseResponse(intel.build());
             assertEquals(0, response); // Accept
         }
+
+        @Test
+        @DisplayName("Re-raise when have two manilhas")
+        void reRaiseWithTwoManilhas() {
+            TrucoCard vira = TrucoCard.of(ACE, HEARTS);
+            List<TrucoCard> botCards = Arrays.asList(
+                    TrucoCard.of(TWO, HEARTS),
+                    TrucoCard.of(TWO, CLUBS),
+                    TrucoCard.of(SEVEN, CLUBS)
+            );
+            intel = GameIntel.StepBuilder.with()
+                    .gameInfo(List.of(), Collections.singletonList(vira), vira, 1)
+                    .botInfo(botCards, 0)
+                    .opponentScore(5);
+
+            int response = sut.getRaiseResponse(intel.build());
+            assertEquals(1, response);
+        }
     }
 }
