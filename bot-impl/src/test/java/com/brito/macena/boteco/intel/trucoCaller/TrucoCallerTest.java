4292fe6be9132b2b88cbb1c3e7e9cfb2b3e3e295
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-15 18:50:28 -0300

    test: implementing nested test group for aggressive Callers

--- bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
+++ bot-impl/src/test/java/com/brito/macena/boteco/intel/trucoCaller/TrucoCallerTest.java
@@ -3,17 +3,20 @@ package com.brito.macena.boteco.intel.trucoCaller;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 
 @DisplayName("TrucoCaller tests")
 public class TrucoCallerTest {
 
     @BeforeAll
-    static void setUpAll() {
-        System.out.println("Starting TrucoCaller tests...");
-    }
+    static void setUpAll() { System.out.println("Starting TrucoCaller tests..."); }
 
     @AfterAll
-    static void tearDownAll() {
-        System.out.println("Finishing TrucoCaller tests...");
+    static void tearDownAll() { System.out.println("Finishing TrucoCaller tests..."); }
+
+    @Nested
+    @DisplayName("AggressiveTrucoCaller method tests")
+    class AgessiveTrucoCallerMethodTests {
+
     }
 }
