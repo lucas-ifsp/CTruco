fb1ba526d107585aa8ead957677f8efc789c5097
brenoaissa <brenoaugusto05@hotmail.com.br>
2024-10-15 23:18:21 -0300

    Refactor wonFirstRound method to check for empty or insufficient round results
    - Updated the method to ensure it throws an IllegalArgumentException if the round results list is null or has less than two elements.
    - Improved the condition to provide clearer validation on the input data.

--- bot-impl/src/main/java/com/fernando/breno/trucomarrecobot/TrucoMarreco.java
+++ bot-impl/src/main/java/com/fernando/breno/trucomarrecobot/TrucoMarreco.java
@@ -144,11 +144,12 @@ public class TrucoMarreco implements BotServiceProvider {
 
 
     public boolean wonFirstRound(GameIntel intel){
-        if (intel.getRoundResults() == null || intel.getRoundResults().size() <= 1) {
+        if (intel.getRoundResults() == null || intel.getRoundResults().isEmpty()) {
             throw new IllegalArgumentException("A lista de resultados da rodada está vazia ou não tem elementos suficientes.");
         }
-        return intel.getRoundResults().get(1).equals(GameIntel.RoundResult.WON);
+        return intel.getRoundResults().get(0).equals(GameIntel.RoundResult.WON);
     }
+
 
     public boolean handStrong(GameIntel intel){
         var hasManilha = numberOfManilhas(intel)  >= 1;
