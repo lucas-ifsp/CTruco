6e02434309ace879e1878f86a4ee3302acaffc12
PedroHBelini <pedroh.belini@outlook.com>
2024-10-15 23:01:01 -0300

    docs: add copyright to MataPatoBot

--- bot-impl/src/main/java/com/belini/luciano/matapatobot/MataPatoBot.java
+++ bot-impl/src/main/java/com/belini/luciano/matapatobot/MataPatoBot.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright (C) 2024 Pedro H.Belini and Lucas Luciano
+ *  Contact: Pedro <dot> Belini <at> ifsp <dot> edu <dot> br
+ *  Contact: Lucas <dot> Luciano <at> ifsp <dot> edu <dot> br
+ *
+ *  This file is part of CTruco (Truco game for didactic purpose).
+ *
+ *  CTruco is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CTruco is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CTruco.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 package com.belini.luciano.matapatobot;
 
 import com.bueno.spi.model.*;
