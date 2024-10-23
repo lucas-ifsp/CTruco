214101be7240c38195a8363005d16cab03a978b9
gcontiero11 <gustcontiero@gmail.com>
2024-10-22 23:45:58 -0300

    fixing the console version, now it's stable

--- console/src/main/java/com/bueno/application/withbots/features/ConsoleTournament.java
+++ console/src/main/java/com/bueno/application/withbots/features/ConsoleTournament.java
@@ -20,50 +20,50 @@ public class ConsoleTournament {
         this.refreshUseCase = refreshUseCase;
     }
 
-    public void startTournament(List<String> bots, int times) {
-        if (bots.size() != 16 && bots.size() != 8 && bots.size() != 4) {
-            System.out.println("invalid number of participants");
-            return;
-        }
-        List<String> participants = new ArrayList<>(bots);
-        Collections.shuffle(participants);
-        dto = tournamentProvider.createTournament(participants, participants.size(), times);
-        System.out.println("ALL Matches: ");
-        dto.matchesDTO().forEach((matchDTO) -> System.out.println("Match number: " +
-                matchDTO.matchNumber() +
-                " " +
-                matchDTO.p1Name() +
-                " VS " +
-                matchDTO.p2Name()
-        ));
-    }
-
-    public void tournamentMenu() {
-        Objects.requireNonNull(dto, "tournament menu requires tournament data");
-        MatchDTO finalMatch = dto.matchesDTO()
-                .stream()
-                .filter(matchDTO -> matchDTO.next() == null).findFirst().orElseThrow();
-
-        while (finalMatch.winnerName() == null) {
-            List<MatchDTO> availableOnes = playUseCase.getAllAvailableMatches(dto);
-            System.out.println("Available matches: " + availableOnes.size());
-            availableOnes.forEach(matchDTO -> System.out.println("Match number: " +
-                    matchDTO.matchNumber() +
-                    "  " +
-                    matchDTO.p1Name() +
-                    " VS " +
-                    matchDTO.p2Name()));
-            final int matchNumber = chooseMatch();
-            playUseCase.playOne(dto.uuid(), matchNumber, dto.times());
-            refreshUseCase.refresh(dto.uuid());
-
-
-            finalMatch = dto.matchesDTO()
-                    .stream()
-                    .filter(matchDTO -> matchDTO.next() == null).findFirst().orElseThrow();
-        }
-        System.out.println("Ganhador: " + finalMatch.winnerName());
-    }
+//    public void startTournament(List<String> bots, int times) {
+//        if (bots.size() != 16 && bots.size() != 8 && bots.size() != 4) {
+//            System.out.println("invalid number of participants");
+//            return;
+//        }
+//        List<String> participants = new ArrayList<>(bots);
+//        Collections.shuffle(participants);
+//        dto = tournamentProvider.createTournament(participants, participants.size(), times);
+//        System.out.println("ALL Matches: ");
+//        dto.matchesDTO().forEach((matchDTO) -> System.out.println("Match number: " +
+//                matchDTO.matchNumber() +
+//                " " +
+//                matchDTO.p1Name() +
+//                " VS " +
+//                matchDTO.p2Name()
+//        ));
+//    }
+//
+//    public void tournamentMenu() {
+//        Objects.requireNonNull(dto, "tournament menu requires tournament data");
+//        MatchDTO finalMatch = dto.matchesDTO()
+//                .stream()
+//                .filter(matchDTO -> matchDTO.next() == null).findFirst().orElseThrow();
+//
+//        while (finalMatch.winnerName() == null) {
+//            List<MatchDTO> availableOnes = playUseCase.getAllAvailableMatches(dto);
+//            System.out.println("Available matches: " + availableOnes.size());
+//            availableOnes.forEach(matchDTO -> System.out.println("Match number: " +
+//                    matchDTO.matchNumber() +
+//                    "  " +
+//                    matchDTO.p1Name() +
+//                    " VS " +
+//                    matchDTO.p2Name()));
+//            final int matchNumber = chooseMatch();
+//            playUseCase.playOne(dto.uuid(), matchNumber, dto.times());
+//            refreshUseCase.refresh(dto.uuid());
+//
+//
+//            finalMatch = dto.matchesDTO()
+//                    .stream()
+//                    .filter(matchDTO -> matchDTO.next() == null).findFirst().orElseThrow();
+//        }
+//        System.out.println("Ganhador: " + finalMatch.winnerName());
+//    }
 
     private int chooseMatch() {
         Scanner scan = new Scanner(System.in);
