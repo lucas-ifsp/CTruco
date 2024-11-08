e6ca48c3b84aed0f1355d08d3e4283baae274685
Felipe Vilela <felipe.vilela01@outlook.com>
2024-11-08 19:56:52 -0300

    feat: starting which card to play

--- bot-impl/src/main/java/com/caua/felipe/cfbot/CFBot.java
+++ bot-impl/src/main/java/com/caua/felipe/cfbot/CFBot.java
@@ -4,6 +4,7 @@ import com.bueno.spi.model.*;
 import com.bueno.spi.service.BotServiceProvider;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CFBot implements BotServiceProvider {
 
@@ -32,40 +33,51 @@ public class CFBot implements BotServiceProvider {
         return toTwoOrThree(intel, myCards);
     }
 
-    //To do
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-//        int maxValue = intel.getCards().stream().map(TrucoCard::getRank).map(CardRank::value).max(Integer::compareTo).get();
-//        TrucoCard vira =  intel.getVira();
-//
-//        List<TrucoCard> cards = intel.getCards();
-//        int quantasManilha = cards.stream().filter(cart -> cart.isManilha(vira)).toList().size();
-//
-//        return CardToPlay.of(intel.getCards().stream().filter(card -> card.getRank().value() == maxValue).findAny().get());
 
         List<TrucoCard> cards = intel.getCards();
+        TrucoCard vira = intel.getVira();
+        cards.sort((card1, card2) -> card1.compareValueTo(card2, vira));
 
         TrucoCard cardMenor = cards.get(0);
+        TrucoCard cardMaior = cards.get(cards.size() - 1);
+
+
 
         for (TrucoCard card : cards){
             if (card.compareValueTo(cardMenor, intel.getVira()) <  0) cardMenor = card;
         }
 
-
         if (whatRound(intel) == 1 && intel.getOpponentCard().isEmpty()){
             return CardToPlay.of(cardMenor);
         }
 
+        if (whatRound(intel) == 1){
+            return getLessToKill(intel, cards, vira);
+        }
+
+        if (whatRound(intel) == 2){
+            if (intel.getOpponentCard().isPresent()) return getLessToKill(intel, cards, vira);
+            if (intel.getRoundResults().get(0) == GameIntel.RoundResult.WON) return CardToPlay.of(cardMaior);
+        }
+
         return CardToPlay.of(intel.getCards().get(0));
-
-
-
     }
 
+    private CardToPlay getLessToKill(GameIntel intel, List<TrucoCard> cards, TrucoCard vira) {
+        Optional<TrucoCard> opponentCard = intel.getOpponentCard();
 
+        List<TrucoCard> winningCards = cards.stream()
+                .filter(card -> opponentCard.isPresent() && card.compareValueTo(opponentCard.get(), vira) > 0)
+                .sorted((card1, card2) -> card1.compareValueTo(card2, vira))
+                .toList();
 
+        TrucoCard cardToPlay = winningCards.isEmpty() ? cards.get(0) : winningCards.get(0);
 
+        return CardToPlay.of(cardToPlay);
+    }
 
 
     public boolean isBiggestCoupleOrBlack(GameIntel gameIntel, List<TrucoCard> myCards) {
