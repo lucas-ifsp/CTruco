9e13f2ef16ce87362795ec80d74873a8bf19081f
Felipe Vilela <felipe.vilela01@outlook.com>
2024-11-12 16:24:31 -0300

    feat: logic update

--- bot-impl/src/main/java/com/caua/felipe/cfbot/CFBot.java
+++ bot-impl/src/main/java/com/caua/felipe/cfbot/CFBot.java
@@ -1,8 +1,12 @@
 package com.caua.felipe.cfbot;
 
-import com.bueno.spi.model.*;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.CardToPlay;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,98 +19,126 @@ public class CFBot implements BotServiceProvider {
         myCards.sort((card1, card2) -> card1.compareValueTo(card2, intel.getVira()));
         TrucoCard vira = intel.getVira();
 
-        if(isBiggestCoupleOrBlack(intel, myCards)) return 1;
+        if (isBiggestCoupleOrBlack(intel, myCards)) return 1;
         if (toTwoOrThree(intel, myCards) && myCards.stream().anyMatch(card -> card.isManilha(vira))) return 1;
         if (whatRound(intel) == 3 && myCards.stream().anyMatch(card -> card.isZap(intel.getVira()))) return 1;
+        if (isClubsAndDiamonds(intel, myCards)) return 0;
         if (whatRound(intel) == 3 && toTwoOrThree(intel, myCards)) return 0;
 
         return -1;
     }
 
-
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
         List<TrucoCard> myCards = intel.getCards();
+        TrucoCard vira = intel.getVira();
 
-        if (isBiggestCoupleOrBlack(intel, myCards)) return true;
-        if (toTwoOrThree(intel, myCards) && myCards.stream().anyMatch(card -> card.isZap(intel.getVira()))) return true;
-        if (toTwoOrThree(intel, myCards) && myCards.stream().anyMatch(card -> card.isCopas(intel.getVira()))) return true;
+        boolean hasManilhas = myCards.stream().anyMatch(card -> card.isManilha(vira));
+        boolean hasZap = myCards.stream().anyMatch(card -> card.isZap(vira));
 
-        return false;
+        return hasManilhas || hasZap || isBiggestCoupleOrBlack(intel, myCards);
     }
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
         List<TrucoCard> myCards = intel.getCards();
+        TrucoCard vira = intel.getVira();
 
-        if (isBiggestCoupleOrBlack(intel, myCards)) return true;
-        if (toTwoOrThree(intel, myCards) && myCards.stream().anyMatch(card -> card.isManilha(intel.getVira()))) return true;
-        if (myCards.stream().filter(card -> card.getRank().value() < 7).count() > 1) return false;
-        if (intel.getOpponentScore() > 8) return false;
-        if (myCards.stream().anyMatch(card -> card.isManilha(intel.getVira())) &&
-                myCards.stream().anyMatch(card -> card.relativeValue(intel.getVira()) > 6)) return true;
-        return false;
+        boolean hasHighCards = myCards.stream().anyMatch(card -> card.relativeValue(vira) > 6);
+        boolean hasManilhas = myCards.stream().anyMatch(card -> card.isManilha(vira));
+
+        if (hasManilhas || isBiggestCoupleOrBlack(intel, myCards)) {
+            return true;
+        }
+        return hasHighCards;
     }
-
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-
         List<TrucoCard> cards = intel.getCards();
         TrucoCard vira = intel.getVira();
+
         cards.sort((card1, card2) -> card1.compareValueTo(card2, vira));
 
-        TrucoCard cardMenor = cards.get(0);
-        TrucoCard cardMaior = cards.get(cards.size() - 1);
+        if (intel.getRoundResults().isEmpty()) return firstToPlay(intel, cards);
+        return playLater(intel, cards);
+    }
 
-        if (whatRound(intel) == 1 && intel.getOpponentCard().isEmpty()){
-            return CardToPlay.of(cardMenor);
-        }
+    private CardToPlay lowCard(List<TrucoCard> cards) {
+        return CardToPlay.of(cards.get(0));
+    }
 
-        if (whatRound(intel) == 1){
-            return getLessToKill(intel, cards, vira);
-        }
+    private CardToPlay highCard(List<TrucoCard> cards) {
+        return CardToPlay.of(cards.get(cards.size() - 1));
+    }
 
-        if (whatRound(intel) == 2){
-            if (intel.getOpponentCard().isPresent()) return getLessToKill(intel, cards, vira);
-            if (intel.getRoundResults().get(0) == GameIntel.RoundResult.WON) return CardToPlay.of(cardMaior);
-        }
+    private CardToPlay firstToPlay(GameIntel intel, List<TrucoCard> cards) {
+        if (whatRound(intel) == 1) return lowCard(cards);
+        if (whatRound(intel) == 2 && intel.getRoundResults().get(0) == GameIntel.RoundResult.WON)
+            return highCard(cards);
 
-        return CardToPlay.of(intel.getCards().get(0));
+        return lowCard(cards);
+    }
+
+    private CardToPlay playLater(GameIntel intel, List<TrucoCard> cards) {
+        if (whatRound(intel) == 1) return CardToPlay.of(cards.get(cards.size() - 1));
+
+        return getLessToKill(intel, cards, intel.getVira());
     }
 
     private CardToPlay getLessToKill(GameIntel intel, List<TrucoCard> cards, TrucoCard vira) {
         Optional<TrucoCard> opponentCard = intel.getOpponentCard();
 
+        if (opponentCard.isEmpty()) {
+            return lowCard(cards);
+        }
+
         List<TrucoCard> winningCards = cards.stream()
-                .filter(card -> opponentCard.isPresent() && card.compareValueTo(opponentCard.get(), vira) > 0)
-                .sorted((card1, card2) -> card1.compareValueTo(card2, vira))
+                .filter(card -> card.compareValueTo(opponentCard.get(), vira) > 0)
+                .sorted(Comparator.comparingInt(card -> card.compareValueTo(opponentCard.get(), vira)))
                 .toList();
 
-        TrucoCard cardToPlay = winningCards.isEmpty() ? cards.get(0) : winningCards.get(0);
+        boolean botWonPreviousRound = intel.getRoundResults().stream()
+                .anyMatch(result -> result == GameIntel.RoundResult.WON);
 
-        return CardToPlay.of(cardToPlay);
+        if (winningCards.isEmpty()) return lowCard(cards);
+        return CardToPlay.of(winningCards.get(0));
+
+
     }
 
+    public boolean isManilhaClubs(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.CLUBS);
+    }
+
+    public boolean isManilhaHearts(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.HEARTS);
+    }
+
+    public boolean isManilhaSpades(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.SPADES);
+    }
+
+    public boolean isManilhaDiamonds(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.DIAMONDS);
+    }
 
     public boolean isBiggestCoupleOrBlack(GameIntel gameIntel, List<TrucoCard> myCards) {
-
-        boolean isManilhaClubs = myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.CLUBS);
-        boolean isManilhaHearts = myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.HEARTS);
-        boolean isManilhaSpades = myCards.stream().anyMatch(card -> card.isManilha(gameIntel.getVira()) && card.getSuit() == CardSuit.SPADES);
-
-        if (isManilhaClubs && isManilhaHearts) return true;
-        return isManilhaClubs && isManilhaSpades;
+        if (isManilhaClubs(gameIntel, myCards) && isManilhaHearts(gameIntel, myCards)) return true;
+        return isManilhaClubs(gameIntel, myCards) && isManilhaSpades(gameIntel, myCards);
     }
 
-    public boolean toTwoOrThree(GameIntel gameIntel, List<TrucoCard> myCards){
-        return myCards.stream().anyMatch(card -> card.getRank().value() == 9 || card.getRank().value() == 10 );
+    public boolean isClubsAndDiamonds(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return isManilhaClubs(gameIntel, myCards) && isManilhaDiamonds(gameIntel, myCards);
     }
 
-    public int whatRound(GameIntel gameIntel){
+
+    public boolean toTwoOrThree(GameIntel gameIntel, List<TrucoCard> myCards) {
+        return myCards.stream().anyMatch(card -> card.getRank().value() == 9 || card.getRank().value() == 10);
+    }
+
+    public int whatRound(GameIntel gameIntel) {
         List<TrucoCard> cards = gameIntel.getCards();
-
-
         if (cards.size() == 3) return 1;
         if (cards.size() == 2) return 2;
         return 3;
