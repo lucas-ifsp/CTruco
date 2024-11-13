3ec474cbf221fe4b83a3ffd1d67afa53e9cc09ff
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-11-13 19:44:03 -0300

    feat(bot-impl/clojurebot): implements on ClojureBot.java concrete methods the injection of dependency for RoundStrategy

--- bot-impl/src/main/java/com/gusmao/matos/clojurebot/ClojureBot.java
+++ bot-impl/src/main/java/com/gusmao/matos/clojurebot/ClojureBot.java
@@ -7,22 +7,30 @@ import com.bueno.spi.service.BotServiceProvider;
 public final class ClojureBot implements BotServiceProvider {
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
-        return false;
+        final RoundStrategy strategy = Playing.buildStrategy(intel);
+
+        return strategy.getMaoDeOnzeResponse(intel);
     }
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        return false;
+        final RoundStrategy strategy = Playing.buildStrategy(intel);
+
+        return strategy.decideIfRaises(intel);
     }
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        return null;
+        final RoundStrategy strategy = Playing.buildStrategy(intel);
+
+        return strategy.chooseCard(intel);
     }
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
-        return 0;
+        final RoundStrategy strategy = Playing.buildStrategy(intel);
+
+        return strategy.getRaiseResponse(intel);
     }
 
     @Override
