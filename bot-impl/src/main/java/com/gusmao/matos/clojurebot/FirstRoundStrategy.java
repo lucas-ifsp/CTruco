20e41ec3460a072e4bd090d46668ffadd2ce5a1b
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-11-13 20:28:42 -0300

    feat(bot-impl/clojurebot): implements initial logic for the FirstRoundStrategy::getMaoDeOnzeResponse

--- bot-impl/src/main/java/com/gusmao/matos/clojurebot/FirstRoundStrategy.java
+++ bot-impl/src/main/java/com/gusmao/matos/clojurebot/FirstRoundStrategy.java
@@ -2,17 +2,25 @@ package com.gusmao.matos.clojurebot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 
 public final class FirstRoundStrategy implements RoundStrategy {
     private final GameIntel gameIntel;
+    private final TrucoCard vira;
 
     public FirstRoundStrategy(GameIntel gameIntel) {
         this.gameIntel = gameIntel;
+        this.vira = gameIntel.getVira();
     }
 
     @Override
     public boolean getMaoDeOnzeResponse() {
-        return false;
+        final int handPower = HandUtils.getHandPower(gameIntel, vira);
+        final int pointsDifference = gameIntel.getScore() - gameIntel.getOpponentScore();
+
+        if (handPower > 22) return true;
+
+        return pointsDifference > 9 && handPower > 18;
     }
 
     @Override
