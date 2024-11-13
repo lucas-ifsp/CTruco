f7e676acd3251823875993ac41d069c1019e434b
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-11-13 20:52:03 -0300

    feat(bot-impl/clojurebot): create HandUtils::getLessCardToWin
    Method that returns the card with the lowest value to kill the card on the table.
    If not, return null.

--- bot-impl/src/main/java/com/gusmao/matos/clojurebot/HandUtils.java
+++ bot-impl/src/main/java/com/gusmao/matos/clojurebot/HandUtils.java
@@ -1,13 +1,27 @@
 package com.gusmao.matos.clojurebot;
 
 
+import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
+
+import java.util.List;
+import java.util.Optional;
 
 public final class HandUtils {
     private HandUtils() {}
 
     public static int getHandPower(GameIntel intel, TrucoCard vira) {
         return intel.getCards().stream().mapToInt(trucoCard -> trucoCard.relativeValue(vira)).sum();
+    }
+
+    public static CardToPlay getLessCardToWin(List<TrucoCard> cards, TrucoCard opponentCard, TrucoCard vira) {
+        final Optional<TrucoCard> optionalCard = cards.stream().filter(card -> card.compareValueTo(opponentCard, vira) > 0).findFirst();
+
+        if (optionalCard.isPresent()) {
+            return CardToPlay.of(optionalCard.get());
+        }
+
+        return null;
     }
 }
