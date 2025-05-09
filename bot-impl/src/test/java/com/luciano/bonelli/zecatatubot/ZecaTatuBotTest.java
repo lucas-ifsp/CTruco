60ea7523874efc284fdafbc850d1d7c2f6e9e2d4
Lucas Luciano <lucasrluciano23@gmail.com>
2025-05-09 15:41:33 -0300

    test: CountHowManyManilhasInHand

--- bot-impl/src/test/java/com/luciano/bonelli/zecatatubot/ZecaTatuBotTest.java
+++ bot-impl/src/test/java/com/luciano/bonelli/zecatatubot/ZecaTatuBotTest.java
@@ -1,20 +1,30 @@
 package com.luciano.bonelli.zecatatubot;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ZecaTatuBotTest {
     private ZecaTatuBot zecaTatuBot;
     private GameIntel.StepBuilder stepBuilder;
+    private GameIntel intel;
 
     @BeforeEach
     public void setup() {
         zecaTatuBot = new ZecaTatuBot();
+        intel = mock(GameIntel.class);
     }
 
     @Nested
@@ -41,4 +51,18 @@ class ZecaTatuBotTest {
     class GetRaiseResponseTest {
     }
 
+    @Test
+    @DisplayName("CountHowManyManilhasInHand")
+    public void countManilhas() {
+        TrucoCard vira = TrucoCard.of(CardRank.SEVEN, CardSuit.SPADES);
+        when(intel.getVira()).thenReturn(vira);
+        TrucoCard card1 = TrucoCard.of(CardRank.THREE, CardSuit.HEARTS);
+        TrucoCard card2 = TrucoCard.of(CardRank.QUEEN, CardSuit.CLUBS);
+        TrucoCard card3 = TrucoCard.of(CardRank.QUEEN, CardSuit.SPADES);
+
+        when(intel.getCards()).thenReturn(List.of(card1, card2, card3));
+        long countManilha = zecaTatuBot.countManilha(intel);
+        assertThat(countManilha).isEqualTo(2);
+    }
 }
+
