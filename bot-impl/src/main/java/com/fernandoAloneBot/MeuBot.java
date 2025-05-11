e969e2ac0b2db9efa72b66c5e8222eff3178c08b
Fernando Rodrigues <fernando.candido@aluno.ifsp.edu.br>
2025-05-10 23:24:47 -0300

    feat: adiciona método para verificar duas manilhas na mão

--- bot-impl/src/main/java/com/fernandoAloneBot/MeuBot.java
+++ bot-impl/src/main/java/com/fernandoAloneBot/MeuBot.java
@@ -2,6 +2,7 @@ package com.fernandoAloneBot;
 
 import com.bueno.spi.model.CardToPlay;
 import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
 
 public class MeuBot implements BotServiceProvider {
@@ -25,8 +26,32 @@ public class MeuBot implements BotServiceProvider {
         return 0;
     }
 
+
+
+
+
+    // Verifica se o jogador tem duas manilhas
+    private boolean hasDuasManilhas(GameIntel intel) {
+        TrucoCard cardVira = intel.getVira();
+        int contador = 0;
+        for (TrucoCard card : intel.getCards()) {
+            if (card.isManilha(cardVira)) {
+                contador++;
+            }
+        }
+        return contador >= 2;
+    }
+
+
+
     @Override
     public String getName() {
         return BotServiceProvider.super.getName();
     }
+
+
 }
+
+
+
+
