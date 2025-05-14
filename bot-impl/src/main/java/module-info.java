db4f2cbfe076fe712b3c9c36317bb66789015ae9
michelledantas <michelledantasluiz@gmail.com>
2025-05-13 23:31:07 -0300

    chore: adding imports

--- bot-impl/src/main/java/module-info.java
+++ bot-impl/src/main/java/module-info.java
@@ -1,14 +1,15 @@
 
-import com.bueno.impl.dummybot.DummyBot;
-import com.carvalho.candido.tomegapbot.GapBot;
-import com.pedro.herick.skilldiffbot.SkillDiffBot;
 import com.petrilli.sandro.malasiabot.MalasiaBot;
+import com.grupo.firsts.PeNaCova;
+import com.abel.francisco.fogao6boca.Fogao6Boca;
+import com.bueno.impl.dummybot.DummyBot;
 import com.aah.refactor.me.RefactorMePleaseBot;
 import com.contiero.lemes.atrasabot.AtrasaBot;
 import com.felipe.fabiano.truccard.Truccard;
+import com.carvalho.candido.tomegapbot.GapBot;
 import com.brito.macena.boteco.BotEco;
-import com.abel.francisco.fogao6boca.Fogao6Boca;
 import com.ghenrique.moedordecana.MoedorDeCana;
+import com.pedro.herick.skilldiffbot.SkillDiffBot;
 
 module bot.impl {
     requires bot.spi;
@@ -16,6 +17,8 @@ module bot.impl {
     requires java.smartcardio;
     requires jdk.jdi;
 
+    exports com.grupo.firsts;
+    exports com.abel.francisco.fogao6boca;
     exports com.bueno.impl.dummybot;
     exports com.aah.refactor.me;
     exports com.contiero.lemes.atrasabot;
@@ -24,19 +27,17 @@ module bot.impl {
     exports com.brito.macena.boteco;
     exports com.ghenrique.moedordecana;
     exports com.pedro.herick.skilldiffbot;
-    exports com.abel.francisco.fogao6boca;
-    exports com.grupo.firsts;
 
     provides com.bueno.spi.service.BotServiceProvider with
-            DummyBot,
-            RefactorMePleaseBot,
-            MalasiaBot,
-            AtrasaBot,
-            Truccard,
-            GapBot,
-            BotEco,
-            MoedorDeCana,
-            Fogao6Boca,
-            SkillDiffBot,
-            PeNaCova;
+        PeNaCova,
+        DummyBot,
+        RefactorMePleaseBot,
+        MalasiaBot,
+        AtrasaBot,
+        Truccard,
+        GapBot,
+        BotEco,
+        MoedorDeCana,
+        Fogao6Boca,
+        SkillDiffBot;
 }
