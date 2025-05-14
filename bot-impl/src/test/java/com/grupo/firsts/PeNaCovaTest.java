067d569b699cf63e222da84f2ef52ee0aef1e9c1
michelledantas <michelledantasluiz@gmail.com>
2025-05-13 23:42:35 -0300

    test: adding test shouldAcceptMaoDeOnze

---
+++ bot-impl/src/test/java/com/grupo/firsts/PeNaCovaTest.java
@@ -0,0 +1,63 @@
+package com.grupo.firsts;
+
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PeNaCovaTest {
+
+  private PeNaCova bot;
+
+  @BeforeEach
+  public void setUp(){
+    bot = new PeNaCova();
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "JACK, HEARTS, QUEEN, SPADES, FIVE, DIAMONDS, true",
+      "TWO, HEARTS, FIVE, SPADES, SIX, DIAMONDS, false"
+  })
+  @DisplayName("Should determine if the bot accepts Mão de Onze")
+  void shouldAcceptMaoDeOnze(
+      CardRank rank1, CardSuit suit1,
+      CardRank rank2, CardSuit suit2,
+      CardRank rank3, CardSuit suit3,
+      boolean expected
+  ){
+
+    GameIntel intel = GameIntel.StepBuilder.with()
+        .gameInfo(
+            List.of(),
+            List.of(),
+            TrucoCard.of(CardRank.THREE,CardSuit.HEARTS),
+            0
+        )
+        .botInfo(
+            List.of(
+                TrucoCard.of(rank1,suit1),
+                TrucoCard.of(rank2,suit2),
+                TrucoCard.of(rank3,suit3)
+            ),
+            5
+        )
+        .opponentScore(3)
+        .build();//Opponent score
+
+
+    boolean result = bot.getMaoDeOnzeResponse(intel);
+
+    assertThat(result).isEqualTo(expected);
+  }
+
+
+}
