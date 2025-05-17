4eedc5f27b87812c9d37bb6e9ec8eed6e8b83f64
Vitor Aguiar <galaxiustraveler@outlook.com>
2025-05-17 20:25:13 -0300

    refactor(trucorinthians): organize round strategies

--- bot-impl/src/main/java/com/aguiar/vitor/trucorinthians/Trucorinthians.java
+++ bot-impl/src/main/java/com/aguiar/vitor/trucorinthians/Trucorinthians.java
@@ -31,30 +31,21 @@ public class Trucorinthians implements BotServiceProvider {
     }
 
     private CardToPlay getStrategicCard(GameIntel intel) {
-        List<TrucoCard> hand = intel.getCards();
-        TrucoCard vira = intel.getVira();
         int round = intel.getRoundResults().size();
         boolean isFirstToPlay = intel.getOpponentCard().isEmpty();
         TrucoCard opponentCard = intel.getOpponentCard().orElse(TrucoCard.closed());
+        List<TrucoCard> hand = intel.getCards();
+        TrucoCard vira = intel.getVira();
 
-        if (round == 0) {
-            return isFirstToPlay ? getWeakest(hand, vira) : getSmallestNonLosing(hand, vira, opponentCard);
-        }
-
-        if (round == 1) {
-            GameIntel.RoundResult firstResult = intel.getRoundResults().get(0);
-            switch (firstResult) {
-                case WON:
-                    return isFirstToPlay ? getWeakest(hand, vira) : getSmallestNonLosing(hand, vira, opponentCard);
-                case LOST:
-                    return getStrongest(hand, vira);
-                case DREW:
-                    break;
-            }
-        }
-
-        if (round == 2) {
-            return getStrongest(hand, vira);
+        switch (round) {
+            case 0:
+                return isFirstToPlay ? getWeakest(hand, vira) : getSmallestNonLosing(hand, vira, opponentCard);
+            case 1:
+                return getSecondRoundStrategy(intel);
+            case 2:
+                return getStrongest(hand, vira);
+            default:
+                break;
         }
 
         return CardToPlay.of(hand.get(0));
@@ -82,5 +73,24 @@ public class Trucorinthians implements BotServiceProvider {
                 .max(Comparator.comparingInt(card -> card.relativeValue(vira)))
                 .orElse(TrucoCard.closed());
         return CardToPlay.of(strongest);
+    }
+
+    private CardToPlay getSecondRoundStrategy(GameIntel intel) {
+        GameIntel.RoundResult firstResult = intel.getRoundResults().get(0);
+        boolean isFirstToPlay = intel.getOpponentCard().isEmpty();
+        TrucoCard opponentCard = intel.getOpponentCard().orElse(TrucoCard.closed());
+        List<TrucoCard> hand = intel.getCards();
+        TrucoCard vira = intel.getVira();
+
+        switch (firstResult) {
+            case WON:
+                return isFirstToPlay ? getWeakest(hand, vira) : getSmallestNonLosing(hand, vira, opponentCard);
+            case LOST:
+                return getStrongest(hand, vira);
+            case DREW:
+                return getStrongest(hand, vira);
+        }
+
+        return getStrongest(hand, vira);
     }
 }
