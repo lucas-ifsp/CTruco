338748aaaa205f92a775b75ac72b96d361f5423c
michelledantas <michelledantasluiz@gmail.com>
2025-05-18 15:50:48 -0300

    test: adding new scenario

--- bot-impl/src/test/java/com/grupo/firsts/PeNaCovaTest.java
+++ bot-impl/src/test/java/com/grupo/firsts/PeNaCovaTest.java
@@ -406,6 +406,15 @@ class PeNaCovaTest {
             ),
             TrucoCard.of(CardRank.FIVE, CardSuit.SPADES),
             1
+        ),
+        Arguments.of(
+            List.of(
+                TrucoCard.of(CardRank.SIX, CardSuit.SPADES),
+                TrucoCard.of(CardRank.ACE, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.DIAMONDS)
+            ),
+            TrucoCard.of(CardRank.FIVE, CardSuit.CLUBS),
+            1
         )
     );
   }
