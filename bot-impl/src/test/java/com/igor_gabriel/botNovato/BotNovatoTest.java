abc98e539d47286963988ca07366de94f00b1c06
Igor da Fonseca <igorfonseca-sc@hotmail.com>
2025-05-18 15:38:15 -0300

    test: Criacao da classe de testes

---
+++ bot-impl/src/test/java/com/igor_gabriel/botNovato/BotNovatoTest.java
@@ -0,0 +1,28 @@
+package com.igor_gabriel.botNovato;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotNovatoTest {
+
+    @Test
+    void getMaoDeOnzeResponse() {
+    }
+
+    @Test
+    void decideIfRaises() {
+    }
+
+    @Test
+    void chooseCard() {
+    }
+
+    @Test
+    void getRaiseResponse() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
